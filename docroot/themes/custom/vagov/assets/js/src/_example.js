0b1e3f71421ebce6e2158726aefd16e4308967d5
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2018-11-08 09:04:26 -0700

    VA-277: Removing test JS file. Adding empty lines to the bottom of twig and yml files

--- docroot/themes/custom/vagov/assets/js/src/_example.js
+++
@@ -1,8 +0,0 @@
-
-(function ($, Drupal) {
-    Drupal.behaviors.exampleBehavior = {
-        attach:function (context, settings) {
-            console.log('test from example behavior');
-        }
-    };
-})(jQuery, Drupal);
