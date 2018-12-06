5c8dcd0577b8d15b0dda2fa7efbaf0ba667c7f24
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2018-12-05 18:25:22 -0700

    VAGOV-647: app scaffolding

---
+++ metalsmith_app/index.js
@@ -0,0 +1,15 @@
+const Metalsmith = require('metalsmith');
+const inplace = require('metalsmith-in-place');
+const debug = require('metalsmith-debug');
+
+Metalsmith(__dirname)
+    .source('./src')
+    .destination('./build')
+    .clean(true)
+    .use(debug())
+    .use(inplace())
+    .build(function(err) { // build
+        if (err) {
+            throw err;
+        } // error handling
+    });
