ec2a63f80e2314622d072f63818aa0538ea95a88
Mouncif Benziane <mouncif.benziane@gmail.com>
2019-01-08 15:21:00 -0800

    make the timeout little longer

--- docker/config/engine/scripts/lando-entrypoint-app.sh
+++ docker/config/engine/scripts/lando-entrypoint-app.sh
@@ -24,7 +24,7 @@ j2 /templates/settings.lando.php.tpl > /app/docroot/sites/default/settings.lando
 
 # Run post-deploy hooks
 # Make sure backend services are up
-/usr/bin/wait-for-it.sh ${DRUPAL_DATABASE_HOST}:${DRUPAL_DATABASE_HOST_PORT}
+/usr/bin/wait-for-it.sh -t 120 ${DRUPAL_DATABASE_HOST}:${DRUPAL_DATABASE_HOST_PORT}
 cd ${LANDO_WEBROOT}/vendor/bin/
 ./drush cache:rebuild
 ./drush updatedb -y
