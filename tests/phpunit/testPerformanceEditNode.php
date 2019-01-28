e97ffb5ab5330c95bfd1e7ccd6c674928b23cd50
schiavone <daniel@snakehill.net>
2019-01-28 12:20:02 -0500

    VAGOV-1360 Test performance of editing landing page

---
+++ tests/phpunit/testPerformanceEditNode.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace tests\phpunit;
+
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * A test to confirm amount of nodes by type.
+ */
+class EditNodePerformance extends ExistingSiteBase {
+
+  /**
+   * A test method to deterine the amount of time to edit a node page.
+   *
+   * @group performance
+   *
+   * @dataProvider benchmarkTime
+   */
+  public function testEditNodePerformance($type, $benchmark) {
+    // Creates a user. Will be automatically cleaned up at the end of the test.
+    $author = $this->createUser();
+
+    // Creates a node. Will be automatically cleaned up at the end of the test.
+    $node = $this->createNode([
+      'title' => 'Llama',
+      'type' => $type,
+      'uid' => $author->id(),
+    ]);
+    $node->setPublished()->save();
+
+    // Start timer.
+    $mtime = microtime();
+    $mtime = explode(" ", $mtime);
+    $mtime = $mtime[1] + $mtime[0];
+    $starttime = $mtime;
+
+    $node->setChangedTime(time())->save();
+
+    // End timer.
+    $mtime = microtime();
+    $mtime = explode(" ", $mtime);
+    $mtime = $mtime[1] + $mtime[0];
+    $endtime = $mtime;
+    $microsecs = ($endtime - $starttime);
+
+    // Test assertion.
+    $secs = number_format($microsecs, 3);
+    $this->assertLessThan($benchmark, $secs, "\nOperation took " . $secs . " seconds which is longer than the benchmark of " . $benchmark . " seconds for type " . $type . ".\n");
+
+    $message = "\nOperation took " . $secs . " seconds compared to the benchmark of " . $benchmark . " seconds for type " . $type . ".\n";
+    fwrite(STDERR, print_r($message, TRUE));
+  }
+
+  /**
+   * Returns benchmark time to beat in order for test to succeed.
+   *
+   * @return array
+   *   Array containing entity type as string and benchmark as int
+   */
+  public function benchmarkTime() {
+    return [
+      ["page", 2],
+      ["landing_page", 2],
+    ];
+  }
+
+}
