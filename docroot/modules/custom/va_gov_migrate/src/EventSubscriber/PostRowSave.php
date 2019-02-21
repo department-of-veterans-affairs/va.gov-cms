21f7d167d8a708dde58771466090ecbb1f1b7661
Beth Jacobson <beth.jacobson@civicactions.com>
2019-02-21 12:14:16 -0500

    VAGOV-1568: Migrate health care sidebar menu and page aliases.

--- docroot/modules/custom/va_gov_migrate/src/EventSubscriber/PostRowSave.php
+++ docroot/modules/custom/va_gov_migrate/src/EventSubscriber/PostRowSave.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\va_gov_migrate\EventSubscriber;
 
+use Drupal\menu_link_content\Entity\MenuLinkContent;
+use Drupal\migration_tools\Message;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\va_gov_migrate\ParagraphMigrator;
 use Drupal\migrate\Event\MigratePostRowSaveEvent;
@@ -44,6 +46,7 @@ class PostRowSave implements EventSubscriberInterface {
         $migrator->process('featured_content', 'field_featured_content');
         $migrator->process(['body', 'nav_linkslist'], 'field_content_block');
         $this->setChangedDate($event);
+        $this->setNodeAlias($event);
         break;
 
       case 'va_alert_block':
@@ -54,11 +57,15 @@ class PostRowSave implements EventSubscriberInterface {
         $this->convertIntroTextToPlainText($event->getDestinationIdValues()[0]);
         $migrator->process('related_links', 'field_related_links');
         $migrator->process('hub_links', 'field_spokes');
+        $this->setNodeAlias($event);
         break;
 
       case 'va_promo':
         $migrator->process('body', 'field_promo_link');
         break;
+
+      case 'va_benefits_menu':
+        $this->setMenuParent($event);
     }
   }
 
@@ -102,6 +109,65 @@ class PostRowSave implements EventSubscriberInterface {
     $last_update = $event->getRow()->getSourceProperty('lastupdate') + 3600 * 4;
     $node->set('changed', $last_update);
     $node->save();
+  }
+
+  /**
+   * Delete any existing alias for this node and set a new one from 'path'.
+   *
+   * Also turns off automatic URL alias generation for this node.
+   *
+   * @param \Drupal\migrate\Event\MigratePostRowSaveEvent $event
+   *   The postRowSave event.
+   */
+  public function setNodeAlias(MigratePostRowSaveEvent $event) {
+    $alias = $event->getRow()->getSourceProperty('path');
+    if (empty($alias)) {
+      return;
+    }
+
+    $alias_storage_helper = \Drupal::service('pathauto.alias_storage_helper');
+    $source = '/node/' . $event->getDestinationIdValues()[0];
+
+    $alias_storage_helper->deleteBySourcePrefix($source);
+    $alias_storage_helper->save([
+      'source' => $source,
+      'alias' => $alias,
+      'language' => 'en',
+    ]);
+
+    // Tell pathauto not to override alias.
+    $pathauto_store = \Drupal::keyValue('pathauto_state.node');
+    $pathauto_store->set($event->getDestinationIdValues()[0], 0);
+  }
+
+  /**
+   * Sets the parent item of the menu item being migrated.
+   *
+   * @param \Drupal\migrate\Event\MigratePostRowSaveEvent $event
+   *   The PostRowSave event.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Drupal\migrate\MigrateException
+   */
+  public function setMenuParent(MigratePostRowSaveEvent $event) {
+    $parent_id = $event->getRow()->getSourceProperty('parent_id');
+    if (!empty($parent_id)) {
+      $parent_link_array = $event->getMigration()->getIdMap()->lookupDestinationIds([$parent_id]);
+      if (empty($parent_link_array)) {
+        Message::make("Couldn't find dest for @id", ['@id' => $parent_id], Message::ERROR);
+      }
+      else {
+        $menu_link = MenuLinkContent::load($event->getDestinationIdValues()[0]);
+        $parent_link = MenuLinkContent::load($parent_link_array[0][0]);
+        if (empty($parent_link)) {
+          Message::make("Couldn't find menu link for @id", ['@id' => $parent_link_array[0][0]], Message::ERROR);
+        }
+        else {
+          $menu_link->set('parent', 'menu_link_content:' . $parent_link->get('uuid')->value);
+          $menu_link->save();
+        }
+      }
+    }
   }
 
 }
