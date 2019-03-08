ac7f34d33c8b62a731f8f3005f33914b0c89c541
Andy Hawks <andy@andyhawks.com>
2019-03-08 12:36:36 -0700

    VAGOV-1757: Add config_split dev config.

--- docroot/sites/default/settings.local.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// @codingStandardsIgnoreFile
-
-$config['environment_indicator.indicator']['bg_color'] = '#ffffff';
-$config['environment_indicator.indicator']['fg_color'] = '#000000';
-$config['environment_indicator.indicator']['name'] = 'Local';
