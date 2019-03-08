ed135db3e548dadaf05cc6c0d170ecfa07f71b65
Andy Hawks <andy@andyhawks.com>
2019-03-08 12:01:09 -0700

    VAGOV-1759: Update environment_indicator config.

--- docroot/sites/default/settings.lando.php
+++ docroot/sites/default/settings.lando.php
@@ -1,5 +1,7 @@
 <?php
 
+// @codingStandardsIgnoreFile
+
 $databases['default']['default'] = array (
   'database' => 'drupal8',
   'username' => 'drupal8',
--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -782,4 +782,8 @@ if (file_exists($app_root . '/' . $site_path . '/settings.lando.php')) {
   include $app_root . '/' . $site_path . '/settings.lando.php';
 }
 
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+    include $app_root . '/' . $site_path . '/settings.local.php';
+}
+
 $config_directories['sync'] = '../config/sync';
