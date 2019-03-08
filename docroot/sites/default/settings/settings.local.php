36db9fa2dc2ad1b5aa0f62498d5d5bd9f5179b63
Andy Hawks <andy@andyhawks.com>
2019-03-08 12:57:42 -0700

    VAGOV-1757: Add cache settings for all environments.

--- docroot/sites/default/settings/settings.local.php
+++ docroot/sites/default/settings/settings.local.php
@@ -3,6 +3,15 @@
 // @codingStandardsIgnoreFile
 
 $config['config_split.config_split.config_dev']['status'] = TRUE;
+$config['system.performance']['cache']['page']['use_internal'] = FALSE;
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['css']['gzip'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$config['system.performance']['js']['gzip'] = FALSE;
+$config['system.performance']['response']['gzip'] = FALSE;
+$config['views.settings']['ui']['show']['sql_query']['enabled'] = TRUE;
+$config['views.settings']['ui']['show']['performance_statistics'] = TRUE;
+$config['system.logging']['error_level'] = 'all';
 $config['environment_indicator.indicator']['bg_color'] = '#ffffff';
 $config['environment_indicator.indicator']['fg_color'] = '#000000';
 $config['environment_indicator.indicator']['name'] = 'Local';
