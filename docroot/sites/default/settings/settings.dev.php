7ad3e872554a7e5294799a1ae47d7cdee7ed4495
Elijah Lynn <elijah@elijahlynn.net>
2019-03-19 22:46:43 -0700

    VAGOV-320 Fix fast404 include and add to DEV.

--- docroot/sites/default/settings/settings.dev.php
+++ docroot/sites/default/settings/settings.dev.php
@@ -27,3 +27,7 @@ $settings['trusted_host_patterns'] = [
     '^dev\.cms\.va\.gov$',
     '^*\.us-gov-west-1\.elb\.amazonaws\.com$',
 ];
+
+if (file_exists($app_root . '/' . $site_path . '/settings/settings.fast_404.php')) {
+  include $app_root . '/' . $site_path . '/settings/settings.fast_404.php';
+}
