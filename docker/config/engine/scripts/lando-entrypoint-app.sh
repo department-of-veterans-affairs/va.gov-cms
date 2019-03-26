86796f10da7896a49b99ecaa1c3cf57cbdda2ea4
Mouncif Benziane <mouncif.benziane@gmail.com>
2019-03-26 11:40:14 -0700

    test one off task deploy

--- docker/config/engine/scripts/lando-entrypoint-app.sh
+++ docker/config/engine/scripts/lando-entrypoint-app.sh
@@ -25,23 +25,23 @@ j2 /templates/settings.lando.php.tpl > /app/docroot/sites/default/settings/setti
 # Run post-deploy hooks
 # Make sure backend services are up
 /usr/bin/wait-for-it.sh -t 120 ${DRUPAL_DATABASE_HOST}:${DRUPAL_DATABASE_HOST_PORT}
-cd ${LANDO_WEBROOT}/vendor/bin/
-./drush cache:rebuild
-./drush updatedb -y
-./drush config:import -y
-./drush cache:rebuild
+# cd ${LANDO_WEBROOT}/vendor/bin/
+# ./drush cache:rebuild
+# ./drush updatedb -y
+# ./drush config:import -y
+# ./drush cache:rebuild
 
 # Sync drupal site/default/files
-if [ "${SYNC_SITE_FILES}" = "yes" ] ; then
-  backup_url=$(curl -L https://s3-us-gov-west-1.amazonaws.com/${S3_BACKUP_BUCKET_PUB}/files/latest_url)
-  curl ${backup_url} -o /tmp/cmsapp_files.tgz
-  mount -a
-  [ -d /app/docroot/sites/default/files ] && echo "Site files directory exists, Not creating" || mkdir /app/docroot/sites/default/files
-  rm -fR /app/docroot/sites/default/files/*
-  tar -xzvf /tmp/cmsapp_files.tgz --directory /app/docroot/sites/default/files
-else
-  echo "Skipping site default files sync" ;
-fi
+# if [ "${SYNC_SITE_FILES}" = "yes" ] ; then
+#   backup_url=$(curl -L https://s3-us-gov-west-1.amazonaws.com/${S3_BACKUP_BUCKET_PUB}/files/latest_url)
+#   curl ${backup_url} -o /tmp/cmsapp_files.tgz
+#   mount -a
+#   [ -d /app/docroot/sites/default/files ] && echo "Site files directory exists, Not creating" || mkdir /app/docroot/sites/default/files
+#   rm -fR /app/docroot/sites/default/files/*
+#   tar -xzvf /tmp/cmsapp_files.tgz --directory /app/docroot/sites/default/files
+# else
+#   echo "Skipping site default files sync" ;
+# fi
 
 # Run the COMMAND
 echo "Running command $@"
