e16218f664362a80e87a6ce7338ed36608d2ad5b
Mouncif Benziane <mouncifb@gmail.com>
2019-04-15 08:15:56 -0700

    check CMS service based on HTTP status code

--- docker/config/engine/scripts/check-health-cms-service.sh
+++ docker/config/engine/scripts/check-health-cms-service.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 HTTP_CODE_STATUS='200'
-HTTP_PORT="80"
-SLEEP_TIME="20"
+HTTP_PORT='80'
+SLEEP_TIME='20'
 
 while true
 do
   STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:$HTTP_PORT)
   if [ $STATUS -eq $HTTP_CODE_STATUS ]; then
-    echo "Got 200! CMS service is up!"
+    echo "Got 200 HTTP status code, CMS service is up."
     break
   else
-    echo "Got $STATUS : CMS Service is not up yet, retrying..."
+    echo "Got $STATUS HTTP status code, CMS Service is not up yet, retrying..."
   fi
   sleep $SLEEP_TIME
 done
