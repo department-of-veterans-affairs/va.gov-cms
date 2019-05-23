041755c1b9d7169601e5b18aab4e4935b7a12379
Beth Jacobson <beth.jacobson@civicactions.com>
2019-05-22 20:36:18 -0400

    VAGOV-000: Migrate tables in accordions.

--- docroot/modules/custom/va_gov_migrate/src/Paragraph/CollapsiblePanelItem.php
+++ docroot/modules/custom/va_gov_migrate/src/Paragraph/CollapsiblePanelItem.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\va_gov_migrate\Paragraph;
 
+use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\va_gov_migrate\ParagraphType;
 use QueryPath\DOMQuery;
 use Drupal\migration_tools\Message;
@@ -12,6 +13,13 @@ use Drupal\migration_tools\Message;
  * @package Drupal\va_gov_migrate\Paragraph
  */
 class CollapsiblePanelItem extends ParagraphType {
+
+  /**
+   * Contains any tables found in the content.
+   *
+   * @var \QueryPath\DOMQuery
+   */
+  protected  $tables;
 
   /**
    * {@inheritdoc}
@@ -37,14 +45,42 @@ class CollapsiblePanelItem extends ParagraphType {
           '@title' => $query_path->children('button.usa-accordion-button')->text(),
         ], Message::ERROR);
     }
+    $contents = $query_path->children('.usa-accordion-content');
+
+    // Tables are handled as child paragraphs, so we need to wrap them in a
+    // parent tag and save them for later.
+    if ($contents->find('table')->count()) {
+      $tables = $contents->find('table')->remove();
+      $this->tables = qp('<div id="tables">' . $tables->html() . '</div>')->find('#tables');
+    }
     return
       [
         'field_title' => $query_path->children('button.usa-accordion-button')->text(),
-        'field_wysiwyg' => [
-          "value" => $query_path->children('.usa-accordion-content')->html(),
-          "format" => "rich_text",
-        ],
+        'field_wysiwyg' => self::toRichText($contents->html()),
       ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getParagraphField() {
+    return 'field_va_paragraphs';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function addChildParagraphs(Paragraph $paragraph, DOMQuery $query_path = NULL) {
+    if (!empty($this->tables)) {
+      parent::addChildParagraphs($paragraph, $this->tables);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function paragraphContent(array $paragraph_fields) {
+    return $paragraph_fields['field_title'] . $paragraph_fields['field_wysiwyg']['value'];
   }
 
 }
