e99caf5b16c22c7e7789d4a803fd19132f6bb157
Beth Jacobson <beth.jacobson@civicactions.com>
2019-06-06 12:33:12 -0400

    VAGOV-000: Remove content test for Q&A (superseded by similarity test).

--- docroot/modules/custom/va_gov_migrate/src/Paragraph/QAUnstructured.php
+++ docroot/modules/custom/va_gov_migrate/src/Paragraph/QAUnstructured.php
@@ -15,27 +15,6 @@ use QueryPath\DOMQuery;
 class QAUnstructured extends QABase {
 
   /**
-   * A DOMQuery containing answer content.
-   *
-   * @var \QueryPath\DOMQuery
-   */
-  protected static $answerQuery;
-
-  /**
-   * A DOMQuery containing content that returned true for isExternalContent.
-   *
-   * @var \QueryPath\DOMQuery
-   */
-  protected static $externalContent;
-
-  /**
-   * Used for error messages.
-   *
-   * @var string
-   */
-  protected static $lastQuestionText;
-
-  /**
    * The heading level of the last question ('1' for h1, '2' for h2, etc).
    *
    * @var string
@@ -70,7 +49,6 @@ class QAUnstructured extends QABase {
     $qp = $query_path->prev();
     while ($qp->count()) {
       if (self::isQuestion($qp)) {
-        $query_path->appendTo(self::$externalContent);
         return TRUE;
       }
       if (substr($qp->tag(), 0, 1) == 'h' &&
@@ -91,13 +69,8 @@ class QAUnstructured extends QABase {
    * {@inheritdoc}
    */
   protected function getFieldValues(DOMQuery $query_path) {
-    // Before we create a new Q&A, make sure the last one worked correctly.
-    $this->checkContent();
-
     // Used to test subsequent elements to see if they're part of the answer.
     self::$lastQuestionLevel = substr($query_path->tag(), 1);
-    // Used for error message if excluded content and answer donn't match.
-    self::$lastQuestionText = $query_path->text();
 
     return [
       'field_question' => $query_path->text(),
@@ -121,7 +94,7 @@ class QAUnstructured extends QABase {
     // all subsequent sibling tags in the query path
     // until we hit another question, an hr, or any header at or above the
     // question level.
-    self::$answerQuery = qp('<div id="answer"/>')->find('#answer');
+    $answerQuery = qp('<div id="answer"/>')->find('#answer');
     $qp = $query_path->next();
     while ($qp->count()) {
       // Stop if it's another question.
@@ -140,16 +113,13 @@ class QAUnstructured extends QABase {
       if ($this->isLinksList($qp)) {
         break;
       }
-      $qp->appendTo(self::$answerQuery);
+      $qp->appendTo($answerQuery);
       $qp = $qp->next();
     }
 
-    // Place to collect external content to compare to question content.
-    self::$externalContent = qp('<div id="answer"/>')->find('#answer');
-
     // Transform the answerQuery we collected in getFieldValues into paragraphs.
-    if (!empty(self::$answerQuery) && self::$answerQuery->children()->count()) {
-      self::$migrator->addParagraphs(self::$answerQuery->children(), $paragraph, $this->getParagraphField());
+    if (!empty($answerQuery) && $answerQuery->children()->count()) {
+      self::$migrator->addParagraphs($answerQuery->children(), $paragraph, $this->getParagraphField());
       self::$migrator->addWysiwyg($paragraph, $this->getParagraphField());
     }
     else {
@@ -177,90 +147,6 @@ class QAUnstructured extends QABase {
    */
   public static function isQuestion(DOMQuery $query_path) {
     return in_array($query_path->tag(), ['h2', 'h3', 'h4']) && substr($query_path->text(), -1) == '?';
-  }
-
-  /**
-   * Make sure that elements we're using matches what we say we're using.
-   *
-   * Elements are added to questions in getFieldValues() and reported as being
-   * used in isExternalContent(). This checks that the logic matches in both
-   * places so we don't lose or duplicate any content.
-   *
-   * @throws \Drupal\migrate\MigrateException
-   */
-  protected function checkContent() {
-    if (isset(self::$answerQuery) && self::$answerQuery->children()->count() &&
-      isset(self::$externalContent) && self::$externalContent->children()->count()) {
-      if (self::$answerQuery->html() != self::$externalContent->html()) {
-        // Elements were lost.
-        if (self::$answerQuery->children()->count() < self::$externalContent->children()->count()) {
-          for ($i = 0; $i < self::$answerQuery->children()->count(); $i++) {
-            if (self::$answerQuery->children()->get($i) != self::$externalContent->children()->get($i)) {
-              $content = self::$externalContent->children()->get($i)->textContent;
-              break;
-            }
-          }
-          if (empty($content)) {
-            $content = self::$externalContent->children()->get($i)->textContent;
-          }
-
-          Message::make('Content was excluded that wasn\'t part of answer for @question on @title, @url',
-            [
-              '@anomaly_type' => "Q&A - excluded content",
-              '@title' => self::$migrator->row->getSourceProperty('title'),
-              '@url' => self::$migrator->row->getSourceIdValues()['url'],
-              '@question' => self::$lastQuestionText,
-            ], Message::ERROR);
-        }
-        // Elements were duplicated.
-        elseif (self::$answerQuery->children()->count() > self::$externalContent->children()->count()) {
-          for ($i = 0; $i < self::$externalContent->children()->count(); $i++) {
-            if (self::$answerQuery->children()->get($i) != self::$externalContent->children()->get($i)) {
-              $content = self::$answerQuery->children()->get($i)->textContent;
-              break;
-            }
-          }
-          if (empty($content)) {
-            $content = self::$answerQuery->children()->get($i)->textContent;
-          }
-          Message::make('Content wasn\'t excluded that was part of answer on @question: @content',
-            [
-              '@question' => self::$lastQuestionText,
-              '@content' => $content,
-            ], Message::ERROR);
-        }
-        // We have the same number of elements, but they don't all match.
-        else {
-          for ($i = 0; $i < self::$answerQuery->children()->count(); $i++) {
-            if (self::$answerQuery->children()->get($i) != self::$externalContent->children()->get($i)) {
-              $content = self::$answerQuery->children()->get($i)->textContent;
-              break;
-            }
-          }
-          Message::make('Excluded content doesn\'t match answer on @question: @content',
-            [
-              '@question' => self::$lastQuestionText,
-              '@content' => $content,
-            ], Message::ERROR);
-        }
-      }
-    }
-    elseif (isset(self::$answerQuery) && self::$answerQuery->children()->count() &&
-      !(isset(self::$externalContent) && self::$externalContent->children()->count())) {
-      Message::make('No excluded content. Content wasn\'t excluded that was part of answer on @question: @content',
-        [
-          '@question' => self::$lastQuestionText,
-          '@content' => self::$answerQuery->firstChild()->html(),
-        ], Message::ERROR);
-    }
-    elseif (!(isset(self::$answerQuery) && self::$answerQuery->children()->count()) &&
-      isset(self::$externalContent) && self::$externalContent->children()->count()) {
-      Message::make('No answer content. Content was excluded that wasn\'t part of answer on @question: @content',
-        [
-          '@question' => self::$lastQuestionText,
-          '@content' => self::$externalContent->firstChild()->html(),
-        ], Message::ERROR);
-    }
   }
 
   /**
