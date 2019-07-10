525a81f412dd2ea2de1e788c309f01762601ea73
Elijah Lynn <elijah.lynn@agile6.com>
2019-07-09 17:59:40 -0700

    VAGOV-4770 Update Jenkins build trigger URL.

--- docroot/sites/default/settings/settings.stg.php
+++ docroot/sites/default/settings/settings.stg.php
@@ -2,7 +2,9 @@
 
 // @codingStandardsIgnoreFile
 
-$settings['va_jenkins_build_job_url_params'] = $settings['va_jenkins_build_job_dev_staging'] . '/buildWithParameters?cmsEnvBuildOverride=' . $settings['va_jenkins_build_env'];
+$settings['jenkins_build_job_path'] = '/job/builds/job/vets-website-content-vagov' . $settings['jenkins_build_env'];
+$settings['jenkins_build_job_params'] = '/buildWithParameters?deploy=true';
+$settings['jenkins_build_job_url'] = $settings['jenkins_build_job_host'] . $settings['jenkins_build_job_path'] . $settings['jenkins_build_job_params'];
 
 $config['config_split.config_split.dev']['status'] = FALSE;
 $config['config_split.config_split.stg']['status'] = TRUE;
