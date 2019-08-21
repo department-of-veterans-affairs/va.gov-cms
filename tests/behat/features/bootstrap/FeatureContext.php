574ca3adc1ab9388bdc65c7a489b34881d7db56b
Jon Pugh <jon@thinkdrop.net>
2019-08-21 17:05:25 -0400

    VAGOV-5683: Wait a minute this isn't needed AND doesn't work! Remove dotenv stuff. We will solve a different way.

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -1,9 +1,7 @@
 <?php
 use DevShop\Behat\DrupalExtension\Context\DevShopDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Dotenv\Dotenv;
 
-$autoloader = require_once dirname(dirname(dirname(dirname(__DIR__)))) . '/docroot/autoload.php';
 /**
  * Defines application features from the specific context.
  */
@@ -16,19 +14,6 @@ class FeatureContext extends DevShopDrupalContext implements SnippetAcceptingCon
    * context constructor through behat.yml.
    */
   public function __construct() {
-
-    // The .env file is only used in CMS-CI and if a developer wants to override
-    // any environment variables.
-
-    // Add any Lando-specific and team-wide variables to .env.lando.
-
-    // Load the .env file from the root of the project, if there is one.
-    $dotenv_file = dirname(dirname(dirname(dirname(__DIR__)))) . '/.env';
-    if (file_exists($dotenv_file)) {
-      $dotenv = new Dotenv(dirname(dirname(dirname(dirname(__DIR__)))));
-      $dotenv->load();
-    }
-
     parent::__construct();
   }
 }
