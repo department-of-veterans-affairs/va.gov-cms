f242a4da53f254f3b86f228a065a49aee574c83d
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-08-24 11:18:16 -0600

    VAGOV-000: add field to root graphql query to get all menu machine names

--- docroot/modules/custom/va_gov_graphql/src/Plugin/GraphQL/Fields/SiteMenus.php
+++ docroot/modules/custom/va_gov_graphql/src/Plugin/GraphQL/Fields/SiteMenus.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\va_gov_graphql\Plugin\GraphQL\Fields;
 
+use Drupal\graphql\GraphQL\Execution\ResolveContext;
+use Drupal\graphql\Plugin\GraphQL\Fields\FieldPluginBase;
+use GraphQL\Type\Definition\ResolveInfo;
+
 /**
  * Returns a list of the machine names of all the menus on the site.
  *
@@ -13,29 +17,20 @@ namespace Drupal\va_gov_graphql\Plugin\GraphQL\Fields;
  *   multi = true,
  * )
  */
-
-use Drupal\graphql\GraphQL\Execution\ResolveContext;
-use Drupal\graphql\Plugin\GraphQL\Fields\FieldPluginBase;
-use GraphQL\Type\Definition\ResolveInfo;
-
-/**
- * SiteMenus Class Doc Comment.
- *
- * @category Class
- * @package Site Menu
- * @author VA
- */
 class SiteMenus extends FieldPluginBase {
 
   /**
    * {@inheritdoc}
    */
   public function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
-    // Get custom and system menus in Drupal.
     $menus_array = menu_ui_get_menus(TRUE);
 
-    // Return an array of drupal menu machine names.
-    yield 'test';
+    // Returns an array of drupal menu machine names.
+    foreach ($menus_array as $key => $value) {
+      if (strpos($key, 'bene') !== FALSE) {
+        yield $key;
+      }
+    }
   }
 
 }
