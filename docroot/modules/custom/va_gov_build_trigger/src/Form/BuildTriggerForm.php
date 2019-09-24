9e58fdd9087729d939597776b13d97385c662651
Jon Pugh <jon@thinkdrop.net>
2019-09-24 17:41:23 -0400

    VAGOV-6050: Update "Build & Deploy" page: Show the environment to be deployed to. Indicate that "prod" deploy will be handled by VFS Jenkins. Save config showing Build is in progress, show message if so. Change config back when web build is complete.

--- docroot/modules/custom/va_gov_build_trigger/src/Form/BuildTriggerForm.php
+++ docroot/modules/custom/va_gov_build_trigger/src/Form/BuildTriggerForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\va_gov_build_trigger\Form;
 
+use Drupal\Core\Url;
 use Drupal;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -13,8 +14,21 @@ use GuzzleHttp\Client;
 
 /**
  * Implements build trigger form.
+ *
+ * The environment variable CMS_ENVIRONMENT_TYPE is used to determine the URL
+ * displayed.
+ *
+ * You can edit the .env file to change CMS_ENVIRONMENT_TYPE=prod to see what
+ * the site will look like in production.
  */
 class BuildTriggerForm extends FormBase {
+
+  const ENVIRONMENTS = [
+    'pre_prod' => 'https://cms.va.gov',
+    'prod' => 'https://www.va.gov',
+    'staging' => 'https://stg.va.gov',
+    'dev' => 'https://dev.va.gov',
+  ];
 
   /**
    * Build the build trigger form.
@@ -26,12 +40,72 @@ class BuildTriggerForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    $environment_type = getenv('CMS_ENVIRONMENT_TYPE')?: 'ci';
+    $target = !empty(self::ENVIRONMENTS[$environment_type]) ?
+      self::ENVIRONMENTS[$environment_type] :
+      getenv('HTTP_HOST');
+
     $form['actions']['#type'] = 'actions';
+    $form['help_1'] = [
+      '#prefix' => '<p>',
+      '#markup' => t('This is a decoupled Drupal website. Content will not be visible in the front-end website until you run a "rebuild" and deploy it to an environment.'),
+      '#suffix' => '</p>',
+      '#weight' => -10,
+    ];
+
     $form['actions']['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Rebuild VA.gov Front-End'),
-      '#description' => 'Trigger the rebuilding of the front-end of this website using the latest content.',
+      '#value' => $this->t('Rebuild & Deploy Content'),
       '#button_type' => 'primary',
+      '#suffix' => ' ' . t('to %site', [
+        '%site' => $target,
+      ]),
+    ];
+
+    $form['environment_type'] = [
+      '#type' => 'value',
+      '#value' => $environment_type,
+    ];
+
+    $form['environment'] = [
+      '#type' => 'value',
+      '#value' => $target,
+    ];
+
+    // Save pending state.
+    $config = \Drupal::service('config.factory')->getEditable('va_gov.build');
+    if ($config->get('web.build.pending', 0)) {
+      drupal_set_message(t('Web Rebuild & Deploy is in progress: You must wait for it to complete before triggering another Rebuild & Deploy.'));
+
+      if ($environment_type == 'lando') {
+        drupal_set_message(t('You are using Lando. Run the command <code>lando composer va:web:build</code> to rebuild the front-end and unlock this form.'), 'warning');
+      }
+
+      $form['actions']['submit']['#disabled'] = TRUE;
+      $form['actions']['submit']['#suffix'] .= ' ' . t('disabled');
+
+      $form['tip']['#prefix'] = '<em>';
+      $form['tip']['#markup'] = t('NOTE: You can run the command <code> drush config-set va_gov.build web.build.pending 0</code> to break this lock.');
+      $form['tip']['#suffix'] = '</em>';
+      $form['tip']['#weight'] = 100;
+    }
+
+    if ($environment_type == 'prod') {
+      $description = t('Rebuilds for this environment will be handled by VFS Jenkins.');
+    }
+    else {
+      $description = t('Rebuilds for this environment are handled internally.');
+    }
+
+    $form['environment_target'] = [
+      '#type' => 'item',
+      '#title' => t('Environment Target'),
+      '#markup' => Drupal::l($target, Url::fromUri('http://' . $target), [
+        'attributes' => [
+          'target' => '_blank',
+        ],
+      ]),
+      '#description' => $description,
     ];
     return $form;
   }
@@ -59,21 +133,18 @@ class BuildTriggerForm extends FormBase {
    *   Object containing current form state.
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
     // If running in CMS-CI and DevShopTaskApiClient has been loaded, use it.
-    if (!empty($_SERVER['DEVSHOP_ENVIRONMENT']) && class_exists('DevShopTaskApiClient')) {
+    if ($form_state->getValue('environment_type') == 'ci' && class_exists('DevShopTaskApiClient')) {
       $task_json = DevShopTaskApiClient::create('vabuild');
       $task = json_decode($task_json);
       if (!empty($task->nid)) {
-        drupal_set_message(t('VA Web Rebuild has been queued.'));
-      }
-      else {
-        drupal_set_message($task_json);
+        drupal_set_message(t('VA Web Rebuild & Deploy has been queued. The process should complete in around 1 minute. !link', [
+          '!link' => l(t('Deploy Log'), 'http://' . $_SERVER['DEVSHOP_HOSTNAME'] . '/node/' . $task->nid),
+        ]));
       }
     }
-    elseif (!empty($_SERVER['LANDO'])) {
-      // If LANDO: Show command to run.
-      drupal_set_message(t('VA Web Rebuild has been queued. Run composer va:web:build to trigger it.'));
+    elseif ($form_state->getValue('environment_type') == 'lando') {
+      // Do nothing if using Lando. The form will display instructions.
     }
     else {
 
@@ -92,94 +163,100 @@ class BuildTriggerForm extends FormBase {
           ->addMessage(t('You cannot trigger a build in this environment. Only the DEV, STAGING and PROD environments support triggering builds.'), 'warning');
         return FALSE;
       }
-    }
 
-    try {
-      // Setup the REQUEST options.
-      $request_options = [
-        'verify' => TRUE,
-        'body' => '',
-        'headers' => [
-          'Accept' => 'text/plain',
-        ],
-        'curl' => [
-          CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
-          CURLOPT_VERBOSE => TRUE,
-          CURLOPT_URL => $jenkins_build_job_url,
-          CURLOPT_POST => 1,
-          // Authorize to the Jenkins API via GitHub login.
-          CURLOPT_USERNAME => $va_cms_bot_github_username,
-          CURLOPT_PASSWORD => $va_cms_bot_github_auth_token,
-          CURLOPT_FOLLOWLOCATION => 1,
-          CURLOPT_RETURNTRANSFER => 1,
-          CURLOPT_HEADER => 1,
-        ],
-      ];
-      // Setup the REQUEST retry logic w/lazy backoff.
-      // @see http://dawehner.github.io/php,/guzzle/2017/05/19/guzzle-retry.html
-      $handler_stack = HandlerStack::create();
-      $handler_stack->push(Middleware::retry(function ($retry, $request, $response, $reason) {
-        // Must be a "201 Created" response code & message, if not then continue
-        // and retry.
-        if ($response && $response->getStatusCode() === 201) {
-          return FALSE;
-        }
-        FormBase::logger('va_gov_build_trigger')
-          ->warning('Retry site build - attempt #' . $retry);
-        // Stop after 3 retries.
-        return $retry < 3;
-      }, function ($retry) {
-        $delay_ms = 1000;
-        return $retry * $delay_ms;
-      }));
+      try {
+        // Setup the REQUEST options.
+        $request_options = [
+          'verify' => TRUE,
+          'body' => '',
+          'headers' => [
+            'Accept' => 'text/plain',
+          ],
+          'curl' => [
+            CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
+            CURLOPT_VERBOSE => TRUE,
+            CURLOPT_URL => $jenkins_build_job_url,
+            CURLOPT_POST => 1,
+            // Authorize to the Jenkins API via GitHub login.
+            CURLOPT_USERNAME => $va_cms_bot_github_username,
+            CURLOPT_PASSWORD => $va_cms_bot_github_auth_token,
+            CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HEADER => 1,
+          ],
+        ];
+        // Setup the REQUEST retry logic w/lazy backoff.
+        // @see http://dawehner.github.io/php,/guzzle/2017/05/19/guzzle-retry.html
+        $handler_stack = HandlerStack::create();
+        $handler_stack->push(Middleware::retry(function ($retry, $request, $response, $reason) {
+          // Must be a "201 Created" response code & message, if not then cont
+          // and retry.
+          if ($response && $response->getStatusCode() === 201) {
+            return FALSE;
+          }
+          FormBase::logger('va_gov_build_trigger')
+            ->warning('Retry site build - attempt #' . $retry);
+          // Stop after 3 retries.
+          return $retry < 3;
+        }, function ($retry) {
+          $delay_ms = 1000;
+          return $retry * $delay_ms;
+        }));
 
-      // Handle the RESPONSE.
-      $client = new Client(['handler' => $handler_stack]);
-      $response = $client->post($jenkins_build_job_host, $request_options);
+        // Handle the RESPONSE.
+        $client = new Client(['handler' => $handler_stack]);
+        $response = $client->post($jenkins_build_job_host, $request_options);
 
-      if ($response->getStatusCode() !== 201) {
-        FormBase::logger('va_gov_build_trigger')
-          ->error('Site rebuild failed with status code {:status_code} {:reason_phrase} and URL {:url}.',
+        if ($response->getStatusCode() !== 201) {
+          FormBase::logger('va_gov_build_trigger')
+            ->error('Site rebuild failed with status code {:status_code} {:reason_phrase} and URL {:url}.',
               [
                 ':status_code' => $response->getStatusCode(),
                 ':reason_phrase' => $response->getReasonPhrase(),
                 ':url' => $jenkins_build_job_url,
               ]
-          );
-        Drupal::messenger()
-          ->addMessage(t('Site rebuild request has failed for :url, check log for more information.', [':url' => $jenkins_build_job_url]), 'error');
+            );
+          Drupal::messenger()
+            ->addMessage(t('Site rebuild request has failed for :url, check log for more information.', [':url' => $jenkins_build_job_url]), 'error');
+        }
+        else {
+          // Get our SQL formatted date.
+          $time_raw = Drupal::service('date.formatter')
+            ->format(time(), 'html_datetime', '', 'UTC');
+          $time = strtok($time_raw, '+');
+
+          // We only need to update field table - field is set on node import.
+          $query = Drupal::database()
+            ->update('node__field_page_last_built')
+            ->fields(['field_page_last_built_value' => $time]);
+          $query->execute();
+
+          // We only need to update - revision field is set on node import.
+          $query_revision = Drupal::database()
+            ->update('node_revision__field_page_last_built')
+            ->fields(['field_page_last_built_value' => $time]);
+          $query_revision->execute();
+
+          Drupal::messenger()
+            ->addMessage(t('Site rebuild request has been triggered with :url. Please visit <a href="@job_link">@job_link</a> to see status.', [
+              ':url' => $jenkins_build_job_url,
+              '@job_link' => $jenkins_build_job_host . $jenkins_build_job_path,
+            ]), 'status');
+        }
       }
-      else {
-        // Get our SQL formatted date.
-        $time_raw = Drupal::service('date.formatter')
-          ->format(time(), 'html_datetime', '', 'UTC');
-        $time = strtok($time_raw, '+');
-
-        // We only need to update field table - field is set on node import.
-        $query = Drupal::database()
-          ->update('node__field_page_last_built')
-          ->fields(['field_page_last_built_value' => $time]);
-        $query->execute();
-
-        // We only need to update - revision field is set on node import.
-        $query_revision = Drupal::database()
-          ->update('node_revision__field_page_last_built')
-          ->fields(['field_page_last_built_value' => $time]);
-        $query_revision->execute();
-
+      catch (RequestException $exception) {
         Drupal::messenger()
-          ->addMessage(t('Site rebuild request has been triggered with :url. Please visit <a href="@job_link">@job_link</a> to see status.', [
-            ':url' => $jenkins_build_job_url,
-            '@job_link' => $jenkins_build_job_host . $jenkins_build_job_path,
-          ]), 'status');
+          ->addMessage(t('Site rebuild request has failed for :url with an Exception, check log for more information. If this is the PROD environment please notify in #cms-engineering Slack and please email vacmssupport@va.gov immediately with the error message you see here.', [':url' => $jenkins_build_job_url]), 'error');
+        watchdog_exception('va_gov_build_trigger', $exception);
+
       }
     }
-    catch (RequestException $exception) {
-      Drupal::messenger()
-        ->addMessage(t('Site rebuild request has failed for :url with an Exception, check log for more information. If this is the PROD environment please notify in #cms-engineering Slack and please email vacmssupport@va.gov immediately with the error message you see here.', [':url' => $jenkins_build_job_url]), 'error');
-      watchdog_exception('va_gov_build_trigger', $exception);
 
-    }
+    // Save pending state.
+    $config = \Drupal::service('config.factory')->getEditable('va_gov.build');
+    $config->set('web.build.pending', 1);
+    $config->save();
+
   }
 
 }
