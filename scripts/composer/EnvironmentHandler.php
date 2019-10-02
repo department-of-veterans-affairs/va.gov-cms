e893a2f871ea17d2244c4755959a3845f97db555
Jon Pugh <jon@thinkdrop.net>
2019-10-01 17:08:13 -0700

    VAGOV-6106: Move GraphQL test to PHP method, ensure 200 code and throw exception if empty response.

--- scripts/composer/EnvironmentHandler.php
+++ scripts/composer/EnvironmentHandler.php
@@ -5,6 +5,7 @@ namespace VA\Composer;
 use Acquia\Lightning\Composer\Package;
 use Dotenv\Dotenv;
 use Dotenv\Exception\ValidationException;
+use Symfony\Component\Process\Process;
 
 // Run here so it is loaded as soon as the file is included.
 EnvironmentHandler::load();
@@ -55,6 +56,45 @@ class EnvironmentHandler {
     }
     catch (\Exception $exception) {
       throw $exception;
+    }
+  }
+
+  /**
+   * Uses curl to confirm that GraphQL endpoint is returning 200 AND is not empty.
+   *
+   * This test was created to debug a situation where the GraphQL endpoint will
+   * fail after content is edited (found during behat testing).
+   *
+   * More details in the issue below.
+   *
+   * This will be a good regression test. GraphQL should respond with a non OK
+   * HTTP code if there is no content or something wrong.
+   *
+   * We will use this test to reproduce the bug and perhaps create a workaround, because it will work if you CURL it enough times!
+   *
+   * A cache rebuild will reset to the error state.
+   *
+   * @see decoupled.feature
+   * @see tests.yml
+   * @see https://va-gov.atlassian.net/browse/VAGOV-6070
+   *
+   * @todo Move to a different handler class to keep things organized.
+   */
+  static function graphQlTest() {
+
+    // Use DRUPAL_ADDRESS to determine URL
+    $graphql_url = getenv('DRUPAL_ADDRESS') . '/graphql';
+    $curl_command = "curl --silent --fail --user api:drupal8 --request POST --data-binary '@./scripts/graphql-curl-test-post-body.txt' {$graphql_url}";
+
+    $process = new Process($curl_command);
+    $process->mustRun();
+
+    $output = $process->getOutput();
+    if (empty($output)) {
+        throw new \Exception("Curl request to $graphql_url succeeded, but GraphQL Output is 0 Bytes! $output. \n Command: " . $process->getCommandLine());
+   }
+    else {
+      print "GraphQL Endpoint returned successfully.";
     }
   }
 }
