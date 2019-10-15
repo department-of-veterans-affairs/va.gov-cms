37c0514bc34bcbb537c2d069d805a9ef2cd01eac
Ethan Teague <metrowebz@gmail.com>
2019-10-15 12:29:41 -0400

    VAGOV-5333: Changing endpoint to key required.

--- docroot/modules/custom/va_gov_consumers/src/FacilityConsumer.php
+++ docroot/modules/custom/va_gov_consumers/src/FacilityConsumer.php
@@ -18,25 +18,30 @@ class FacilityConsumer {
    *   The facility id.
    */
   public function contentRender($facility_id) {
+    if (!empty(getenv('FACILITY_API'))) {
+      $client = new Client();
+      // Make sure we get a good response before heavy lifting.
+      try {
+        $data_source = 'https://dev-api.va.gov/services/va_facilities/v0/facilities/' . $facility_id;
+        $response = $client->get($data_source, [
+          'headers' => [
+            'apikey' => getenv('CMS_FACILITY_API'),
+          ],
+        ]);
 
-    $client = new Client();
-    // Make sure we get a good response before heavy lifting.
-    try {
-      $data_source = 'https://api.va.gov/v0/facilities/va/' . $facility_id;
-      $response = $client->get($data_source);
+        if ($response->getStatusCode() === 200) {
+          $data = Json::decode($response->getBody());
+        }
 
-      if ($response->getStatusCode() === 200) {
-        $data = Json::decode($response->getBody());
+      }
+      // Record any trouble to watchdog.
+      catch (GuzzleException $e) {
+        watchdog_exception('Facility_content', $e->getMessage());
       }
 
-    }
-    // Record any trouble to watchdog.
-    catch (GuzzleException $e) {
-      watchdog_exception('Facility_content', $e->getMessage());
-    }
+      return $data;
 
-    return $data;
-
+    }
   }
 
 }
