46a10a442553af9cfbe4850565d26d1fb085d7e3
Kevin Walsh <kevin.walsh@civicactions.com>
2019-10-30 00:04:40 -0400

    VAGOV-000: Basic gulp setup for sass compiling.

---
+++ docroot/themes/custom/vagovadmin/gulp-tasks/pattern-lab.js
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * Task: Generate PatternLab, Watch PatternLab.
+ */
+
+module.exports = function(gulp, options, plugins) {
+  'use strict';
+
+  // Start the Patternlab server and watch for changes
+  gulp.task('patternlab:watch', function() {
+    var options = {
+      continueOnError: false, // default = false, true means don't emit error event
+      pipeStdout: false // default = false, true means stdout is written to file.contents
+    };
+    var reportOptions = {
+      err: true, // default = true, false means don't write err
+      stderr: true, // default = true, false means don't write stderr
+      stdout: true // default = true, false means don't write stdout
+    };
+    console.log('server started on http://localhost:8080 - use ctrl+c to exit...');
+    return gulp.src('./pattern-lab')
+      .pipe(plugins.exec('cd <%= file.path %> && php core/console --server --with-watch', options))
+      .pipe(plugins.exec.reporter(reportOptions));
+  });
+
+  // Export Pattern lab
+  gulp.task('patternlab:generate', function() {
+    var options = {
+      continueOnError: false, // default = false, true means don't emit error event
+      pipeStdout: false // default = false, true means stdout is written to file.contents
+    };
+    var reportOptions = {
+      err: true, // default = true, false means don't write err
+      stderr: true, // default = true, false means don't write stderr
+      stdout: true // default = true, false means don't write stdout
+    };
+    return gulp.src('./pattern-lab')
+      .pipe(plugins.exec('cd <%= file.path %> && php core/console --generate', options))
+      .pipe(plugins.exec.reporter(reportOptions));
+  });
+};
