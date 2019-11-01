03ddbcf9d89ad49b9a52193c2199b2e57f2e4bb1
Kevin Walsh <kevin.walsh@civicactions.com>
2019-11-01 12:57:54 -0400

    VAGOV-6185: Sidebars for Drupal node view pages with menus and metadata. (#667)
    * VAGOV-6190: Create 3 column layout for admin theme.
    
    * VAGOV-6190: Move preview button to right sidebar.
    
    * VAGOV-6190: Create view about current content, and place in right sidebar.
    
    * VAGOV-6190: Remove meta and owner fields now displayed in right sidebar.
    
    * VAGOV-6185: Place menu blocks in left sidebar.
    
    * VAGOV-000: Minor readability improvement for node view pages.
    
    * VAGOV-6185: Views behat.
    
    * VAGOV-000: Squash with view.
    
    * VAGOV-000: Basic gulp setup for sass compiling.
    
    * VAGOV-000: Refactor CSS to SASS.
    
    * VAGOV-000: Owner field should be required for all node types.
    
    * VAGOV-6185: Fix view that was causing twig-related php errors.
    
    * VAGOV-000: Picking away at those dated field labels and help text.
    
    * VAGOV-6185: Add metatag fields back to node display, for now.
    
    * VAGOV-000: Update drupal spec content model test.
    
    * VAGOV-000: Update patch for path_redirect_import to match latest dev.

---
+++ docroot/themes/custom/vagovadmin/gulp-tasks/build.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Task: Build task for running frontend build. Also used to compile sass & PL without watching
+ * Usage: gulp build
+ */
+
+module.exports = function (gulp, plugins, options) {
+  'use strict';
+
+  // Frontend build
+  gulp.task('build', [
+    'js-lint',
+    'sass-lint',
+    'sass-for-build',
+    'patternlab:generate'
+  ]);
+
+};
---
+++ docroot/themes/custom/vagovadmin/gulp-tasks/watch.js
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * Task: Lint & Watch Sass.
+ * Usage: gulp watch
+ */
+
+module.exports = function(gulp, options, plugins) {
+  'use strict';
+
+  function lintFile(file) {
+    gulp.src(file)
+      .pipe(plugins.sassLint())
+      .pipe(plugins.sassLint.format());
+  }
+
+  // Keep an eye on Sass files for changes and only lint changed files
+  gulp.task('watch', function() {
+    gulp.watch([
+      options.sass.sassFiles,
+      options.sass.plFiles
+    ], function(ev) {
+      if (ev.type === 'added' || ev.type === 'changed') {
+        lintFile(ev.path);
+      }
+    });
+    // Compile sass changes
+    gulp.watch([
+        options.sass.sassFiles,
+        options.sass.plFiles],
+      ['sass']);
+  });
+};
