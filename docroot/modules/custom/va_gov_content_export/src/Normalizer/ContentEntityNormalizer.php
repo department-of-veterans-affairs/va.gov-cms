01f59c0ad9f8cf0dbe51ef38bbe4c1e8c00211bf
Elijah Lynn <elijah.lynn@agile6.com>
2020-01-24 18:18:02 -0800

    Add nid,vid and id (in some cases) to tome_sync exports.

--- docroot/modules/custom/va_gov_content_export/src/Normalizer/ContentEntityNormalizer.php
+++ docroot/modules/custom/va_gov_content_export/src/Normalizer/ContentEntityNormalizer.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * @file Overrides to tome_sync's ContentEntityNormalizer.
+ */
 
 namespace Drupal\va_gov_content_export\Normalizer;
 
+use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\tome_sync\Normalizer\ContentEntityNormalizer as TomeSyncContentEntityNormalizer;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Drupal\serialization\Normalizer\ContentEntityNormalizer as BaseContentEntityNormalizer;
 
 /**
  * Normalizes/denormalizes Drupal content entities into an array structure.
@@ -18,8 +24,37 @@ class ContentEntityNormalizer extends TomeSyncContentEntityNormalizer {
    *
    * @var array
    */
-  protected $fieldDenyList = [
+  protected $fieldDenyList = [];
     // Add metatags back in. Leave this commented out.
 //    'metatag',
-  ];
+//  ];
+
+  public function __construct(EntityManagerInterface $entity_manager, DenormalizerInterface $inner_service) {
+    $this->innerService = $inner_service;
+    parent::__construct($entity_manager);
+  }
+
+  /**
+   * Can't use `parent::parent` in PHP.
+   *
+   * @see https://stackoverflow.com/a/8212262/292408
+   *
+   * @param $entity
+   * @param $format
+   * @param $context
+   *
+   * @return array|bool|float|int|string|null
+   */
+  public function baseNormalize($entity, $format, $context) {
+    return BaseContentEntityNormalizer::normalize($entity, $format, $context);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function normalize($entity, $format = NULL, array $context = []) {
+    $values = $this->baseNormalize($entity, $format, $context);
+    // Empty, to unnormalize Tome as we want the entity IDs.
+    return $values;
+  }
 }
