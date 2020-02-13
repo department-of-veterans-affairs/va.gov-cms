ab7df0cdf22ad6ff41223f429871cc4396becfc4
indytechcook <neil.hastings@gmail.com>
2020-02-13 16:52:26 -0500

    VAGOV-6194: Clean up use of TOME serializer in the service

--- docroot/modules/custom/va_gov_content_export/src/Normalizer/ContentEntityNormalizer.php
+++ docroot/modules/custom/va_gov_content_export/src/Normalizer/ContentEntityNormalizer.php
@@ -4,41 +4,19 @@ namespace Drupal\va_gov_content_export\Normalizer;
 
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityManagerInterface;
-use Drupal\node\Entity\Node;
-use Drupal\tome_sync\Normalizer\ContentEntityNormalizer as TomeSyncContentEntityNormalizer;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Drupal\serialization\Normalizer\ContentEntityNormalizer as BaseContentEntityNormalizer;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * Normalizes/denormalizes Drupal content entities into an array structure.
  *
  * @internal
  */
-class ContentEntityNormalizer extends TomeSyncContentEntityNormalizer {
-
-  /**
-   * Field names that should be excluded from normalization.
-   *
-   * Should only be used when more generic logic cannot be used.
-   *
-   * @var array
-   */
-  protected $fieldDenyList = [
-    // Add metatags back in. Leave this commented out.
-    // 'metatag',
-  ];
-
-  /**
-   * @var NormalizerInterface
-   */
-  protected $innerService;
+class ContentEntityNormalizer extends BaseContentEntityNormalizer {
 
   /**
    * ContentEntityNormalizer constructor.
    */
-  public function __construct(EntityManagerInterface $entity_manager, NormalizerInterface $inner_service) {
-    $this->innerService = $inner_service;
+  public function __construct(EntityManagerInterface $entity_manager) {
     parent::__construct($entity_manager);
    }
 
@@ -46,7 +24,7 @@ class ContentEntityNormalizer extends TomeSyncContentEntityNormalizer {
    * @inheritDoc
    */
   public function normalize($entity, $format = NULL, array $context = []) {
-    $values = $this->innerService->normalize($entity, $format, $context);
+    $values = parent::normalize($entity, $format, $context);
 
     // Empty, to unnormalize Tome as we want the entity IDs.
     $breadcrumb_values = $this->getBreadCrumbsForValues($entity);
