ee06b904985494876e975f62b4b4a3bb2e08a014
indytechcook <neil.hastings@gmail.com>
2020-02-21 08:32:12 -0500

    Fix phpcs failures

--- docroot/modules/custom/va_gov_content_export/src/TomeExporter.php
+++ docroot/modules/custom/va_gov_content_export/src/TomeExporter.php
@@ -1,11 +1,14 @@
 <?php
 
-
 namespace Drupal\va_gov_content_export;
-
 
 use Drupal\tome_sync\Exporter;
 
+/**
+ * Exporter class for Tome.
+ *
+ * Overridden to exclude more types of entities.
+ */
 class TomeExporter extends Exporter {
   /**
    * An array of excluded entity types.
@@ -18,4 +21,5 @@ class TomeExporter extends Exporter {
     'user_role',
     'user_history',
   ];
+
 }
--- docroot/modules/custom/va_gov_content_export/src/VaGovContentExportServiceProvider.php
+++ docroot/modules/custom/va_gov_content_export/src/VaGovContentExportServiceProvider.php
@@ -1,14 +1,18 @@
 <?php
 
-
 namespace Drupal\va_gov_content_export;
-
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 
+/**
+ * ServiceProvider.
+ */
 class VaGovContentExportServiceProvider extends ServiceProviderBase {
 
+  /**
+   * {@inheritdoc}
+   */
   public function alter(ContainerBuilder $container) {
     $container->removeDefinition('tome_sync.config_event_subscriber');
   }
