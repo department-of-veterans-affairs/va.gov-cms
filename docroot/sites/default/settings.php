9aa2d27e0e296c38c373ee0b654b2b7b168dd217
indytechcook <neil.hastings@gmail.com>
2020-03-04 16:55:59 -0500

    Update tome sync config

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -164,5 +164,5 @@ if (file_exists($app_root . '/' . $site_path . '/settings/settings.local.php')) 
   include $app_root . '/' . $site_path . '/settings/settings.local.php';
 }
 
-$settings['tome_content_directory'] =  "{$settings['file_public_path']}/content-export";
-$settings['tome_files_directory'] =  "{$settings['file_public_path']}/files-export";
+$settings['tome_content_directory'] = isset($settings['file_public_path']) ? "{$settings['file_public_path']}/export-content" : 'sites/default/files/export-content';
+$settings['tome_files_directory'] = isset($settings['file_public_path']) ? "{$settings['file_public_path']}/export-files" : 'sites/default/files/export-files';
