75a40776de03571ab2c2f41a0647d85227fdfe66
indytechcook <neil.hastings@gmail.com>
2020-03-05 14:31:17 -0500

    Fix phpcs

--- docroot/modules/custom/va_gov_content_export/src/TomeExporter.php
+++ docroot/modules/custom/va_gov_content_export/src/TomeExporter.php
@@ -23,23 +23,4 @@ class TomeExporter extends Exporter {
     'user_history',
   ];
 
-  /**
-   * Acquires a lock for writing to the index.
-   *
-   * @return resource
-   *   A file pointer resource on success.
-   *
-   * @throws \Exception
-   *   Throws an exception when the index file cannot be written to.
-   */
-  protected function acquireContentIndexLock() {
-    $destination = $this->getContentIndexFilePath();
-    $directory = dirname($destination);
-    @mkdir($directory);
-    $handle = fopen($destination, 'c+');
-    if (!flock($handle, LOCK_EX)) {
-      throw new \Exception('Unable to acquire lock for the index file.');
-    }
-    return $handle;
-  }
 }
