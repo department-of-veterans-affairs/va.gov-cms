be656075dab7c859c572b637b58c4cd6b9792e31
indytechcook <neil.hastings@gmail.com>
2020-03-10 13:20:40 -0400

    Override Tomes FileSync to allow htaccess to be read only

---
+++ docroot/modules/custom/va_gov_content_export/src/TomeFileSync.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Drupal\va_gov_content_export;
+
+
+use Drupal\Core\Config\StorageException;
+use Drupal\tome_sync\FileSync;
+
+/**
+ * Overriding FileSync to allow for .htaccess file to not be private.
+ *
+ * The CI system does not delete this file correctly due to read only
+ * permissions so we override the protected nature to allow it to be deleted.
+ */
+class TomeFileSync extends FileSync {
+
+  /**
+   * Ensures that the file directory exists.
+   */
+  protected function ensureFileDirectory() {
+    $file_directory = $this->getFileDirectory();
+    file_prepare_directory($file_directory, FILE_CREATE_DIRECTORY);
+    // Here is the overridden line of code.  Added FALSE.
+    file_save_htaccess($file_directory, FALSE);
+    if (!file_exists($file_directory)) {
+      throw new StorageException('Failed to create config directory ' . $file_directory);
+    }
+  }
+
+}
