f3c0add11e23e5ce2d1057463d80419ba4839979
Ethan Teague <metrowebz@gmail.com>
2020-03-20 12:22:57 -0400

    VACMS-5673: Adding onLoad handler.

--- docroot/modules/custom/va_gov_backend/js/gtm_tag_trackers.js
+++ docroot/modules/custom/va_gov_backend/js/gtm_tag_trackers.js
@@ -1,26 +1,28 @@
 (function ($, Drupal) {
   Drupal.behaviors.vaGovTagTracker = {
     attach: function (context, settings) {
+      // Build our gtm settings push object.
+      const dataCollection = {
+        'pagePath': settings.gtm_data.pagePath ? settings.gtm_data.pagePath : null,
+        'pageTitle': settings.gtm_data.pageTitle ? settings.gtm_data.pageTitle : null,
+        'firstSectionLevel': settings.gtm_data.firstSectionLevel ? settings.gtm_data.firstSectionLevel : null,
+        'secondSectionLevel': settings.gtm_data.secondSectionLevel ? settings.gtm_data.secondSectionLevel : null,
+        'thirdSectionLevel': settings.gtm_data.thirdSectionLevel ? settings.gtm_data.thirdSectionLevel : null,
+        'fourthSectionLevel': settings.gtm_data.fourthSectionLevel ? settings.gtm_data.foourthSectionLevel : null,
+        'nodeID': settings.gtm_data.nodeID ? settings.gtm_data.nodeID : null,
+        'contentTitle': settings.gtm_data.contentTitle ? settings.gtm_data.contentTitle : null,
+        'contentType': settings.gtm_data.contentType ? settings.gtm_data.contentType : null,
+        'contentOwner': settings.gtm_data.contentOwner ? settings.gtm_data.contentOwner : null,
+      }
+
       // For processing our click events.
-      pushGTM = (selector, event) => {
-        // Build our gtm settings push object.
-        const dataCollection = {
-          'pagePath': settings.gtm_data.pagePath ? settings.gtm_data.pagePath : null,
-          'pageTitle': settings.gtm_data.pageTitle ? settings.gtm_data.pageTitle : null,
-          'firstSectionLevel': settings.gtm_data.firstSectionLevel ? settings.gtm_data.firstSectionLevel : null,
-          'secondSectionLevel': settings.gtm_data.secondSectionLevel ? settings.gtm_data.secondSectionLevel : null,
-          'thirdSectionLevel': settings.gtm_data.thirdSectionLevel ? settings.gtm_data.thirdSectionLevel : null,
-          'fourthSectionLevel': settings.gtm_data.fourthSectionLevel ? settings.gtm_data.foourthSectionLevel : null,
-          'nodeID': settings.gtm_data.nodeID ? settings.gtm_data.nodeID : null,
-          'contentTitle': settings.gtm_data.contentTitle ? settings.gtm_data.contentTitle : null,
-          'contentType': settings.gtm_data.contentType ? settings.gtm_data.contentType : null,
-          'contentOwner': settings.gtm_data.contentOwner ? settings.gtm_data.contentOwner : null,
-        }
+      const pushGTM = (selector, event) => {
         // Push cms data into dataLayer.
         selector.forEach(el => {
+          // Using jQuery begrudgingly. ES6 doesn't have a nice once() method.
           $(el, context).once().click(function (e) {
+            dataCollection['event'] = event;
             dataLayer.push(dataCollection);
-            dataLayer.push({ 'event': event });
           });
         });
       }
@@ -36,7 +38,18 @@
           event: 'lower-level-nav'
         },
         {
+          // Edit tab on node page.
           selector: document.querySelectorAll('li.tabs__tab a[rel="edit-form"]'),
+          event: 'content-edit'
+        },
+        {
+          // Edit button on content view page.
+          selector: document.querySelectorAll('.views-field-edit-node a.button'),
+          event: 'content-edit'
+        },
+        {
+          // Edit button on bulk content view page.
+          selector: document.querySelectorAll('.views-field-operations li.dropbutton-action a'),
           event: 'content-edit'
         },
         {
@@ -57,10 +70,19 @@
         },
       ];
 
-      // Send it off to GTM.
+      // Send it off to GTM on click.
       targets.forEach(e => {
         pushGTM(e.selector, e.event);
       });
+
+      // Send data to GTM onLoad.
+      const gtmPageLoadPush = () => {
+        dataCollection['event'] = 'pageLoad';
+        dataLayer.push(dataCollection);
+      }
+
+      window.addEventListener('load', gtmPageLoadPush);
+
     }
   };
 
