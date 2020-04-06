4dcd79ef18d9501cc85d123fb90bee0ce1ab5b3c
Neil Hastings <neil.hastings@agile6.com>
2020-04-06 09:41:32 -0400

    Updates to create the directories correctly in tome sync

--- docroot/modules/custom/va_gov_content_export/src/TomeExporter.php
+++ docroot/modules/custom/va_gov_content_export/src/TomeExporter.php
@@ -2,7 +2,14 @@
 
 namespace Drupal\va_gov_content_export;
 
+use Drupal\Core\Config\StorageInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\Session\AccountSwitcherInterface;
 use Drupal\tome_sync\Exporter;
+use Drupal\tome_sync\FileSyncInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Exporter class for Tome.
@@ -10,6 +17,13 @@ use Drupal\tome_sync\Exporter;
  * Overridden to exclude more types of entities.
  */
 class TomeExporter extends Exporter {
+
+  /**
+   * File System.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
 
   /**
    * An array of excluded entity types.
@@ -24,6 +38,41 @@ class TomeExporter extends Exporter {
   ];
 
   /**
+   * Creates an Exporter object.
+   *
+   * This was overridden to allow the file system to be injected.
+   *
+   * @param \Drupal\Core\Config\StorageInterface $content_storage
+   *   The target content storage.
+   * @param \Symfony\Component\Serializer\Serializer $serializer
+   *   The serializer.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
+   *   The event dispatcher.
+   * @param \Drupal\Core\Session\AccountSwitcherInterface $account_switcher
+   *   The account switcher.
+   * @param \Drupal\tome_sync\FileSyncInterface $file_sync
+   *   The file sync service.
+   * @param \Drupal\Core\File\FileSystemInterface $file_system
+   *   The file system interface.
+   */
+  public function __construct(
+    StorageInterface $content_storage,
+    Serializer $serializer,
+    EntityTypeManagerInterface $entity_type_manager,
+    EventDispatcherInterface $event_dispatcher,
+    AccountSwitcherInterface $account_switcher,
+    FileSyncInterface $file_sync,
+    FileSystemInterface $file_system
+  ) {
+    parent::__construct($content_storage, $serializer, $entity_type_manager,
+      $event_dispatcher, $account_switcher, $file_sync);
+
+    $this->fileSystem = $file_system;
+  }
+
+  /**
    * Acquires a lock for writing to the index.
    *
    * @return resource
@@ -31,14 +80,14 @@ class TomeExporter extends Exporter {
    *
    * @throws \Exception
    *   Throws an exception when the index file cannot be written to.
+   *
+   * @TODO rework this error logic since this can cause a node not to save.
    */
   protected function acquireContentIndexLock() {
     $destination = $this->getContentIndexFilePath();
     $directory = dirname($destination);
-    // Overridding how the directory is being created because we want to
-    // use the default parent directory permission
-    // for devshop tests to work correctly.
-    @mkdir($directory);
+    // Overridden to allow the drupal file system to create the directory.
+    $this->fileSystem->prepareDirectory($directory);
     $handle = fopen($destination, 'c+');
     if (!flock($handle, LOCK_EX)) {
       throw new \Exception('Unable to acquire lock for the index file.');
