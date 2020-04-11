3788efe5b916abe75544a193dbfa2544dfc945be
Steve Wirt <swirtMiles@138230.no-reply.drupal.org>
2020-04-10 23:56:54 -0400

    Add charcter transform to get rid of PDO breaking characters.

--- docroot/modules/custom/va_gov_migrate/src/Plugin/migrate_plus/data_parser/Csv.php
+++ docroot/modules/custom/va_gov_migrate/src/Plugin/migrate_plus/data_parser/Csv.php
@@ -62,7 +62,63 @@ class Csv extends DataParserPluginBase implements ContainerFactoryPluginInterfac
     $csv = trim($csv, " ");
     $csv .= "\n";
 
+    $this->swapBreakingCharacters($csv);
+
     return $csv;
+  }
+
+  /**
+   * Remove common characters that often lead to PDO exception.
+   */
+  private function swapBreakingCharacters(&$csv) {
+    // Map based on:
+    // https://www.php.net/manual/en/function.mb-convert-encoding.php
+    // http://www.htmlentities.com/html/entities/
+    $map = [
+      chr(0x8A) => chr(0xA9),
+      chr(0x8C) => chr(0xA6),
+      chr(0x8D) => chr(0xAB),
+      chr(0x8E) => chr(0xAE),
+      chr(0x8F) => chr(0xAC),
+      chr(0x9C) => chr(0xB6),
+      chr(0x9D) => chr(0xBB),
+      chr(0xA1) => chr(0xB7),
+      chr(0xA5) => chr(0xA1),
+      chr(0xBC) => chr(0xA5),
+      chr(0x9F) => chr(0xBC),
+      chr(0xB9) => chr(0xB1),
+      chr(0x9A) => chr(0xB9),
+      chr(0xBE) => chr(0xB5),
+      chr(0x9E) => chr(0xBE),
+      chr(0x80) => '&euro;',
+      chr(0x82) => '&sbquo;',
+      chr(0x84) => '&bdquo;',
+      chr(0x85) => '&hellip;',
+      chr(0x86) => '&dagger;',
+      chr(0x87) => '&Dagger;',
+      chr(0x89) => '&permil;',
+      chr(0x8B) => '&lsaquo;',
+      chr(0x91) => '&lsquo;',
+      chr(0x92) => '&rsquo;',
+      chr(0x93) => '&ldquo;',
+      chr(0x94) => '&rdquo;',
+      chr(0x95) => '&bull;',
+      chr(0x96) => '&ndash;',
+      chr(0x97) => '&mdash;',
+      chr(0x99) => '&trade;',
+      chr(0x9B) => '&rsquo;',
+      chr(0xA6) => '&brvbar;',
+      chr(0xA9) => '&copy;',
+      chr(0xAB) => '&laquo;',
+      chr(0xAE) => '&reg;',
+      chr(0xB1) => '&plusmn;',
+      chr(0xB5) => '&micro;',
+      chr(0xB6) => '&para;',
+      chr(0xB7) => '&middot;',
+      chr(0xBB) => '&raquo;',
+    ];
+    $csv = html_entity_decode(mb_convert_encoding(strtr($csv, $map), 'UTF-8', 'ISO-8859-2'), ENT_QUOTES, 'UTF-8');
+
   }
 
   /**
