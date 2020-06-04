f4d75a412a243018bb0a8981cdbb4b1d36e7a1b4
Neil Hastings <neil.hastings@agile6.com>
2020-06-04 11:35:24 -0400

    VACMS-1651: Add custom Site Alert Exception

---
+++ docroot/modules/custom/va_gov_backend/src/Exception/SiteAlertException.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Drupal\va_gov_backend\Exception;
+
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class SiteAlertException extends HttpException {
+  /**
+   * @param string     $message  The internal exception message
+   * @param \Exception $previous The previous exception
+   * @param int        $code     The internal exception code
+   */
+  public function __construct($message = null, \Exception $previous = null, $code = 0)
+  {
+    parent::__construct(200, $message, $previous, [], $code);
+  }
+}
