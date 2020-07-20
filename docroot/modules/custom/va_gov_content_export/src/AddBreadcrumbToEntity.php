41b1120fe909ffe98d4cc4ca224b8dd1235fddf4
Neil Hastings <neil.hastings@agile6.com>
2020-07-20 15:13:59 -0400

    Change CMS Export Breadcrumb Mangaer to use a GraphQL Subrequest

--- docroot/modules/custom/va_gov_content_export/src/AddBreadcrumbToEntity.php
+++ docroot/modules/custom/va_gov_content_export/src/AddBreadcrumbToEntity.php
@@ -8,8 +8,9 @@ use Drupal\Core\Breadcrumb\ChainBreadcrumbBuilderInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\Exception\UndefinedLinkTemplateException;
 use Drupal\Core\ParamConverter\ParamConverterManagerInterface;
-use Drupal\Core\Routing\RouteMatch;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\graphql\GraphQL\Buffers\SubRequestBuffer;
 use Symfony\Cmf\Component\Routing\RouteObjectInterface;
 
 /**
@@ -40,6 +41,20 @@ class AddBreadcrumbToEntity {
   protected $paramConverterManager;
 
   /**
+   * The subrequest buffer service.
+   *
+   * @var \Drupal\graphql\GraphQL\Buffers\SubRequestBuffer
+   */
+  protected $subRequestBuffer;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
    * An array of excluded entity types.
    *
    * @var string[]
@@ -59,15 +74,23 @@ class AddBreadcrumbToEntity {
    *   The routeProvider service.
    * @param \Drupal\Core\ParamConverter\ParamConverterManagerInterface $paramConverterManager
    *   The paramConverterManger service.
+   * @param \Drupal\graphql\GraphQL\Buffers\SubRequestBuffer $subRequestBuffer
+   *   The sub-request buffer service.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
+   *   The current route match.
    */
   public function __construct(
     ChainBreadcrumbBuilderInterface $breadcrumbBuilder,
     RouteProviderInterface $routeProvider,
-    ParamConverterManagerInterface $paramConverterManager
+    ParamConverterManagerInterface $paramConverterManager,
+    SubRequestBuffer $subRequestBuffer,
+    RouteMatchInterface $routeMatch
   ) {
     $this->breadcrumbBuilder = $breadcrumbBuilder;
     $this->routeProvider = $routeProvider;
     $this->paramConverterManager = $paramConverterManager;
+    $this->subRequestBuffer = $subRequestBuffer;
+    $this->routeMatch = $routeMatch;
   }
 
   /**
@@ -155,15 +178,13 @@ class AddBreadcrumbToEntity {
    * @throws \Drupal\Core\ParamConverter\ParamNotConvertedException
    */
   protected function getBreadCrumbForEntity(ContentEntityInterface $entity, string $route_name) : Breadcrumb {
-    $routeParameters = $entity->toUrl()->getRouteParameters();
-    $route = $this->routeProvider->getRouteByName($route_name);
-    $routeParameters[RouteObjectInterface::ROUTE_NAME] = $route_name;
-    $routeParameters[RouteObjectInterface::ROUTE_OBJECT] = $route;
-    $routeParameters += $route->getDefaults();
-    $upcasted_parameters = $this->paramConverterManager->convert($routeParameters);
+    // Use a subrequest so the route context is set correctly.
+    $url = $entity->toUrl();
+    $resolve = $this->subRequestBuffer->add($url, function () {
+      return $this->breadcrumbBuilder->build($this->routeMatch);
+    });
 
-    $routeMatch = new RouteMatch($route_name, $route, $upcasted_parameters, $routeParameters);
-    return $this->breadcrumbBuilder->build($routeMatch);
+    return $resolve()->getValue();
   }
 
   /**
