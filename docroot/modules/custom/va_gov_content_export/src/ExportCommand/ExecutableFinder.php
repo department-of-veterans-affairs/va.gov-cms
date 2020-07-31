f7fd6b47a4386d5f9602dba1484b1cd934454bdd
Neil Hastings <neil.hastings@agile6.com>
2020-07-31 09:52:54 -0400

    VACMS-2117 Add drush 8 cms export commands (#2524)
    * VACMS-2117: Add drush8 cms export commands
    
    * VACMS-2117: code style cleanup
    
    * Dynamiclly find the executable
    
    * Fix issue with exec finder class
    
    * Update docroot/modules/custom/va_gov_content_export/src/ExportCommand/BuildCommands.php
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>

---
+++ docroot/modules/custom/va_gov_content_export/src/ExportCommand/ExecutableFinder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\va_gov_content_export\ExportCommand;
+
+/**
+ * Find the drush exectable.
+ */
+class ExecutableFinder {
+
+  /**
+   * Find the executable.
+   *
+   * @param string $command
+   *   The name of the command.
+   *
+   * @return string
+   *   An executable string, i.e. "drush @foo.bar" or "./vendor/bin/drupal".
+   */
+  public function findExecutable(string $command) : string {
+    $args = [];
+    foreach ($_SERVER['argv'] as $arg) {
+      if ($arg === $command) {
+        break;
+      }
+      if (strpos($arg, '--backend') !== 0) {
+        $args[] = $arg;
+      }
+    }
+    if (isset($_SERVER['PWD']) && !is_file($args[0]) && is_file($_SERVER['PWD'] . '/' . $args[0])) {
+      $args[0] = $_SERVER['PWD'] . '/' . $args[0];
+    }
+    return implode(' ', $args);
+  }
+
+}
