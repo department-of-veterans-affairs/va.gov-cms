1e784d4adafec3e5f016dce1898ec16122dffa0a
Neil Hastings <neil.hastings@agile6.com>
2020-08-07 10:46:42 -0400

    phpcs fixes

--- docroot/modules/custom/va_gov_content_export/src/SiteStatus/SiteStatus.php
+++ docroot/modules/custom/va_gov_content_export/src/SiteStatus/SiteStatus.php
@@ -2,18 +2,51 @@
 
 namespace Drupal\va_gov_content_export\SiteStatus;
 
-use Drupal\Core\Site\Settings;
+use Drupal\Core\State\StateInterface;
 
 /**
  * SiteStatus Service.
  */
 class SiteStatus implements SiteStatusInterface {
 
+  public const VA_GOV_DEPLOY_MODE = 'va_gov.deploy_mode';
+
+  /**
+   * State object.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * SiteStatus constructor.
+   *
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The State.
+   */
+  public function __construct(StateInterface $state) {
+    $this->state = $state;
+  }
+
   /**
    * {@inheritDoc}
    */
   public function inDeployMode(): bool {
-    return Settings::get('va_site_deploy_mode', FALSE);
+    return $this->state->get(static::VA_GOV_DEPLOY_MODE, FALSE);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function enableDeployMode(): void {
+    $this->state->set(static::VA_GOV_DEPLOY_MODE, TRUE);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function disableDeployMode(): void {
+    $this->state->set(static::VA_GOV_DEPLOY_MODE, FALSE);
   }
 
 }
