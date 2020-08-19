74adaf766d9d0aa1fc55b01c92e862004f73e68b
Neil Hastings <neil.hastings@agile6.com>
2020-08-19 08:04:09 -0400

    Code format cleanup

--- docroot/modules/custom/va_gov_backend/src/Exception/SuccessHTTPException.php
+++ docroot/modules/custom/va_gov_backend/src/Exception/SuccessHTTPException.php
@@ -1,19 +1,28 @@
 <?php
 
-
 namespace Drupal\va_gov_backend\Exception;
-
 
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
+/**
+ * An excpetion which returns a 200.
+ *
+ * This allows for stopping a HTTP request and returning a 200 and static HTML.
+ */
 class SuccessHTTPException extends HttpException {
+
   /**
-   * @param string     $message  The internal exception message
-   * @param \Exception $previous The previous exception
-   * @param int        $code     The internal exception code
+   * Constructor.
+   *
+   * @param string $message
+   *   The internal exception message.
+   * @param \Exception $previous
+   *   The previous exception.
+   * @param int $code
+   *   The internal exception code.
    */
-  public function __construct($message = null, \Exception $previous = null, $code = 0)
-  {
+  public function __construct($message = NULL, \Exception $previous = NULL, $code = 0) {
     parent::__construct(200, $message, $previous, [], $code);
   }
+
 }
