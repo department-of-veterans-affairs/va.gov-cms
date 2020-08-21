d3d7bd05cd5ec48a30dd468185d5a9c767d4f68b
Elijah Lynn <elijah@elijahlynn.net>
2020-08-20 18:59:46 -0700

    Update docroot/modules/custom/va_gov_backend/src/Exception/SuccessHTTPException.php

--- docroot/modules/custom/va_gov_backend/src/Exception/SuccessHTTPException.php
+++ docroot/modules/custom/va_gov_backend/src/Exception/SuccessHTTPException.php
@@ -7,7 +7,8 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 /**
  * An exception which returns a 200.
  *
- * This allows for stopping a HTTP request from bootstrapping Drupal, then returning a 200 with static HTML.
+ * This allows for stopping a HTTP request from bootstrapping Drupal, 
+ * then returning a 200 with static HTML.
  */
 class SuccessHTTPException extends HttpException {
 
