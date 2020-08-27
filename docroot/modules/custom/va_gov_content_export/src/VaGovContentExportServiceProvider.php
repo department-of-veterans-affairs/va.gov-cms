89511b2ae80af9200c18f5fbcd54be66b5258db4
Neil Hastings <neil.hastings@agile6.com>
2020-08-27 16:20:10 -0400

    PHPCS update

--- docroot/modules/custom/va_gov_content_export/src/VaGovContentExportServiceProvider.php
+++ docroot/modules/custom/va_gov_content_export/src/VaGovContentExportServiceProvider.php
@@ -14,7 +14,10 @@ class VaGovContentExportServiceProvider extends ServiceProviderBase {
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
+    // Remove tome sync config event subscriber to not trigger a config export.
     $container->removeDefinition('tome_sync.config_event_subscriber');
+    // Remove tome syncs field item processor since it removes processed field.
+    $container->removeDefinition('serializer.normalizer.field_tome_sync');
   }
 
 }
