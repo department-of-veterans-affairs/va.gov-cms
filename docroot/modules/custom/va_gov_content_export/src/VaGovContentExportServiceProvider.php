c153c8ac5c43acb0f3cd0d004822389d45cc7792
Steve Wirt <swirtSJW@users.noreply.github.com>
2020-08-28 09:24:00 -0400

    Remove trailing space. Vowe never to use inline editor.

--- docroot/modules/custom/va_gov_content_export/src/VaGovContentExportServiceProvider.php
+++ docroot/modules/custom/va_gov_content_export/src/VaGovContentExportServiceProvider.php
@@ -14,7 +14,7 @@ class VaGovContentExportServiceProvider extends ServiceProviderBase {
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
-    // Remove Tome Sync's config event subscriber to not trigger a 
+    // Remove Tome Sync's config event subscriber to not trigger a
     // config export which we don't use for our use case.
     $container->removeDefinition('tome_sync.config_event_subscriber');
     // Remove Tome Sync's field item processor since it removes the
