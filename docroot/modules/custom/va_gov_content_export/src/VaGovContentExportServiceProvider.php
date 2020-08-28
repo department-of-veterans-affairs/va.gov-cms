f75573e234dbb3ec49bf07c38e76d4ef7945c50c
Steve Wirt <swirtSJW@users.noreply.github.com>
2020-08-27 21:31:48 -0400

    Clarify documentation comments.
    Co-authored-by: Elijah Lynn <elijah@elijahlynn.net>

--- docroot/modules/custom/va_gov_content_export/src/VaGovContentExportServiceProvider.php
+++ docroot/modules/custom/va_gov_content_export/src/VaGovContentExportServiceProvider.php
@@ -14,9 +14,9 @@ class VaGovContentExportServiceProvider extends ServiceProviderBase {
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
-    // Remove tome sync config event subscriber to not trigger a config export.
+    // Remove Tome Sync's config event subscriber to not trigger a config export which we don't use for our use case.
     $container->removeDefinition('tome_sync.config_event_subscriber');
-    // Remove tome syncs field item processor since it removes processed field.
+    // Remove Tome Sync's field item processor since it removes the `processed` field which must be present in output. 
     $container->removeDefinition('serializer.normalizer.field_tome_sync');
   }
 
