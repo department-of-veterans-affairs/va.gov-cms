190f7856ba8a05b0b19ca69c36f2519b13dfe8a8
Ethan Teague <metrowebz@gmail.com>
2020-08-29 10:07:51 -0400

    VACMS-2471: Updating search logic.

--- docroot/modules/custom/va_gov_backend/src/Plugin/Validation/Constraint/UniqueTitleValidator.php
+++ docroot/modules/custom/va_gov_backend/src/Plugin/Validation/Constraint/UniqueTitleValidator.php
@@ -15,9 +15,10 @@ class UniqueTitleValidator extends ConstraintValidator {
    */
   public function validate($item, Constraint $constraint) {
 
+    // We will use this for comparison against results.
+    $original_comparator = $this->format($item->value);
     // Grab first word to use in search query.
-    $lower_case_value = strtolower($item->value);
-    $first_word = strtok($lower_case_value, ' ');
+    $first_word_comparator = strtok($original_comparator, ' ');
 
     // Per internal dev meeting, dependency injection doesn't
     // work well with constraints.
@@ -26,7 +27,7 @@ class UniqueTitleValidator extends ConstraintValidator {
     $node_storage = \Drupal::entityTypeManager()->getStorage('node');
     $query = $node_storage->getQuery();
     $query->condition('type', 'q_a');
-    $query->condition('title', $first_word . '%', 'LIKE');
+    $query->condition('title', $first_word_comparator . '%', 'LIKE');
     // Exclude the current entity.
     if (!empty($id = $this->context->getRoot()->getEntity()->id())) {
       $query->condition('nid', $id, '!=');
@@ -37,13 +38,46 @@ class UniqueTitleValidator extends ConstraintValidator {
     if (!empty($nodes)) {
       // See if we have any that match our field value.
       foreach ($nodes as $node) {
-        // Loose comparison just to be safe.
-        if ($lower_case_value == strtolower($node->get('title')->value)) {
-          $this->context->addViolation($constraint->notUniqueTitle, ['%title' => $item->value, ':nid' => $node->id()]);
+        // Compare our input title against db first word match.
+        if ($this->compare($original_comparator, $node->get('title')->value)) {
+          // Identical, so throw violation.
+          $this->context->addViolation($constraint->notUniqueTitle, ['%title' => $node->get('title')->value, ':nid' => $node->id()]);
         }
       }
     }
+  }
 
+  /**
+   * Returns formatted string for comparison.
+   *
+   * @return string
+   *   Returns a formatted string.
+   */
+  public function format($string) {
+    // Strip punctuation.
+    $strip_punctuation = trim(preg_replace('/[^a-z0-9]+/i', ' ', $string));
+    return strtolower($strip_punctuation);
+  }
+
+  /**
+   * Compare original with db result.
+   *
+   * @param string $a
+   *   The input.
+   * @param string $b
+   *   The db result.
+   *
+   * @return bool
+   *   Returns TRUE is strings identical.
+   */
+  public function compare($a, $b) {
+    $b_formatted = $this->format($b);
+    // Loose comparison just to be safe.
+    if ($a == $b_formatted) {
+      return TRUE;
+    }
+
+    return FALSE;
   }
 
 }
