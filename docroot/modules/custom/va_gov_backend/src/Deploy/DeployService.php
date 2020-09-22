63fe5a6f96d9861147fe5f76b21550f4dee32c87
Neil Hastings <neil.hastings@agile6.com>
2020-09-22 10:53:59 -0400

    When maintenace mode is enabled allow CMS content export tar to be available

--- docroot/modules/custom/va_gov_backend/src/Deploy/DeployService.php
+++ docroot/modules/custom/va_gov_backend/src/Deploy/DeployService.php
@@ -2,16 +2,36 @@
 
 namespace Drupal\va_gov_backend\Deploy;
 
+use Drupal\Core\Site\Settings;
+use Drupal\va_gov_backend\Deploy\Plugin\CMSExport;
 use Drupal\va_gov_backend\Deploy\Plugin\HealthCheck;
 use Drupal\va_gov_backend\Deploy\Plugin\MaintenanceMode;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
- * A class to control what happens in a deploy process durring settings.php.
+ * A class to control what happens in a deploy process during settings.php.
  *
  * This all occurs before the database, cache and Drupal services are available.
  */
 class DeployService {
+
+  /**
+   * Initiate the DeployService class.
+   *
+   * @param array $settings
+   *   The Drupal settings array.
+   *
+   * @return \Drupal\va_gov_backend\Deploy\DeployService
+   *   The deploy service.
+   */
+  public static function create(array $settings = []) : DeployService {
+    // We must instantiate a Settings object here since we are
+    // before Drupal core has a chance.
+    new Settings($settings);
+
+    return new static();
+  }
 
   /**
    * A list of the deploy static class.
@@ -23,6 +43,7 @@ class DeployService {
    */
   public static function deployPlugins() : array {
     return [
+      CMSExport::class,
       HealthCheck::class,
       MaintenanceMode::class,
     ];
@@ -43,7 +64,13 @@ class DeployService {
       /** @var \Drupal\va_gov_backend\Deploy\Plugin\DeployPluginInterface $deploy_plugin */
       $deploy_plugin = new $plugin();
       if ($deploy_plugin->match($request)) {
-        $deploy_plugin->run($request, $app_root, $site_path);
+        // If we have a Symfony Response Object then send it.
+        $response = $deploy_plugin->run($request, $app_root, $site_path);
+        if (!empty($response) && ($response instanceof Response)) {
+          $response->send();
+          // We exit here because otherwise Drupal will try to run it's handler.
+          exit();
+        }
       }
     }
   }
--- docroot/modules/custom/va_gov_backend/src/Deploy/Plugin/CMSExport.php
+++ docroot/modules/custom/va_gov_backend/src/Deploy/Plugin/CMSExport.php
@@ -2,12 +2,24 @@
 
 namespace Drupal\va_gov_backend\Deploy\Plugin;
 
+use Drupal\Core\StreamWrapper\PublicStream;
+use Drupal\Core\StreamWrapper\StreamWrapperInterface;
+use Drupal\va_gov_content_export\Archive\ArchiveArgs;
+use Drupal\va_gov_content_export\Archive\ArchiveArgsFactory;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * CMS Export plugin.
  */
 class CMSExport implements DeployPluginInterface {
+
+  /**
+   * A stream wrapper.
+   *
+   * @var \Drupal\Core\StreamWrapper\StreamWrapperInterface
+   */
+  protected $streamWrapperInstance;
 
   /**
    * {@inheritDoc}
@@ -24,8 +36,80 @@ class CMSExport implements DeployPluginInterface {
   /**
    * {@inheritDoc}
    */
-  public function run(Request $request, string $app_root, string $site_path): ?bool {
-    $i = 1;
+  public function run(Request $request, string $app_root, string $site_path) {
+    $this->registerStreamWrapper();
+    if ($this->fileExists()) {
+      $url = $this->getUrl();
+
+      return RedirectResponse::create($url);
+    }
+  }
+
+  /**
+   * Register the stream wrapper to be used later.
+   */
+  protected function registerStreamWrapper() : void {
+    $archiveArgs = $this->getArchiveArgs();
+    $output_uri = $archiveArgs->getOutputPath();
+    $this->streamWrapperInstance = new PublicStream();
+    $this->streamWrapperInstance->setUri($output_uri);
+  }
+
+  /**
+   * Get the Arguments used to find the tar archive.
+   *
+   * @return \Drupal\va_gov_content_export\Archive\ArchiveArgs
+   *   The arguments used to find the tar archive.
+   */
+  protected function getArchiveArgs() : ArchiveArgs {
+    $archiveArgsFactory = new ArchiveArgsFactory();
+    return $archiveArgsFactory->createContentArgs();
+  }
+
+  /**
+   * Does the tar archive file exist?
+   *
+   * @return bool
+   *   Does the file exist.
+   */
+  protected function fileExists() : bool {
+    $file_name = $this->getFilePath();
+    return file_exists($file_name);
+  }
+
+  /**
+   * Get the file path to the tar archive.
+   *
+   * @return null|string
+   *   Either the local path to the file or null.
+   */
+  protected function getFilePath() : ?string {
+    $streamWrapper = $this->getStreamWrapper();
+    if ($streamWrapper) {
+      return $streamWrapper->realpath();
+    }
+
+    return NULL;
+  }
+
+  /**
+   * Get the stream wrapper instance.
+   *
+   * @reutrn Drupal\Core\StreamWrapper\StreamWrapperInterface
+   *  The stream wrapper.
+   */
+  protected function getStreamWrapper() : ?StreamWrapperInterface {
+    return $this->streamWrapperInstance;
+  }
+
+  /**
+   * Get the URl to the file.
+   *
+   * @return string
+   *   The url.
+   */
+  protected function getUrl() : string {
+    return $this->getStreamWrapper()->getExternalUrl();
   }
 
 }
