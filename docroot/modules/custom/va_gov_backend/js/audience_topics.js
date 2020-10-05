9656ee9404573386a8eca637ed24a9a9bba0e376
Adrian Rollett <acrollet@gmail.com>
2020-10-05 16:29:15 -0600

    VACMS-2563: make selectors work when form is loaded via ajax

--- docroot/modules/custom/va_gov_backend/js/audience_topics.js
+++ docroot/modules/custom/va_gov_backend/js/audience_topics.js
@@ -8,57 +8,56 @@
    */
   function enforceMaximumNumberOfTags() {
     // Get the total number of tags selected between in the Topics and Beneficiaries fields.
-    let total = $('#edit-field-tags-0-subform-field-topics').find('input:checked').length;
-    const audienceSelected = $('#edit-field-tags-0-subform-field-audience-beneficiares-wrapper')
-      .find('input[id!="edit-field-tags-0-subform-field-audience-beneficiares-none"]:checked').length;
+    let total = $('div[id^="edit-field-tags-0-subform-field-topics"]').find('input:checked').length;
+    const audienceSelected = $('div[id^="edit-field-tags-0-subform-field-audience-beneficiares-wrapper"]')
+      .find('input:not([id^="edit-field-tags-0-subform-field-audience-beneficiares-none"]):checked').length;
     total += audienceSelected;
 
     if (total >= 4) {
       // If a total of four or more tags have been selected, prevent the user from selecting more.
-      $('#edit-field-tags-0-subform-field-topics').find('input[type=checkbox]:not(:checked)').attr('disabled', true);
-      if (!audienceSelected) {
-        $('#edit-field-tags-0-subform-field-audience-beneficiares-wrapper')
-          .find('input[id!="edit-field-tags-0-subform-field-audience-beneficiares-none"]').attr('disabled', true);
-      }
+      $('div[id^="edit-field-tags-0-subform-field-topics"]').find('input[type=checkbox]:not(:checked)').attr('disabled', true);
+      $('div[id^="edit-field-tags-0-subform-field-audience-beneficiares-wrapper"]')
+        .find('input:not([id^="edit-field-tags-0-subform-field-audience-beneficiares-none"]):checked').attr('disabled', true);
     } else {
       // Otherwise, ensure that more may be selected.
-      $('#edit-field-tags-0-subform-field-topics').find('input[type=checkbox]').attr('disabled', false);
-      $('#edit-field-tags-0-subform-field-audience-beneficiares-wrapper')
-        .find('input[id!="edit-field-tags-0-subform-field-audience-beneficiares-none"]').attr('disabled', false);
+      $('div[id^="edit-field-tags-0-subform-field-topics"]').find('input[type=checkbox]').attr('disabled', false);
+      $('div[id^="edit-field-tags-0-subform-field-audience-beneficiares-wrapper"]')
+        .find('input').attr('disabled', false);
     }
   }
 
   Drupal.behaviors.vaGovAudienceTopics = {
     attach: function () {
+
       // Add required marker to fieldset. This is necessary because we cannot mark
       // any of the child fields as required.
       $('#edit-group-tags > legend').addClass('form-required');
 
       // Hide the beneficiaries field when the audience select does not have benficiaries selected.
-      if ($('#edit-field-tags-0-subform-field-audience-selection').val() !== 'beneficiaries') {
-        $('#edit-field-tags-0-subform-field-audience-beneficiares-wrapper').hide();
+      if ($('select[id^="edit-field-tags-0-subform-field-audience-selection"]').val() !== 'beneficiaries') {
+        $('div[id^="edit-field-tags-0-subform-field-audience-beneficiares-wrapper"]').hide();
       }
 
       // Hide the 'N/A' option for the Beneficiaries field.
-      $('#edit-field-tags-0-subform-field-audience-beneficiares-none').parent().hide();
+      $('input[id^="edit-field-tags-0-subform-field-audience-beneficiares-none"]').parent().hide();
 
       // Disable the "Non-beneficiaries" option in the field_audience_selection dropdown.
-      $('#edit-field-tags-0-subform-field-audience-selection option[value="non-beneficiaries"]').attr('disabled', true);
+      $('select[id^="edit-field-tags-0-subform-field-audience-selection"] option[value="non-beneficiaries"]').attr('disabled', true);
 
       // Enforce tag selection rules.
       enforceMaximumNumberOfTags();
 
       // React when the Audience dropdown is changed.
-      $('#edit-field-tags-0-subform-field-audience-selection').change(function () {
+      $('select[id^="edit-field-tags-0-subform-field-audience-selection"]').change(function () {
         const selection = $(this).val();
 
         if (selection === 'beneficiaries') {
           // Show beneficiaries field when selected.
-          $('#edit-field-tags-0-subform-field-audience-beneficiares-wrapper').show();
+          $('div[id^="edit-field-tags-0-subform-field-audience-beneficiares-wrapper"]').show();
         } else {
           // Otherwise clear any selected value in the beneficiaries field and hide it.
-          $('#edit-field-tags-0-subform-field-audience-beneficiares-none').prop('checked', true);
-          $('#edit-field-tags-0-subform-field-audience-beneficiares-wrapper').hide();
+          $('input[id^="edit-field-tags-0-subform-field-audience-beneficiares-none"]').prop('checked', true);
+          $('div[id^="edit-field-tags-0-subform-field-audience-beneficiares-wrapper"]').hide();
         }
 
         // Enforce tag selection rules.
@@ -66,7 +65,7 @@
       });
 
       // React when the tag/audience fields are changed.
-      $('#edit-field-tags-0-subform-field-topics, #edit-field-tags-0-subform-field-audience-beneficiares-wrapper').find('input').change(function () {
+      $('div[id^="edit-field-tags-0-subform-field-topics"], div[id^="edit-field-tags-0-subform-field-audience-beneficiares-wrapper"]').find('input').change(function () {
 
         // Enforce tag selection rules.
         enforceMaximumNumberOfTags();
