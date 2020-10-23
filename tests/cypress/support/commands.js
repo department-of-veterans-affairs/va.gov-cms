1f1f11527051093c01521227a007ccad80b8f2e4
Adrian Rollett <acrollet@chama.local>
2020-10-23 15:57:11 -0600

    VACMS-3289: commit poc wip

---
+++ tests/cypress/support/commands.js
@@ -0,0 +1,17 @@
+Cypress.Commands.add('login', (user, password) => {
+  console.log(`in login for user ${user}`);
+  return cy.request({
+    method: 'POST',
+    url: '/user/login', 
+    form: true,
+    body: { 
+      name: user,
+      pass: password,
+      form_id: 'user_login_form' 
+    }
+  });
+});
+ 
+Cypress.Commands.add('logout', () => {
+  return cy.request('/user/logout');
+});
