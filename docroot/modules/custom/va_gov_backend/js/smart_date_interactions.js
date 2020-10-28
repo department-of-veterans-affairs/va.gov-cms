b0fbb56820f87ce007389b9573dfea0a56c26c5b
Adrian Rollett <acrollet@gmail.com>
2020-10-28 09:04:37 -0600

    Fix js warning

--- docroot/modules/custom/va_gov_backend/js/smart_date_interactions.js
+++ docroot/modules/custom/va_gov_backend/js/smart_date_interactions.js
@@ -3,22 +3,18 @@
  */
 
 (function (Drupal) {
-  Drupal.behaviors.vaGovServiceLocationAddress = {
+  Drupal.behaviors.smartDateInteractions = {
     attach: function (context, settings) {
-      // Look to see if container div for end time has hidden class.
-      // If it does, hode the All day checkbox.
       const timeEnd = document.querySelector('.time-end');
-      if (timeEnd.classList.contains('hidden')) {
+
+      if (timeEnd !== null && typeof timeEnd === 'object' && timeEnd.classList.contains('hidden')) {
         const alldayLabels = document.querySelectorAll('.allday-label');
+
         alldayLabels.forEach(label => {
           label.style.display = 'none';
           label.classList.add('hidden');
-        })
-        console.log('toggles: ', alldayLabels);
-
-        //context.querySelectorAll('.allday-label').classList.add('hidden');
+        });
       }
-
     }
   };
 
