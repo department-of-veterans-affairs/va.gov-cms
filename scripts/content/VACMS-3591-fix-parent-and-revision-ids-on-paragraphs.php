52002200f3be2c0cee176fe11a0044b2c2118b05
Nathan Douglas <nathan.douglas@agile6.com>
2020-11-30 10:17:50 -0500

    First draft of a script that repairs two tables.

--- scripts/content/VACMS-3591-fix-parent-and-revision-ids-on-paragraphs.php
+++ scripts/content/VACMS-3591-fix-parent-and-revision-ids-on-paragraphs.php
@@ -2,20 +2,23 @@
 
 /**
  * @file
- * One-time migration for fixing improperly cloned paragraphs.
+ * One-time migration for repairing rows of improperly cloned paragraphs.
  *
- * Users of the entity_clone form were deselecting the checkboxes for
- * Paragraphs entities, which led to cloned nodes having references to
- * the original node's Paragraphs.
+ * After the fix for #3450, we still have some inconsistencies in the database
+ * causing issues:
+ * - `revision_id` wrong on `paragraphs_item` and `paragraphs_item_field_data`
+ * - `parent_id` wrong on `paragraphs_item_data` and
+ *     `paragraphs_item_revision_field_data`
  *
  * Our approach here will be:
  *
  * 1. Retrieve a list of all paragraphs with multiple parents.
  * 2. Repair the nodes owning those paragraphs:
  *   A. Determine whether it is a cloned node; if not, no action is necessary.
- *   B. Clone the paragraph.
- *   C. Update the cloned node to refer instead to the cloned paragraph.
- *   D. Save the node.
+ *   B. Load the node and its paragraphs, and for each paragraph:
+ *     i. Point paragraphs_item.revision_id to the appropriate paragraph
+ *        revision ID.
+ *     ii. Point paragraphs_item_field_data.revision_id
  * 3. Confirm that no paragraphs exist in the original condition.
  *
  */
@@ -78,7 +81,6 @@ function getParagraphsFields(): array {
  */
 function getMultiplyParentedParagraphFieldRows(string $parent_type, string $parent_field_name): array {
   $database = \Drupal::database();
-
   $query = $database->select('paragraphs_item_revision_field_data', 'revision');
   $query->condition('parent_type', $parent_type);
   $query->condition('parent_field_name', $parent_field_name);
@@ -112,29 +114,6 @@ function getMultiplyParentedParagraphFieldRows(string $parent_type, string $pare
   $count = count($result);
   logMessage("{$parent_type}: {$parent_field_name}... {$count} multiply-parented field rows found.");
 
-  return $result;
-}
-
-/**
- * Clone a paragraph and prepare it for insertion into the original node.
- *
- * @param \Drupal\entity_clone\EntityClone\EntityCloneInterface $cloner
- *  A cloner object.
- * @param \Drupal\paragraphs\ParagraphInterface $paragraph
- *   The paragraph to clone.
- * @param int $parent_id
- *   The parent ID, used to strip unwanted revisions.
- * @param array $properties
- *  Properties used to create the clone paragraph.
- * @param array& $already_cloned
- *  A list of entities already cloned, for use by entity_clone.
- *
- * @return \Drupal\paragraphs\ParagraphInterface
- *   The cloned paragraph.
- */
-function cloneParagraph(EntityCloneInterface $cloner, ParagraphInterface $paragraph, int $parent_id, array $properties, array &$already_cloned): ParagraphInterface {
-  $clone = $paragraph->createDuplicate();
-  $result = $cloner->cloneEntity($paragraph, $clone, $properties, $already_cloned);
   return $result;
 }
 
@@ -193,49 +172,114 @@ function replaceParagraph(EntityCloneInterface $cloner, ContentEntityInterface $
 }
 
 /**
- * Process an improperly cloned paragraph.
+ * Repair a specific paragraph whose parent references are messed up.
  *
  * @param string $parent_type
- *   The type of entity parenting this paragraph.
+ *   The machine name of the parent entity type.
  * @param string $parent_field_name
- *   The machine name of the parent entity.
- * @param int $id
- *   The paragraph entity ID.
- * @param int[] $parent_ids
- *   The parents with improper references to this paragraph.
+ *   The machine name of the parent entity paragraph field.
+ * @param int $parent_id
+ *   The unique identifier of the parent entity.
+ * @param int $paragraph_id
+ *   The unique identifier of the paragraph to repair.
  */
-function processParagraph(string $parent_type, string $parent_field_name, int $target_id, array $parent_ids): void {
+function repairParagraph(string $parent_type, string $parent_field_name, int $parent_id, int $paragraph_id) {
+  $database = \Drupal::database();
   $entity_type_manager = \Drupal::entityTypeManager();
-  $cloner = new ContentEntitycloneBase($entity_type_manager, $parent_type);
   $parent_storage = $entity_type_manager->getStorage($parent_type);
-  $parents = $parent_storage->loadMultiple($parent_ids);
-  $already_cloned = [];
-  $properties = [
-    'children' => [],
-  ];
-  foreach ($parents as $parent_id => $parent) {
-    replaceParagraph($cloner, $parent, $parent_field_name, $target_id, $properties, $already_cloned);
-  }
-}
+  $parent = $parent_storage->load($parent_id);
+  $parent_field = $parent->get($parent_field_name);
+  $parent_field_value = $parent_field->getValue();
+  foreach ($parent_field_value as $delta => $value) {
+    if ((int) $value['target_id'] === $paragraph_id) {
+      $revision_id = (int) $value['target_revision_id'];
+      logMessage("$parent_type #{$parent_id}->$parent_field_name [$delta] points to revision #$revision_id of paragraph #$paragraph_id.");
 
-/**
- * Delete non-default revisions of the specified nodes.
- *
- * @param int[] $nids
- *   The nodes whose revision histories should be deleted.
- */
-function deleteRevisionHistoryForNodes(array $nids) {
-  $node_storage = \Drupal::entityManager()->getStorage('node');
-  $nodes = $node_storage->loadMultiple($nids);
-  foreach ($nodes as $node) {
-    $default_revision_id = $node->getRevisionId();
-    $revision_ids = array_diff($node_storage->revisionIds($node), [ $default_revision_id ]);
-    foreach ($revision_ids as $revision_id) {
-      logMessage("Deleting old revision #$revision_id for node #{$node->id()}.");
-      $node_storage->deleteRevision($revision_id);
+      // Check the corresponding row in the paragraphs_item table.
+      $query = $database->select('paragraphs_item', 'p');
+      $query->addField('p', 'id', 'id');
+      $query->addField('p', 'revision_id', 'revision_id');
+      $query->condition('id', $paragraph_id);
+      $row = $query->execute()->fetchObject();
+      $row_revision_id = (int) $row->revision_id;
+      if ($revision_id === $row_revision_id) {
+        logMessage("paragraphs_item row for $paragraph #$paragraph_id also points to revision #$revision_id... no action needed.");
+      }
+      else {
+        logMessage("paragraphs_item row for $paragraph #$paragraph_id points to revision #$row_revision_id... updating.");
+        $query = $database->update("paragraphs_item");
+        $query->fields([
+          "revision_id" => $revision_id,
+        ]);
+        $query->condition('id', $paragraph_id);
+        $query->condition('revision_id', $row_revision_id);
+        $query->execute();
+      }
+
+      // Check the corresponding row in the paragraphs_item_field_data table.
+      $query = $database->select('paragraphs_item_field_data', 'p');
+      $query->addField('p', 'id', 'id');
+      $query->addField('p', 'revision_id', 'revision_id');
+      $query->addField('p', 'parent_id', 'parent_id');
+      $query->addField('p', 'parent_type', 'parent_type');
+      $query->condition('id', $paragraph_id);
+      $row = $query->execute()->fetchObject();
+      $row_revision_id = (int) $row->revision_id;
+      $row_parent_id = (int) $row->parent_id;
+      $row_parent_type = $row->parent_type;
+      if ($parent_type !== $row_parent_type) {
+        throw new \Exception("paragraphs_item_field_data #$paragraph_id is pointing to $row_parent_type (expected: $parent_type).  Something is terribly wrong!");
+      }
+      else if ($revision_id === $row_revision_id && $parent_id === $row_parent_id) {
+        logMessage("paragraphs_item_field_data row for $paragraph #$paragraph_id also points to revision #$revision_id and is parented by $parent_type #$parent_id... no action needed.");
+      }
+      else {
+        logMessage("paragraphs_item_field_data row for $paragraph #$paragraph_id points to revision #$row_revision_id (should be $revision_id) and parent $parent_type #$row_parent_id (should be #$parent_id)... updating.");
+        $query = $database->update("paragraphs_item_field_data");
+        $query->fields([
+          "revision_id" => $revision_id,
+          "parent_id" => $parent_id,
+        ]);
+        $query->condition('id', $paragraph_id);
+        $query->condition('revision_id', $row_revision_id);
+        $query->condition('parent_id', $row_parent_id);
+        $query->execute();
+      }
+
+      // Check the corresponding row in the paragraphs_item_revision_field_data table.
+      $query = $database->select('paragraphs_item_revision_field_data', 'p');
+      $query->addField('p', 'id', 'id');
+      $query->addField('p', 'revision_id', 'revision_id');
+      $query->addField('p', 'parent_id', 'parent_id');
+      $query->addField('p', 'parent_type', 'parent_type');
+      $query->condition('id', $paragraph_id);
+      $query->condition('revision_id', $revision_id);
+      $row = $query->execute()->fetchObject();
+      $row_revision_id = (int) $row->revision_id;
+      $row_parent_id = (int) $row->parent_id;
+      $row_parent_type = $row->parent_type;
+      if ($parent_type !== $row_parent_type) {
+        throw new \Exception("paragraphs_item_revision_field_data #$paragraph_id is pointing to $row_parent_type (expected: $parent_type).  Something is terribly wrong!");
+      }
+      else if ($parent_id === $row_parent_id) {
+        logMessage("paragraphs_item_revision_field_data row for $paragraph #$paragraph_id @ revision #$revision_id is parented by $parent_type #$parent_id... no action needed.");
+      }
+      else {
+        logMessage("paragraphs_item_revision_field_data row for $paragraph #$paragraph_id @ revision #$revision_id is parented by $parent_type #$row_parent_id (should be #$parent_id)... updating.");
+        $query = $database->update("paragraphs_item_revision_field_data");
+        $query->fields([
+          "parent_id" => $parent_id,
+        ]);
+        $query->condition('id', $paragraph_id);
+        $query->condition('revision_id', $row_revision_id);
+        $query->condition('parent_id', $row_parent_id);
+        $query->execute();
+      }
+
     }
   }
 }
+
 
 /**
  * Main entry point for our script.
@@ -272,11 +316,10 @@ function run() {
         logMessage("Paragraph #{$target_id} has {$parent_count} parents: {$parent_list}");
         // We can leave this paragraph with its original parent,
         // but we wanna give clones to its other parents.
-        $cloned_parent_ids = array_slice($parent_ids, 1);
-        processParagraph($parent_type, $parent_field_name, $target_id, $cloned_parent_ids);
-        // Those improperly cloned nodes should also lose their revision histories to avoid
-        // reoccurrences of this very problem.
-        deleteRevisionHistoryForNodes($cloned_parent_ids);
+        $cloned_parent_ids = array_slice($parent_ids, 0, 1);
+        if (!empty($cloned_parent_ids)) {
+          repairParagraph($parent_type, $parent_field_name, (int) $cloned_parent_ids[0], (int) $target_id);
+        }
       }
     }
 
