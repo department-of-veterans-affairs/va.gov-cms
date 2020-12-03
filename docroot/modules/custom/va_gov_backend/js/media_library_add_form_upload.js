4d2cf85c93a4267de42ef25f0734040064ca7e89
Nathan Douglas <nathan.douglas@agile6.com>
2020-12-03 08:25:58 -0500

    Changes in response to Adrian's comments. <3

--- docroot/modules/custom/va_gov_backend/js/media_library_add_form_upload.js
+++ docroot/modules/custom/va_gov_backend/js/media_library_add_form_upload.js
@@ -4,18 +4,15 @@
 
 (function ($, Drupal) {
 
-  Drupal.behaviors.vaGovMediaLibraryReusable = {
+  Drupal.behaviors.vaGovMediaLibraryReusableSaveAndSelect = {
     attach: function () {
       $(document).ajaxComplete(function (event, xhr, settings) {
         $('input.field_media_in_library[type=checkbox]:not(checked)').each(function () {
           $('.ui-dialog-buttonpane button').first().hide();
         });
-        $('input.field_media_in_library[type=checkbox]:checked').each(function () {
-          $('.ui-dialog-buttonpane button').first().show();
-        });
         $('input.field_media_in_library')
-          .off('.vagov_media_library')
-          .on('change.vagov_media_library', function (object) {
+          .once()
+          .change(function (object) {
             if (this.checked) {
               $('.ui-dialog-buttonpane button').first().show();
             }
