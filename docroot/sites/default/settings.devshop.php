b8ad8e289b319de3d49709c55a8593a8fc2f06dc
Neil Hastings <neil.hastings@agile6.com>
2020-12-10 10:43:54 -0500

    Update front end url settings

--- docroot/sites/default/settings.devshop.php
+++ docroot/sites/default/settings.devshop.php
@@ -75,4 +75,4 @@ $settings['trusted_host_patterns'] = [
 $settings['va_cms_bot_github_auth_token'] = getenv('GITHUB_TOKEN') ?: FALSE;
 
 $settings['va_gov_frontend_build_type'] = 'devshop';
-$settings['va_gov_frontend_url'] = $webhost_on_cli;
+$settings['va_gov_frontend_url'] = $webhost_on_cli . '/static';
