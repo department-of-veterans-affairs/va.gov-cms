c232d1319195f211ad26093c3c8b0ca5e1ca98e7
Adrian Rollett <acrollet@gmail.com>
2020-12-10 16:06:24 -0700

    Commit WIP

--- docroot/modules/custom/va_gov_user/js/sections_accordion.js
+++ docroot/modules/custom/va_gov_user/js/sections_accordion.js
@@ -2,9 +2,9 @@
  * @file
  */
 
-(($, Drupal) => {
+(function ($, Drupal) {
   Drupal.behaviors.vaGovSectionsAccordion = {
-    attach(context) {
+    attach(context, settings) {
       // Add aria-hidden attribute to all collapsed areas.
       $(".sections")
         .find(".subsections")
