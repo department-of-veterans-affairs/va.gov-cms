da963d48249146c909721683d6b1e11ca4f9a42f
Adrian Rollett <acrollet@gmail.com>
2020-12-14 13:36:32 -0700

    Various cypress accessibility testing improvements

--- tests/cypress/integration/homepageAccessibility.spec.js
+++ tests/cypress/integration/homepageAccessibility.spec.js
@@ -1,25 +1,3 @@
-// @TODO move this to a separate file.
-function terminalLog(violations) {
-  cy.task(
-    'log',
-    `${violations.length} accessibility violation${
-      violations.length === 1 ? '' : 's'
-    } ${violations.length === 1 ? 'was' : 'were'} detected`
-  );
-  // pluck specific keys to keep the table readable
-  const violationData = violations.map(
-    ({ id, impact, description, nodes }, idx) => ({
-      id,
-      impact,
-      description,
-      target: nodes[idx].target,
-      nodes: nodes.length
-    })
-  )
-
-  cy.task('table', violationData);
-}
-
 describe('Component accessibility test', () => {
   // Test homepage while logged out.
   it('Login page has no detectable accessibility violations on load.', () => {
@@ -27,7 +5,7 @@ describe('Component accessibility test', () => {
     cy.injectAxe();
     
     cy.get('body').each((element, index) => {
-      cy.checkA11y(null, null, terminalLog);
+      cy.checkA11y(null, null, cy.terminalLog);
     });
   });
 });
--- tests/cypress/integration/nodeCreationAccessibility.spec.js
+++ tests/cypress/integration/nodeCreationAccessibility.spec.js
@@ -1,4 +1,22 @@
-const routes = ['/node/add/page', '/node/add/q_a'];
+const routes = [
+  '/sections',
+  '/node/add/page',
+  '/node/add/landing_page',
+  '/node/add/documentation_page',
+  '/node/add/event',
+  '/node/add/health_care_local_facility',
+  '/node/add/health_care_region_detail_page',
+  '/node/add/health_care_region_page',
+  '/node/add/office',
+  '/node/add/outreach_asset',
+  '/node/add/person_profile',
+  '/node/add/press_release',
+  '/node/add/q_a',
+  '/node/add/regional_health_care_service_des',
+  '/node/add/news_story',
+  '/node/add/support_service',
+  '/user',
+];
 
 describe('Component accessibility test', () => {
   routes.forEach((route) => {
@@ -11,9 +29,16 @@ describe('Component accessibility test', () => {
 
       cy.visit(route);
       cy.injectAxe();
-      
+
+      const axeRuntimeOptions = {
+        runOnly: {
+          type: 'tag',
+          values: ['wcag2a', 'wcag2aa']
+        }
+      };
+
       cy.get('body').each((element, index) => {
-        cy.checkA11y(null, null, terminalLog);
+        cy.checkA11y(null, axeRuntimeOptions, cy.terminalLog);
       });
     });
   });
