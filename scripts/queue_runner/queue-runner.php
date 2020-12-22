ca0560b30b5fbf6c9e44c79bb29ddec71deaf7cf
Neil Hastings <neil.hastings@agile6.com>
2020-12-22 10:23:06 -0500

    Fix directory permissions

--- scripts/queue_runner/queue-runner.php
+++ scripts/queue_runner/queue-runner.php
@@ -1,14 +1,18 @@
 <?php
 
 use Drupal\Core\DrupalKernel;
+use Drupal\Core\Site\Settings;
 use Symfony\Component\HttpFoundation\Request;
 
 $tugboat_root = getenv('DOCROOT');
-$autoloader = $tugboat_root . '/autoload.php';
-
+$autoloader = require $tugboat_root . '/autoload.php';
+require_once $tugboat_root . '/core/includes/bootstrap.inc';
 $request = Request::createFromGlobals();
-DrupalKernel::createFromRequest($request, $autoloader, 'prod');
+Settings::initialize($tugboat_root, DrupalKernel::findSitePath($request), $autoloader);
 
+$kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod')->boot();
+
+// @TODO create Application class
 $i = 0;
 while (1 == 1) {
   echo $i . PHP_EOL;
