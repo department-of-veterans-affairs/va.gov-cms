eea0ed18eda1ffee065e1c7c63db596f6756e2e3
Adrian Rollett <acrollet@gmail.com>
2021-02-04 13:14:36 -0700

    Require exact match of yarn version

--- scripts/composer/check_yarn_version.sh
+++ scripts/composer/check_yarn_version.sh
@@ -9,9 +9,9 @@ cd "$(git rev-parse --show-toplevel)"
 
 YARN_VERSION_INSTALLED="$( yarn --version )"
 REQUIRED_YARN_VERSION=$( node --print --eval="require('./web/package.json').engines.yarn" | sed 's/[<>=~]*//' )
-if [ ! "$( printf '%s\n' "$REQUIRED_YARN_VERSION" "$YARN_VERSION_INSTALLED" | sort --version-sort | head --lines=1 )" = "$REQUIRED_YARN_VERSION" ]; then
-  echo "Installed yarn version ${YARN_VERSION_INSTALLED} is older than the required version: ${REQUIRED_YARN_VERSION}!"
+if [ "${REQUIRED_YARN_VERSION}" != "${YARN_VERSION_INSTALLED}" ]; then
+  echo "Installed yarn version '${YARN_VERSION_INSTALLED}' does not match the required version: '${REQUIRED_YARN_VERSION}'!"
   exit 1
 else
-  echo "Installed yarn version ${YARN_VERSION_INSTALLED} is equal or greater to the required version ${REQUIRED_YARN_VERSION}."
+  echo "Installed yarn version '${YARN_VERSION_INSTALLED}' matches the required version '${REQUIRED_YARN_VERSION}'."
 fi
