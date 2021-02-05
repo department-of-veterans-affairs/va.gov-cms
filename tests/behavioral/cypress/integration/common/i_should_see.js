38dbc1a2d65e4d37279886ff638d5baf6dc3b84c
Nathan Douglas <nathan.douglas@agile6.com>
2021-02-05 15:03:53 -0500

    Incorporates Adrian's suggestion about the element thing.

--- tests/behavioral/cypress/integration/common/i_should_see.js
+++ tests/behavioral/cypress/integration/common/i_should_see.js
@@ -1,7 +1,7 @@
 import { Then } from 'cypress-cucumber-preprocessor/steps';
 
-Then(`I should see element {string}`, (text) => cy.get(text).should('exist'));
-Then(`I should not see element {string}`, (text) => cy.get(text).should('not.exist'));
+Then(`I should see element with selector {string}`, (text) => cy.get(text).should('exist'));
+Then(`I should not see element with selector {string}`, (text) => cy.get(text).should('not.exist'));
 
 Then(`I should see {string}`, (text) => cy.contains(text).should('exist'));
 Then(`I should not see {string}`, (text) => cy.contains(text).should('not.exist'));
