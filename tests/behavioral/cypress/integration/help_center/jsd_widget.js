86fccc9c3e2bdc1ae7ad66c0e2c9926d1c882936
Nathan Douglas <nathan.douglas@agile6.com>
2021-02-05 15:12:56 -0500

    Use regexes in step definitions to appease the deities of fluidity and vex Neil.

--- tests/behavioral/cypress/integration/help_center/jsd_widget.js
+++ tests/behavioral/cypress/integration/help_center/jsd_widget.js
@@ -1,4 +1,4 @@
 import { Then } from 'cypress-cucumber-preprocessor/steps';
 
-Then(`I should see the JSD widget`, () => cy.get('iframe#jsd-widget').iframe().find('p').should('exist'));
-Then(`I should not see the JSD widget`, () => cy.get('iframe#jsd-widget').should('not.exist'));
+Then(/I should see(?: (a|an|the))? JSD widget/, () => cy.get('iframe#jsd-widget').iframe().find('p').should('exist'));
+Then(/I should not see(?: (a|an|the))? JSD widget/, () => cy.get('iframe#jsd-widget').should('not.exist'));
