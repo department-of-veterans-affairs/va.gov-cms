8ec50b1261ac8087c3a0a04bd0c7fe3dc45afb4b
Nathan Douglas <nathan.douglas@agile6.com>
2021-02-05 12:07:18 -0500

    Refactors widget hook, makes What's new in the CMS block test more readable.

--- tests/behavioral/cypress/integration/help_center/whats_new_in_the_cms_block.js
+++ tests/behavioral/cypress/integration/help_center/whats_new_in_the_cms_block.js
@@ -1,4 +1,4 @@
 import { Then } from 'cypress-cucumber-preprocessor/steps';
 
-Then(`I should see the what's new in the CMS block`, () => cy.get('#block-whatsnewinthecms').should('exist'));
-Then(`I should not see the what's new in the CMS block`, () => cy.get('#block-whatsnewinthecms').should('not.exist'));
+Then(`I should see the what's new in the CMS block`, () => cy.findByText(`What's new in the CMS`).should('exist'));
+Then(`I should not see the what's new in the CMS block`, () => cy.findByText(`What's new in the CMS`).should('not.exist'));
