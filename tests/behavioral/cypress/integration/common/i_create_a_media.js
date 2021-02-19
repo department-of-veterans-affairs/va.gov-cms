390ac00510590c131590e492754ced36489ca437
Nathan Douglas <nathan.douglas@agile6.com>
2021-02-19 09:18:02 -0500

    Working image-cropping in media creation
    omg

--- tests/behavioral/cypress/integration/common/i_create_a_media.js
+++ tests/behavioral/cypress/integration/common/i_create_a_media.js
@@ -3,9 +3,6 @@ import faker from "faker";
 
 const creators = {
   image: () => {
-    const POINTER_DOWN = window.PointerEvent ? 'pointerdown' : 'mousedown';
-    const POINTER_MOVE = window.PointerEvent ? 'pointermove' : 'mousemove';
-    const POINTER_UP = window.PointerEvent ? 'pointerup' : 'mouseup';
     cy.visit('/media/add/image');
     cy.scrollTo('top');
     cy.findAllByLabelText('Name').type('[Test Data] ' + faker.lorem.sentence(), { force: true });
@@ -13,52 +10,33 @@ const creators = {
     cy.findAllByLabelText('Owner').select('Veterans Affairs');
     cy.get('#edit-image-0-upload').attachFile('images/polygon_image.png').wait(1000);
     cy.findAllByLabelText('Alternative text').type(faker.lorem.sentence(), { force: true });
+
     const resizePoint = cy.get('span.cropper-face.cropper-move');
     resizePoint.scrollIntoView();
     cy.scrollToSelector('.image-widget-data');
     cy.window().then((window) => {
+      const POINTER_DOWN = window.PointerEvent ? 'pointerdown' : 'mousedown';
+      const POINTER_MOVE = window.PointerEvent ? 'pointermove' : 'mousemove';
+      const POINTER_UP = window.PointerEvent ? 'pointerup' : 'mouseup';
       const jQuery = window.jQuery;
-      const cropper = jQuery("[data-drupal-iwc=wrapper]").data('ImageWidgetCrop').types[0].cropper;
+      const cropperType = jQuery("[data-drupal-iwc=wrapper]").data('ImageWidgetCrop').types[0];
+      const cropper = cropperType.cropper;
       const { dragBox } = cropper;
       const $wrapper = jQuery(dragBox).closest('.crop-preview-wrapper');
       const $cropBox = $wrapper.find('.cropper-crop-box');
       const $points = $wrapper.find('.cropper-point');
-      jQuery(dragBox)
-        .on('mouseover', (event) => {
-          console.log('mouseover');
-        })
-        .on('pointerdown', (event) => {
-          console.log('pointerdown');
-        })
-        .on('pointermove', (event) => {
-          console.log('pointermove');
-        })
-        .on('pointerup', (event) => {
-          console.log('pointerup');
-        });
-      $cropBox
-        .on('cropStart', (event) => {
-          console.log('cropStart');
-        })
-        .on('cropMove', (event) => {
-          console.log('cropMove');
-        })
-        .on('cropEnd', (event) => {
-          console.log('cropEnd');
-        });
-      cy.wrap($cropBox)
+      const moveBox = jQuery('.cropper-face.cropper-move')[0].getBoundingClientRect();
+      cy.wrap(dragBox)
         .trigger('mouseover', { force: true })
-        .trigger('pointerdown', { which: 1, force: true })
-        .trigger('pointermove', { clientX: 5, clientY: 5, force: true })
-        .trigger('pointermove', { clientX: 35, clientY: 35, force: true })
-        .trigger('pointerup', { force: true });
-      cy.wrap($wrapper)
-        .trigger('mouseover', { force: true })
-        .trigger('pointerdown', { which: 1, force: true })
-        .trigger('pointermove', { clientX: 5, clientY: 5, force: true })
-        .trigger('pointermove', { clientX: 35, clientY: 35, force: true })
-        .trigger('pointerup', { force: true });
-      cy.wait(1000);
+        .wait(100)
+        .trigger(POINTER_DOWN, { which: 1, force: true })
+        .wait(100)
+        .trigger(POINTER_MOVE, 15, 15, { which: 1, force: true })
+        .wait(100)
+        .trigger(POINTER_MOVE, 50, 50, { which: 1, force: true })
+        .wait(100)
+        .trigger(POINTER_UP, { which: 1, force: true });
+      cy.get('form.media-form').find('input#edit-submit').click();
     });
   }
 };
