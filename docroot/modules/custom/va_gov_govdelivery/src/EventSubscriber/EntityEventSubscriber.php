94b5391acdc2d83a41b658281dd7dea63aedb3cb
Adrian Rollett <acrollet@gmail.com>
2021-02-24 16:08:00 -0700

    VACMS-4203: remove computed fields and dependent code (#4364)
    * Add test for published node with "send email update" unchecked
    
    * Add setUp method
    
    * Add dataProvider
    
    * *Really* add the dataProvider
    
    * Add hook_event_dispatcher module
    
    * Improve phpdocs
    
    * Render and test bulletin messages
    
    * Use event subscribers instead of hooks & move computed field logic to bulletin processing service
    
    * Fix event subscriber
    
    * Use correct event class
    
    * Use DI in processStatusBulletin
    
    * Code housekeeping
    
    * Inject Entity Type Manager service
    
    * Inject Entity Type Manager service
    
    * Improve readability
    
    * Make phpcs happy.
    
    * Remove banner alert computed field
    
    * Remove computed_field module
    
    * 🤓
    
    * Oh yeah, the spec tool
    
    * Fix lockfile
    
    * Use generic nomenclature in subscriber
    
    * Update docroot/modules/custom/va_gov_govdelivery/src/EventSubscriber/EntityEventSubscriber.php
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>
    
    * Restore missing line
    
    * Refactor for readability
    
    * Use node-specific form alter hook
    
    * Consistent quote style
    
    * Improve form_alter logic
    
    * Use constants for send types
    
    * Restore computed_field module to allow deployment to work
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>

---
+++ docroot/modules/custom/va_gov_govdelivery/src/EventSubscriber/EntityEventSubscriber.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\va_gov_govdelivery\EventSubscriber;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\core_event_dispatcher\Event\Entity\AbstractEntityEvent;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\node\NodeInterface;
+use Drupal\va_gov_govdelivery\Service\ProcessStatusBulletin;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * GovDelivery Entity Event Subscriber.
+ */
+class EntityEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Status bulletin processing service.
+   *
+   * @var \Drupal\va_gov_govdelivery\Service\ProcessStatusBulletin
+   */
+  protected $processStatusBulletin;
+
+  /**
+   * Constructs a new EventSubscriber object.
+   *
+   * @var \Drupal\va_gov_govdelivery\Service\ProcessStatusBulletin $process_status_bulletin
+   *   The bulletin processing service.
+   */
+  public function __construct(ProcessStatusBulletin $process_status_bulletin) {
+    $this->processStatusBulletin = $process_status_bulletin;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() : array {
+    return [
+      HookEventDispatcherInterface::ENTITY_INSERT => 'handleEntityUpsert',
+      HookEventDispatcherInterface::ENTITY_UPDATE => 'handleEntityUpsert',
+    ];
+  }
+
+  /**
+   * React to entity insert/update events.
+   *
+   * @param Drupal\core_event_dispatcher\Event\Entity\AbstractEntityEvent $event
+   *   The dispatched event.
+   */
+  public function handleEntityUpsert(AbstractEntityEvent $event) : void {
+    $entity = $event->getEntity();
+    if ($this->isStatusUpdateNode($entity)) {
+      $this->processStatusBulletin->processNode($entity);
+    }
+  }
+
+  /**
+   * Determine if this is a status update node.
+   *
+   * @param Drupal\Core\Entity\EntityInterface $entity
+   *   The entity being created or updated.
+   *
+   * @return bool
+   *   Whether or not this is an status update node.
+   */
+  protected function isStatusUpdateNode(EntityInterface $entity) : bool {
+    return ($entity instanceof NodeInterface) &&
+      ($entity->getType() === 'full_width_banner_alert');
+  }
+
+}
