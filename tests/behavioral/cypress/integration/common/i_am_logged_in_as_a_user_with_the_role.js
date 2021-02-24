eb747376a477b0e89e370092bfa40b3253a77c28
Nathan Douglas <ndouglas@users.noreply.github.com>
2021-02-24 16:04:44 -0500

    VACMS-2743: Fix image cropping issues. (#4348)
    * VACMS-2743: Fix image cropping issues.
    
    * WIP on diffing images.
    
    * WIP (Not working)
    
    * Adds environment variable when running interactively.
    
    Let's throw a little extra state in, WCGW?
    
    * Working image-cropping in media creation
    
    omg
    
    * Use `lando drush` if running interactively.
    
    * Adds scrollToSelector() command that behaves little more intuitively with smooth scrolling.
    
    Not that I ended up using it.
    
    * Adds pixelmatch and pngjs libraries for direct comparison of images.
    
    * Fixes a misplaced option.
    
    * Fixture for properly generated thumbnails.
    
    * Alias the created user's username and password.
    
    * Adds a step definition for viewing a particular image derivative.
    
    * Adds aliases to i_create_a_media step definition, including aliases.
    
    * Adds the_derivative_of_the_media_image_should_match_the_fixture step definition.
    
    * Adds derivative comparison step to image crop test.
    
    * Set appserver container to use host IPC namespace...
    to prevent Chromium from OOM'ing during more demanding tests.
    
    * Add some more source images for tests.
    
    * Adds an assertion to the derivative-fixture-matching step definition.
    
    * Adds that second test.
    
    * Apparently working solution.
    
    * Rewrites Cypress command I inadvertently deleted.
    
    * Moves image_style_warmer stuff to be later in the va_gov_backend module file.  IDK, seems less obnoxious that way.
    
    * Didn't realize that my config work was redundant.  TIL.
    
    * Update docroot/modules/custom/va_gov_backend/va_gov_backend.module
    
    Co-authored-by: Mike Chelen (he/him) <michael.chelen@va.gov>
    
    * Moves hooks to va_gov_media module, improves styling.
    
    * What's a couple of guard clauses between friends, eh?
    
    * Refinements to get tests to pass.
    
    * Movin' on up (movin' on up) that guard clause
    
    * Functions are more functional when parentheses are included
    
    * Update docroot/modules/custom/va_gov_media/va_gov_media.module
    
    Co-authored-by: Adrian Rollett <acrollet@gmail.com>
    
    * Update tests/behavioral/cypress/integration/common/the_derivative_of_the_media_image_should_match_the_fixture.js
    
    Co-authored-by: Adrian Rollett <acrollet@gmail.com>
    
    * Update tests/behavioral/cypress/integration/common/the_derivative_of_the_media_image_should_match_the_fixture.js
    
    Co-authored-by: Adrian Rollett <acrollet@gmail.com>
    
    * Some suggested changes from Adrian.
    
    Co-authored-by: Mike Chelen (he/him) <michael.chelen@va.gov>
    Co-authored-by: Adrian Rollett <acrollet@gmail.com>

--- tests/behavioral/cypress/integration/common/i_am_logged_in_as_a_user_with_the_role.js
+++ tests/behavioral/cypress/integration/common/i_am_logged_in_as_a_user_with_the_role.js
@@ -4,6 +4,8 @@ import faker from "faker";
 Then(`I am logged in as a user with the {string} role`, (text) => {
   let username = 'test_' + faker.internet.userName();
   let password = faker.internet.password();
+  cy.wrap(username).as('username');
+  cy.wrap(password).as('password');
   cy.drupalAddUserWithRole(text, username, password);
   cy.drupalLogin(username, password);
 });
