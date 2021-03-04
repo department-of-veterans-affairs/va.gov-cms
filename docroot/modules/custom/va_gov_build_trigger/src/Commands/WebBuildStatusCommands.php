732c9ab6f537416e207413aec0b0c712739915cc
Nathan Douglas <nathan.douglas@agile6.com>
2021-03-04 13:03:53 -0500

    Adds Drush (10) commands for getting/setting web-build-status.

---
+++ docroot/modules/custom/va_gov_build_trigger/src/Commands/WebBuildStatusCommands.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\va_gov_build_trigger\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\va_gov_build_trigger\WebBuildStatusInterface;
+
+/**
+ * A Drush interface to the Web Build Status service.
+ */
+class WebBuildStatusCommands extends DrushCommands {
+
+  /**
+   * The Site Status service.
+   *
+   * @var \Drupal\va_gov_build_trigger\WebBuildStatusInterface
+   */
+  protected $webBuildStatus;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\va_gov_build_trigger\WebBuildStatusInterface $webBuildStatus
+   *   The site status service.
+   */
+  public function __construct(WebBuildStatusInterface $webBuildStatus) {
+    $this->webBuildStatus = $webBuildStatus;
+  }
+
+  /**
+   * Get the web build status.
+   *
+   * @command va-gov:get-web-build-status
+   * @aliases va-gov-get-web-build-status
+   */
+  public function getWebBuildStatus() {
+    echo ($this->webBuildStatus->getWebBuildStatus() ? 'ENABLED' : 'DISABLED') . PHP_EOL;
+  }
+
+  /**
+   * Set the web build status.
+   *
+   * @param string $status
+   *   Will be coerced to an int or string.
+   *
+   * @command va-gov:set-web-build-status
+   * @aliases va-gov-set-web-build-status
+   */
+  public function setWebBuildStatus(string $status) {
+    $status = filter_var($status, FILTER_VALIDATE_BOOLEAN);
+    $this->webBuildStatus->setWebBuildStatus($status);
+    if ($this->webBuildStatus->getWebBuildStatus() !== $status) {
+      throw new \Exception('Failed to set Web Build Status.');
+    }
+    $this->logger->success(dt('Web Build Status is now :status.', [
+      ':status' => ($status ? 'active' : 'inactive'),
+    ]));
+  }
+
+}
