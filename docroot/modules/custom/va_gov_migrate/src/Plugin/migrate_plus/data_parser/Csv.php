86abec91bc04f8ae895c6486948772128b8a8ec0
Adrian Rollett <acrollet@gmail.com>
2021-03-05 18:54:32 +0000

    Minor cleanup

--- docroot/modules/custom/va_gov_migrate/src/Plugin/migrate_plus/data_parser/Csv.php
+++ docroot/modules/custom/va_gov_migrate/src/Plugin/migrate_plus/data_parser/Csv.php
@@ -223,7 +223,7 @@ class Csv extends DataParserPluginBase implements ContainerFactoryPluginInterfac
     $delimiter = (!empty($this->configuration['delimiter'])) ? $this->configuration['delimiter'] : ',';
     $enclosure = (!empty($this->configuration['enclosure'])) ? $this->configuration['enclosure'] : '';
     $escape = (!empty($this->configuration['escape'])) ? $this->configuration['escape'] : '';
-    $header_offset = (!empty($this->configuration['header_offset'])) ? $this->configuration['header_offset'] : NULL;
+    $header_offset = (!empty($this->configuration['header_offset'])) ? ($this->configuration['header_offset'] - 1) : NULL;
     $headers = $this->getHeaders();
 
     // Create the League CSV reader object to handle parsing and reading.
@@ -241,7 +241,7 @@ class Csv extends DataParserPluginBase implements ContainerFactoryPluginInterfac
       $reader->setEscape($escape);
     }
     if (!empty($header_offset) && is_numeric($header_offset)) {
-      $reader->setHeaderOffset(($header_offset - 1));
+      $reader->setHeaderOffset($header_offset);
     }
 
     $data = [];
