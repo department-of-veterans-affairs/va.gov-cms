4f34ffe168d5bd9f8f2b6cae0e3fea91b05ca0e2
Nathan Douglas <nathan.douglas@agile6.com>
2021-03-05 12:36:20 -0500

    Refactors Lando and Tugboat environments to use the queue service instead of the trait.

--- docroot/modules/custom/va_gov_build_trigger/src/Plugin/VAGov/Environment/Tugboat.php
+++ docroot/modules/custom/va_gov_build_trigger/src/Plugin/VAGov/Environment/Tugboat.php
@@ -2,13 +2,13 @@
 
 namespace Drupal\va_gov_build_trigger\Plugin\VAGov\Environment;
 
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\va_gov_build_trigger\CommandExportable;
+use Drupal\va_gov_build_trigger\Command\CommandRunner;
 use Drupal\va_gov_build_trigger\Environment\EnvironmentPluginBase;
 use Drupal\va_gov_build_trigger\Form\TugboatBuildTriggerForm;
+use Drupal\va_gov_build_trigger\Service\WebBuildCommandQueueInterface;
 use Drupal\va_gov_build_trigger\WebBuildCommandBuilder;
 use Drupal\va_gov_build_trigger\WebBuildStatusInterface;
-use Drupal\va_gov_build_trigger\Command\CommandRunner;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -22,15 +22,14 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class Tugboat extends EnvironmentPluginBase {
   use CommandRunner;
-  use QueueHelper;
   use CommandExportable;
 
   /**
-   * The queue storage manager.
+   * The queue service.
    *
-   * @var \Drupal\Core\Entity\EntityStorageInterface
+   * @var \Drupal\va_gov_build_trigger\Service\WebBuildCommandQueueInterface
    */
-  protected $queueLoader;
+  protected $queueService;
 
   /**
    * {@inheritDoc}
@@ -42,10 +41,10 @@ class Tugboat extends EnvironmentPluginBase {
     LoggerInterface $logger,
     WebBuildStatusInterface $webBuildStatus,
     WebBuildCommandBuilder $webBuildCommandBuilder,
-    EntityStorageInterface $queueLoader
+    WebBuildCommandQueueInterface $queueService
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $logger, $webBuildStatus, $webBuildCommandBuilder);
-    $this->queueLoader = $queueLoader;
+    $this->queueService = $queueService;
   }
 
   /**
@@ -59,7 +58,7 @@ class Tugboat extends EnvironmentPluginBase {
       $container->get('logger.factory')->get('va_gov_build_trigger'),
       $container->get('va_gov.build_trigger.web_build_status'),
       $container->get('va_gov.build_trigger.web_build_command_builder'),
-      $container->get('entity_type.manager')->getStorage('advancedqueue_queue')
+      $container->get('va_gov_build_trigger.web_build_command_queue')
     );
   }
 
@@ -67,17 +66,15 @@ class Tugboat extends EnvironmentPluginBase {
    * {@inheritDoc}
    */
   public function triggerFrontendBuild(string $front_end_git_ref = NULL, bool $full_rebuild = FALSE) : void {
-    /** @var \Drupal\advancedqueue\Entity\QueueInterface $queue */
-    $queue = $this->queueLoader->load('command_runner');
-
     if ($full_rebuild && $this->webBuildCommandBuilder->useContentExport()) {
-      $commands = [$this->getExportCommand()];
-      $this->queueCommands($commands, $queue);
+      $this->queueService->enqueueCommands([
+        $this->getExportCommand(),
+      ]);
     }
 
     // A new command variable since the rebuild commands has been queued.
     $commands = $this->webBuildCommandBuilder->buildCommands($front_end_git_ref);
-    $this->queueCommands($commands, $queue);
+    $this->queueService->enqueueCommands($commands);
 
     $this->messenger()->addStatus('A request to rebuild the front end has been submitted.');
   }
