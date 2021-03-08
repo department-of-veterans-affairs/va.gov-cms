a045b57266d0211a133125268b13934b69d7c6a4
Neil Hastings <neil.hastings@agile6.com>
2021-03-08 16:37:27 -0500

    Add permissions to Consumer role test

--- tests/phpunit/Security/RolesPermissionsTest.php
+++ tests/phpunit/Security/RolesPermissionsTest.php
@@ -178,6 +178,7 @@ class RolesPermissionsTest extends ExistingSiteBase {
           'access bulletin queue trigger api',
           'access openapi api docs',
           'administer menu',
+          'bypass graphql field security',
           'use graphql explorer',
           'use graphql voyager',
           'view any unpublished content',
