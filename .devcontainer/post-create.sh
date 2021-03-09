67da6c2f54f6f0586d6b7862b91cce453fcbff31
Mike Chelen (he/him) <michael.chelen@va.gov>
2021-03-09 00:45:57 +0000

    add lando install script

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# download lando https://docs.lando.dev/basics/installation.html#linux
+wget -O ./tmp/lando-stable.deb https://files.devwithlando.io/lando-stable.deb
+
+# install lando https://docs.lando.dev/basics/installation.html#docker-ce
+sudo dpkg -i --ignore-depends=docker-ce ./tmp/lando-stable.deb
