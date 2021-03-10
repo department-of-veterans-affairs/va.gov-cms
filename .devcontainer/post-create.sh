0453d07ceb4a1ed00d62d7054b8288ff13a3409b
Adrian Rollett <acrollet@gmail.com>
2021-03-10 11:52:41 -0700

    Add upstream git remote

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,3 +5,6 @@ wget -O ./tmp/lando-stable.deb https://files.devwithlando.io/lando-stable.deb
 
 # install lando https://docs.lando.dev/basics/installation.html#docker-ce
 sudo dpkg -i --ignore-depends=docker-ce ./tmp/lando-stable.deb
+
+# Add upstream git remote.
+cd ~/workspace/va.gov-cms && git remote add upstream https://github.com/department-of-veterans-affairs/va.gov-cms.git
