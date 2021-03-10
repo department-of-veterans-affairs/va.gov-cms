b18bd66a5f496f45c2d2f47e12b5ea82f330b8e9
Adrian Rollett <acrollet@gmail.com>
2021-03-10 19:07:19 +0000

    Add codespaces config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# download lando https://docs.lando.dev/basics/installation.html#linux
-wget -O ./tmp/lando-stable.deb https://files.devwithlando.io/lando-stable.deb
+# Download lando https://docs.lando.dev/basics/installation.html#linux
+wget -O /tmp/lando-stable.deb https://files.devwithlando.io/lando-stable.deb
 
-# install lando https://docs.lando.dev/basics/installation.html#docker-ce
-sudo dpkg -i --ignore-depends=docker-ce ./tmp/lando-stable.deb
+# Install lando https://docs.lando.dev/basics/installation.html#docker-ce
+sudo dpkg -i --ignore-depends=docker-ce /tmp/lando-stable.deb
+
+# Remove lando package.
+rm /tmp/lando-stable.deb
 
 # Add upstream git remote.
 cd ~/workspace/va.gov-cms && git remote add upstream https://github.com/department-of-veterans-affairs/va.gov-cms.git
