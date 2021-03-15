7a9e98edd3462fe7e1805fb56f7c5564949c2c4e
Nathan Douglas <ndouglas@users.noreply.github.com>
2021-03-15 11:23:43 -0400

    VACMS 4110: Improve content release integration with AdvancedQueue Part Deux (#4620)
    * Removes Drush 8 commands from va_gov_build_trigger.
    
    * Adds Composer command to wrap running the Advanced Queue.
    
    * Behavioral tests in progress.
    
    * Adds WebBuildCommandBuilderInterface.
    
    * Adds BuildFrontendInterface.
    
    * Adds Drush 10 Web Build commands.
    
    * Takes a stab at actually addressing the primary AC of the PR.
    
    * Use WebBuildCommandBuilderInterface instead of WebBuildCommandBuilder in some of the things.
    
    * Use 'callable', not 'callback' in phpdoc
    
    * Fix a slightly misleading return document.
    
    * Adds missing use statement that might've been screwing things up in the Preview form.
    
    * Fixes incorrect documentation.
    
    * Adds some Drush commands for viewing and manipulating the queue and its jobs.
    
    * Fixes an (assumed) typo in a state key.
    
    * Moves setting/resetting the web build status out of composer.json, since it's handled in the web build job type now.
    
    * Adds some Composer scripts for dealing with web build tasks.
    
    * D'oh, fixes composer.json I just borked.
    
    * D'oh, disable web build status after we succeed :P
    
    * Adds an i_clear_the_web_build_queue step definition to make tests more reliable.  At least for Cypress :-/
    
    * Make content release Cypress tests more sane!
    
    * Adds Cypress step definitions and tests for initiating content releases from the command line.
    
    * Was running the command-built process unnecessarily when its results were only used in --dry-run
    
    * Clear web build queue at the end of behavioral tests so that it doesn't throw off execution of other tests?  IDK.
    
    * Adds Composer script for enqueueing a build command.
    
    * Shorthand for checking web build status.
    
    * Removes Composer scripts that just wrap Drush commands.
    
    * Adds documentation for Drush va_gov_build_trigger commands.
    
    * Friendlier delimiters for find-and-replace in Composer commands.  h/t @acrollet
    
    * Fixes dumb thing I did.
    
    Co-authored-by: Adrian Rollett <acrollet@gmail.com>

---
+++ docroot/modules/custom/va_gov_build_trigger/src/Service/BuildFrontendInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\va_gov_build_trigger\Service;
+
+use Drupal\node\NodeInterface;
+
+/**
+ * Class for processing facility status to GovDelivery Bulletin.
+ */
+interface BuildFrontendInterface {
+
+  /**
+   * Triggers the appropriate frontend Build based on the environment.
+   *
+   * @param string $front_end_git_ref
+   *   Front end git reference to build (branch name or PR number).
+   * @param bool $full_rebuild
+   *   Trigger a full content export rebuild.
+   */
+  public function triggerFrontendBuild(string $front_end_git_ref = NULL, bool $full_rebuild = FALSE) : void;
+
+  /**
+   * Set the config state of build pending.
+   *
+   * @param bool $state
+   *   The state that should be set for build pending.
+   */
+  public function setPendingState(bool $state) : void;
+
+  /**
+   * Method to trigger a frontend build as the result of a save.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node object of a node just updated or saved.
+   */
+  public function triggerFrontendBuildFromContentSave(NodeInterface $node);
+
+}
