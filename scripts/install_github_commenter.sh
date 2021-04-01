26d5de7ce7585a5e1933d8a182a11e4b27b50a64
Nathan Douglas <nathan.douglas@agile6.com>
2021-04-01 08:59:44 -0400

    Args are for wimps.

--- scripts/install_github_commenter.sh
+++ scripts/install_github_commenter.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e;
-: "${1?"Usage: $0 DESTINATION"}";
-destination="${1}";
+destination="${1:-./bin}";
 version="${2:-0.8.0}";
 executable_path="${destination}/github-commenter";
 curl --location "https://github.com/cloudposse/github-commenter/releases/download/${version}/github-commenter_linux_amd64" --output "${executable_path}";
