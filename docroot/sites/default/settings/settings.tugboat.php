d02561b5836b654bea8d894bf0df17ba6b20270c
Nathan Douglas <nathan.douglas@agile6.com>
2021-04-13 08:52:37 -0400

    VACMS-4927: Configure Memcache support in Tugboat.

--- docroot/sites/default/settings/settings.tugboat.php
+++ docroot/sites/default/settings/settings.tugboat.php
@@ -57,3 +57,13 @@ $settings['va_gov_frontend_url'] = getenv('FRONTEND_ADDRESS');
 $settings['va_gov_frontend_build_type'] = 'tugboat';
 $settings['va_gov_app_root'] = getenv('TUGBOAT_ROOT');
 $settings['va_gov_web_root'] = getenv('TUGBOAT_ROOT') . '/web';
+
+if (extension_loaded('memcache')) {
+  $settings['memcache']['servers'] = [
+    'memcache:11211' => 'default',
+  ];
+  $settings['cache']['default'] = 'cache.backend.memcache';
+  $settings['memcache']['bins'] = [
+    'default' => 'default',
+  ];
+}
