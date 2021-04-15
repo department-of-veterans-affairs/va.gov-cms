e869e736a340487db09a19691a27b9bb1b91903c
Nathan Douglas <nathan.douglas@agile6.com>
2021-04-15 10:12:39 -0400

    Swaps. the. parameters. in. array_map(). :weary:

--- docroot/sites/default/settings/settings.brd_common.php
+++ docroot/sites/default/settings/settings.brd_common.php
@@ -7,9 +7,9 @@ if (extension_loaded('memcache')) {
   $memcache_nodes = getenv('CMS_MEMCACHE_NODES');
   if (!empty($memcache_nodes)) {
     $memcache_servers = explode(',', $memcache_nodes);
-    $memcache_servers = array_map($memcache_servers, function ($memcache_server) {
+    $memcache_servers = array_map(function ($memcache_server) {
       return trim($memcache_server) . ':11211';
-    });
+    }, $memcache_servers);
     $settings['memcache']['servers'] = [];
     foreach ($memcache_servers as $memcache_server) {
       $settings['memcache']['servers'][$memcache_server] = 'default';
