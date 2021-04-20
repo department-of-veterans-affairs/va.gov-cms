c4365c9c704cc42b91901a71ce2b1172312aa71f
Nathan Douglas <nathan.douglas@agile6.com>
2021-04-20 09:05:01 -0400

    Adds lock support in Memcache.

--- docroot/sites/default/settings/settings.tugboat.php
+++ docroot/sites/default/settings/settings.tugboat.php
@@ -66,4 +66,5 @@ if (extension_loaded('memcache')) {
   $settings['memcache']['bins'] = [
     'default' => 'default',
   ];
+  $settings['container_yamls'][] = $app_root . '/' . $site_path . '/../default/services/services.memcache.yml';
 }
