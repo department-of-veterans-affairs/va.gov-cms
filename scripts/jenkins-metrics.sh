bb77d0007b912fb2064ace7a2e71f907e01177e2
Mike Chelen <Michael.Chelen@va.gov>
2021-04-22 17:43:08 -0400

    capture more build info

--- scripts/jenkins-metrics.sh
+++ scripts/jenkins-metrics.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
 
-# assumes socks is running on port 2001
+# requirements
+
+# socks proxy running on port 2001
+
+# jq command must be installed
 
 # add to ~/.bash_profile or other shell:
-
 # export JENKINS_USERNAME=your-name
 # export JENKINS_API_KEY=yourapikey
 
-# bash ./jenkins-collect-metrics.sh
 
-
-LOG=$(curl -X POST -L \
+BUILD_INFO=`curl -s -X POST -L \
     --socks5-hostname localhost:2001 \
     --user $JENKINS_USERNAME:$JENKINS_API_KEY \
-    http://jenkins.vfs.va.gov/job/builds/job/vets-website-content-vagovprod/1216/consoleText)
+    http://jenkins.vfs.va.gov/job/builds/job/vets-website-content-vagovprod/lastSuccessfulBuild/api/json`
 
-echo $LOG
+BUILD_NUMBER=`echo $BUILD_INFO | jq '.number'`
+    
+BUILD_DURATION=`echo $BUILD_INFO | jq '.duration'` # msec
+
+BUILD_TIMESTAMP=`echo $BUILD_INFO | jq '.timestamp'`
+
+
+
+
+
+BUILD_LOG=`curl -s -X POST -L \
+    --socks5-hostname localhost:2001 \
+    --user $JENKINS_USERNAME:$JENKINS_API_KEY \
+    http://jenkins.vfs.va.gov/job/builds/job/vets-website-content-vagovprod/lastSuccessfulBuild/consoleText`
+
+
+
+GQL_TIME=`echo $BUILD_LOG | grep -oP 'queries in \d+s' | grep -oP '\d+'` # seconds
+
+BUILD_TIME=`echo $BUILD_LOG | grep -oP 'Done in \d+\.\d+s' | grep -oP '\d+\.\d+' | tail -1` # seconds
+
+
+printf "build number: $BUILD_NUMBER
+build duration (msec): $BUILD_DURATION
+build timestamp: $BUILD_TIMESTAMP
+gql query time (s): $GQL_TIME
+build time (s): $BUILD_TIME
+"
+
+
+
+
