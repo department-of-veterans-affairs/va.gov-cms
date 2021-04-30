97b5d71d1d374d2d39b6bc7692f46d58905f562d
Neil Hastings <neil.hastings@agile6.com>
2021-04-30 08:15:35 -0400

    Change job to correct prefix

--- docroot/sites/default/settings/settings.staging.php
+++ docroot/sites/default/settings/settings.staging.php
@@ -4,7 +4,7 @@
 
 include dirname(__FILE__) . '/settings.brd_common.php';
 
-$settings['jenkins_build_job_path'] = '/job/builds/job/content-build-content-only-vagovdev' . $settings['jenkins_build_env'];
+$settings['jenkins_build_job_path'] = '/job/builds/job/content-build-content-only-vagov' . $settings['jenkins_build_env'];
 $settings['jenkins_build_job_params'] = '/buildWithParameters?deploy=true';
 $settings['jenkins_build_job_url'] = $settings['jenkins_build_job_host'] . $settings['jenkins_build_job_path'] . $settings['jenkins_build_job_params'];
 
