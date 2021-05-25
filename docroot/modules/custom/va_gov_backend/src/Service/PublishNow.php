32ba7956387eec8f45511a7704ed5743a0845bd9
Nathan Douglas <nathan.douglas@agile6.com>
2021-05-25 16:34:20 -0400

    Adds some logic, yo.

--- docroot/modules/custom/va_gov_backend/src/Service/PublishNow.php
+++ docroot/modules/custom/va_gov_backend/src/Service/PublishNow.php
@@ -3,12 +3,48 @@
 namespace Drupal\va_gov_backend\Service;
 
 use Drupal\Core\Url;
+use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\node\NodeInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Collects logic related to the "Publish Now" button.
  */
 class PublishNow implements PublishNowInterface {
+
+  /**
+   * The Current User service.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The Current Request.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestInterface
+   */
+  protected $request;
+
+  /**
+   * The VA.gov URL Service.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestInterface
+   */
+  protected $liveUrl;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(
+    AccountProxyInterface $currentUser,
+    RequestStack $requestStack,
+    VaGovUrlInterface $liveUrl
+  ) {
+    $this->currentUser = $currentUser;
+    $this->request = $requestStack->getCurrentRequest();
+    $this->liveUrl = $liveUrl;
+  }
 
   /**
    * Get the publish URL.
@@ -24,6 +60,23 @@ class PublishNow implements PublishNowInterface {
    * {@inheritDoc}
    */
   public function shouldDisplayButton(NodeInterface $node) : bool {
+    if (!$node->isPublished()) {
+      return FALSE;
+    }
+    if (!$this->currentUser->hasPermission('administer content')) {
+      return FALSE;
+    }
+    if ($this->request->getHost() === 'prod.cms.va.gov') {
+      return FALSE;
+    }
+    // phpcs:disable
+    // This code is not working as expected; will look at tests.
+    /*
+    if (!$this->liveUrl->vaGovFrontEndUrlForEntityIsLive($node)) {
+      return FALSE;
+    }
+    */
+    // phpcs:enable
     return TRUE;
   }
 
