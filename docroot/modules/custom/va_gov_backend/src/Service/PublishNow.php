453fee3358cac91bcaa010c8f0e5f65c5ee1e040
Nathan Douglas <nathan.douglas@agile6.com>
2021-05-25 16:07:06 -0400

    Removes cruft.

--- docroot/modules/custom/va_gov_backend/src/Service/PublishNow.php
+++ docroot/modules/custom/va_gov_backend/src/Service/PublishNow.php
@@ -2,40 +2,13 @@
 
 namespace Drupal\va_gov_backend\Service;
 
-use Drupal\Core\Routing\ResettableStackedRouteMatchInterface;
 use Drupal\Core\Url;
 use Drupal\node\NodeInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Collects logic related to the "Publish Now" button.
  */
 class PublishNow implements PublishNowInterface {
-
-  /**
-   * The route matching interface.
-   *
-   * @var \Drupal\Core\Routing\ResettableStackedRouteMatchInterface
-   */
-  protected $routeMatch;
-
-  /**
-   * The current request.
-   *
-   * @var \Symfony\Component\HttpFoundation\RequestInterface
-   */
-  protected $currentRequest;
-
-  /**
-   * Constructor.
-   */
-  public function __construct(
-    ResettableStackedRouteMatchInterface $routeMatch,
-    RequestStack $requestStack
-  ) {
-    $this->routeMatch = $routeMatch;
-    $this->currentRequest = $requestStack->getCurrentRequest();
-  }
 
   /**
    * Get the publish URL.
