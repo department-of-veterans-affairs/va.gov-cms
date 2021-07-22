11ab8200b851c1473e881682156558e34faf3229
Nathan Douglas <nathan.douglas@agile6.com>
2021-07-22 06:22:51 -0400

    VACMS-5650: Updates to behavioral tests

--- tests/behavioral/cypress/integration/common/i_create_a_node.js
+++ tests/behavioral/cypress/integration/common/i_create_a_node.js
@@ -8,7 +8,7 @@ const creators = {
     cy.findAllByLabelText('Page title').type('[Test Data] ' + faker.lorem.sentence(), { force: true });
     cy.findAllByLabelText('Page introduction').type(faker.lorem.sentence(), { force: true });
     cy.findAllByLabelText('Section').select('Veterans Affairs', { force: true });
-    cy.findAllByLabelText('Parent link').select('-- CMS help center', { force: true });
+    cy.findAllByLabelText('Parent link').select('-- CMS Knowledge Base (disabled)', { force: true });
     return cy.get('form.node-form').find('input#edit-submit').click();
   },
   office: () => {
