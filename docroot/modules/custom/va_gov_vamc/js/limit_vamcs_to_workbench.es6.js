916c4cf49b1f41a7e6309931702b2252c3a93d7e
Ethan Teague <metrowebz@gmail.com>
2021-07-23 13:40:48 -0400

    VACMS-5912: Limit banner vamc selection options.

---
+++ docroot/modules/custom/va_gov_vamc/js/limit_vamcs_to_workbench.es6.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ */
+
+((Drupal) => {
+  // Grab our fields.
+  const adminField = document.getElementById("edit-field-administration");
+
+  const checkBoxDivs = document.querySelectorAll(
+    "#edit-field-banner-alert-vamcs-wrapper div.form-type-checkbox"
+  );
+
+  const winnower = () => {
+    // Get our base match text string.
+    const adminFieldText = adminField.options[adminField.selectedIndex].text;
+    // Get our search string from the field text.
+    const adminMatcher = adminFieldText.replace(/(^-+)/g, "");
+
+    // Winnow options that don't contain adminMatcher.
+    checkBoxDivs.forEach((i) => {
+      // Apply reset everytime we fire.
+      const text = i.querySelector("label span.field-content").textContent;
+      i.classList.remove("hidden-option");
+      if (!text.includes(adminMatcher)) {
+        i.classList.add("hidden-option");
+      }
+    });
+  };
+
+  Drupal.behaviors.vaGovLimitServiceOptions = {
+    attach() {
+      winnower();
+      adminField.addEventListener("change", winnower);
+    },
+  };
+})(Drupal);
--- docroot/modules/custom/va_gov_vamc/src/EventSubscriber/EntityEventSubscriber.php
+++ docroot/modules/custom/va_gov_vamc/src/EventSubscriber/EntityEventSubscriber.php
@@ -22,6 +22,17 @@ class EntityEventSubscriber implements EventSubscriberInterface {
   }
 
   /**
+   * Add js script to Banner node form.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Form\FormIdAlterEvent $event
+   *   The event.
+   */
+  public function alterBannerNodeForm(FormIdAlterEvent $event): void {
+    $form = &$event->getForm();
+    $form['#attached']['library'][] = 'va_gov_vamc/limit_vamcs_to_workbench';
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents(): array {
@@ -29,6 +40,9 @@ class EntityEventSubscriber implements EventSubscriberInterface {
       // React on Op status forms.
       'hook_event_dispatcher.form_node_vamc_operating_status_and_alerts_form.alter' => 'alterOpStatusNodeForm',
       'hook_event_dispatcher.form_node_vamc_operating_status_and_alerts_edit_form.alter' => 'alterOpStatusNodeForm',
+      // React on banner forms.
+      'hook_event_dispatcher.form_node_full_width_banner_alert_form.alter' => 'alterBannerNodeForm',
+      'hook_event_dispatcher.form_node_full_width_banner_alert_edit_form.alter' => 'alterBannerNodeForm',
     ];
   }
 
