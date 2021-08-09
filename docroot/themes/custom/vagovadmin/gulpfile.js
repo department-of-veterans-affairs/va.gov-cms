fb6ef22108f79c845e76c30210665ed1f20d70d8
Nathan Douglas <ndouglas@users.noreply.github.com>
2021-08-09 11:48:49 -0400

    VACMS 5548: Resolve some Dependabot vulnerability alerts. (#6049)
    * Updates gulp-sass to 5.0.
    
    * Some more security alerts.
    
    * Includes changed assets.
    
    Co-authored-by: Adrian Rollett <acrollet@gmail.com>

--- docroot/themes/custom/vagovadmin/gulpfile.js
+++ docroot/themes/custom/vagovadmin/gulpfile.js
@@ -6,7 +6,7 @@
 **/
 
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+var sass = require("gulp-sass")(require("sass"));
 var autoprefixer = require("gulp-autoprefixer");
 var sourcemaps = require("gulp-sourcemaps");
 
