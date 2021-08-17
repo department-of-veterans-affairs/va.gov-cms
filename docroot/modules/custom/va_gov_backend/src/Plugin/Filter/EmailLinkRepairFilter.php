0a99cd7f71bc9142db15bb4e9bf5a2d8f273ce43
Nathan Douglas <nathan.douglas@agile6.com>
2021-08-17 08:22:34 -0400

    Adds tests, removes references to unused class.

--- docroot/modules/custom/va_gov_backend/src/Plugin/Filter/EmailLinkRepairFilter.php
+++ docroot/modules/custom/va_gov_backend/src/Plugin/Filter/EmailLinkRepairFilter.php
@@ -2,12 +2,9 @@
 
 namespace Drupal\va_gov_backend\Plugin\Filter;
 
-use Drupal\Core\Path\PathValidatorInterface;
 use Drupal\Component\Utility\Html;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Repairs formatting of email address links.
@@ -22,43 +19,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE
  * )
  */
-class EmailLinkRepairFilter extends FilterBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The path validator.
-   *
-   * @var \Drupal\Core\Path\PathValidatorInterface
-   */
-  protected $pathValidator;
-
-  /**
-   * Constructs a Email Link Repair Filter object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
-   *   The path validator.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, PathValidatorInterface $path_validator) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->pathValidator = $path_validator;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('path.validator')
-    );
-  }
+class EmailLinkRepairFilter extends FilterBase {
 
   /**
    * {@inheritdoc}
