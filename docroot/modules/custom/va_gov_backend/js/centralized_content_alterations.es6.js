fd3075bede167f16bf26836b1abf35c25f8be6df
Ethan Teague <metrowebz@gmail.com>
2021-10-22 01:00:17 -0400

    VACMS-4016: Update cc page to display differently for non-admins. (#6707)
    * VACMS-4016: Update cc page to display differently for non-admins.
    
    * VACMS-4016: Disable top fields for non-admins.
    
    * Update docroot/modules/custom/va_gov_user/src/Service/UserPermsService.php
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>
    
    * Update docroot/modules/custom/va_gov_backend/src/EventSubscriber/EntityEventSubscriber.php
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>
    
    * Update docroot/modules/custom/va_gov_backend/src/EventSubscriber/EntityEventSubscriber.php
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>
    
    * VACMS-4016 Fix broken string handling.
    
    * Rearrange array order for phpcs.
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>
    Co-authored-by: Steve Wirt <swirtMiles@138230.no-reply.drupal.org>

---
+++ docroot/modules/custom/va_gov_backend/js/centralized_content_alterations.es6.js
@@ -0,0 +1,75 @@
+/**
+ * @file
+ */
+
+((Drupal) => {
+  const fieldTreatments = (dom) => {
+    CKEDITOR.config.autoGrow_onStartup.valueOf = 750;
+    const adminRoles = ["administrator"];
+    // If national content and user isn't admin, start operating.
+    if (
+      drupalSettings.gtm_data.contentType === "centralized_content" &&
+      !adminRoles.some((item) =>
+        drupalSettings.gtm_data.userRoles.includes(item)
+      )
+    ) {
+      // Grab our National descriptor paragraphs.
+      const ccParagraphs = dom.querySelectorAll(
+        "div.cc-special-treatment-paragraph.centralized_content_descriptor"
+      );
+      // Grab our National wysiwyg paragraphs.
+      const wysiParagraphs = dom.querySelectorAll(
+        ".draggable.paragraph-type--wysiwyg"
+      );
+      // Grab our National wysiwyg textareas.
+      const wysiParagraphsText = dom.querySelectorAll(
+        "div.field--name-field-wysiwyg"
+      );
+
+      // Pull out the toggle drag handles on the descriptor paragraphs.
+      ccParagraphs.forEach((item) => {
+        if (item && item.parentElement) {
+          item.parentElement.classList.add("cc-paragraph-header");
+          if (item.parentElement.previousElementSibling) {
+            item.parentElement.previousElementSibling.classList.add(
+              "cc-paragraph-toggle-remove"
+            );
+          }
+        }
+      });
+
+      // Add special class to national content wysi paragraphs.
+      wysiParagraphs.forEach((item) => {
+        if (
+          item &&
+          item.previousElementSibling.classList.contains(
+            "paragraph-type--centralized-content-descriptor"
+          )
+        ) {
+          item.classList.add("cc-national-wysi-padding");
+        }
+      });
+
+      // Pull out the toggle drag handles on the wysiwyg paragraphs.
+      wysiParagraphsText.forEach((item) => {
+        if (
+          item &&
+          item.parentElement.parentElement.parentElement.parentElement
+            .previousElementSibling
+        ) {
+          item.parentElement.parentElement.parentElement.parentElement.previousElementSibling.classList.add(
+            "cc-paragraph-toggle-remove"
+          );
+        }
+      });
+    }
+  };
+  /**
+   * Behaviors for manipulationg elements when user shouldn't have access.
+   * */
+  Drupal.behaviors.nationalDataAccessControl = {
+    attach(context) {
+      fieldTreatments(context);
+    },
+  };
+})(Drupal);
