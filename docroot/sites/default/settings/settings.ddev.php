876ac6876bca8a2085901a8d13f92609be7d8c2d
Cameron Eagans <me@cweagans.net>
2021-10-29 12:19:08 -0600

    VACMS-6789: Add ddev configuration (#6791)
    * Add ddev configuration
    
    * Add ddev.settings.php
    
    * Update ddev custom commands so that they run in the right directory
    
    * Update pre-commit hook to work with both lando and ddev
    
    * Don't exclude .git dir from sync since the cms depends on it being present
    
    * Add ddev gitignore
    
    * Get xdebug working with ddev
    
    * Add a note to https.md about setting up local https with ddev
    
    * Disable mutagen by default

---
+++ docroot/sites/default/settings/settings.ddev.php
@@ -0,0 +1,44 @@
+<?php
+
+// phpcs:ignoreFile
+
+$settings['jenkins_build_job_path'] = '/job/builds/job/vets-website-content-vagov' . $settings['jenkins_build_env'];
+$settings['jenkins_build_job_params'] = '/buildWithParameters?deploy=true';
+$settings['jenkins_build_job_url'] = $settings['jenkins_build_job_host'] . $settings['jenkins_build_job_path'] . $settings['jenkins_build_job_params'];
+$settings['skip_permissions_hardening'] = TRUE;
+
+$config['config_split.config_split.dev']['status'] = FALSE;
+$config['config_split.config_split.stg']['status'] = FALSE;
+$config['config_split.config_split.prod']['status'] = FALSE;
+$config['config_split.config_split.local']['status'] = TRUE;
+$config['config_split.config_split.tugboat']['status'] = FALSE;
+
+$config['system.performance']['cache']['page']['use_internal'] = FALSE;
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['css']['gzip'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$config['system.performance']['js']['gzip'] = FALSE;
+$config['system.performance']['response']['gzip'] = FALSE;
+$config['views.settings']['ui']['show']['sql_query']['enabled'] = TRUE;
+$config['views.settings']['ui']['show']['performance_statistics'] = TRUE;
+$config['system.logging']['error_level'] = 'all';
+$config['environment_indicator.indicator']['bg_color'] = '#0B5512'; // dark green.
+$config['environment_indicator.indicator']['fg_color'] = '#ffffff';
+$config['environment_indicator.indicator']['name'] = 'ddev';
+
+$webhost_on_cli = 'https://va-gov-cms.ddev.site';
+
+// Link to this file locally since lando can not access prod where the real
+// file exists.  You will need to copy the file from the same path on prod.
+$config['migrate_plus.migration.va_node_form']['source']['urls'] = ['https://va-gov-cms.ddev.site/sites/default/files/migrate_source/va_forms_data.csv'];
+
+$settings['trusted_host_patterns'] = ['.*'];
+
+$settings['va_gov_frontend_build_type'] = 'lando';
+$settings['va_gov_frontend_url'] = $webhost_on_cli . '/static';
+$settings['va_gov_app_root'] = getenv('DDEV_APPROOT');
+$settings['va_gov_web_root'] = getenv('DDEV_APPROOT') . '/web';
+
+$settings['memcache']['servers'] = [
+  'memcache:11211' => 'default',
+];
