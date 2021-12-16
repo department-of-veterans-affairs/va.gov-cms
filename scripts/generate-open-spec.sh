c49fd35e617824e7f8371c1349aa3b66698d1f27
Tim Cosgrove <tim.cosgrove@agile6.com>
2021-12-15 18:47:30 -0800

    VACMS-7081: changes to the generator, two example specs.

--- scripts/generate-open-spec.sh
+++ scripts/generate-open-spec.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e;
 
-url="{$1}/jsonapi/vagov";
+url="$1/openapi/va_gov";
 tmp_name=$(mktemp)
-wget --output-document="${tmp_name}" "{$url}"
+wget --output-document="${tmp_name}" "${url}"
 mv {$tmp_name} "./docroot/openapi.json"
