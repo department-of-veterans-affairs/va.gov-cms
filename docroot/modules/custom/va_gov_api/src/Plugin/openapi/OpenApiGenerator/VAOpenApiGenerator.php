f57ba8721d4e02cf9eb336351f2299fc565409b1
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2021-12-20 17:45:55 -0800

    Vacms 7081 updated poc openapi (#7308)
    * Add jsonapi_extras and custom QandA endpont
    
    * Update openapi.yml
    
    * VACMS-6491: first pass at Banners endpoint.
    
    * VACMS-000: allow post-import-db and post-start ddev hooks to run.
    
    * VACMS-6666: adding full banner-alerts endpoint.
    
    * VACMS-6666: enabled CORS for the API POC server.
    
    * VACMS-6491: added GET to allowed methods.
    
    * VACMS-6491: Set up more explicit CORS configuration.
    
    * VACMS-6666: added paths to endpoint response, and renamed class/resource.
    
    * VACMS-6491: changed paths retrieval to more compact method.
    
    * VACMS-6666: corrected setting of cache dependency.
    
    * VACMS-6666: disabled caching for banner alert endpoint response.
    
    * VACMS-000: updated vscode config for ddev.
    
    * VACMS-7081: changes to the generator, two example specs.
    
    * VACMS-7081: remove jsonapi_extras config overrides.
    
    * VACMS-7081: update API specs to include product API endpoints and schema.
    
    * VACMS-7081: remove errant openapi.json.
    
    * VACMS-7081: pull latest main.
    
    * VACMS-7081: add openapi_ui and swagger libraries to build.
    
    * VACMS-7081: revert ddev title.
    
    * VACMS-7081: move openapi specs to shared location.
    
    * VACMS-7081: fixing and ignoring some phpstan warnings.
    
    * VACMS-7081: clean up generate spec script, add necessary packages.
    
    * VACMS-7081: allow anonymous users to make requests to API structure.
    
    Co-authored-by: Neil Hastings <neil.hastings@gmail.com>
    Co-authored-by: Neil Hastings <neil.hastings@agile6.com>

--- docroot/modules/custom/va_gov_api/src/Plugin/openapi/OpenApiGenerator/VAOpenApiGenerator.php
+++ docroot/modules/custom/va_gov_api/src/Plugin/openapi/OpenApiGenerator/VAOpenApiGenerator.php
@@ -5,6 +5,7 @@ namespace Drupal\va_gov_api\Plugin\openapi\OpenApiGenerator;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\jsonapi\ResourceType\ResourceType;
 use Drupal\openapi_jsonapi\Plugin\openapi\OpenApiGenerator\JsonApiGenerator;
+use Drupal\Core\Entity\ContentEntityTypeInterface;
 
 /**
  * Defines an OpenApi Schema Generator for the VA.
@@ -18,6 +19,32 @@ use Drupal\openapi_jsonapi\Plugin\openapi\OpenApiGenerator\JsonApiGenerator;
  * )
  */
 class VAOpenApiGenerator extends JsonApiGenerator {
+
+  /**
+   * Undocumented function.
+   */
+  public function getSpecification() {
+    $spec = parent::getSpecification();
+    // openapi_jsonapi does not provide any reasonable way to override its
+    // methods.
+    $desiredDefinitions = [
+      'node' => [
+        'banner',
+        'q_a',
+        'full_width_banner_alert',
+        'checklist',
+        'media_list_images',
+        'media_list_videos',
+        'support_resources_detail_page',
+        'faq_multiple_q_a',
+        'step_by_step',
+      ],
+    ];
+    $definitions = $this->getSpecificDefinitions($desiredDefinitions);
+    ksort($definitions);
+    $spec['definitions'] = $definitions;
+    return $spec;
+  }
 
   /**
    * {@inheritDoc}
@@ -40,7 +67,34 @@ class VAOpenApiGenerator extends JsonApiGenerator {
     $read_only_methods = ['GET', 'HEAD', 'OPTIONS', 'TRACE'];
 
     $api_paths = [];
+    // Pull the objects we're getting so we can get the routes that match.
+    $desiredDefinitions = [
+      'node' => [
+        'banner',
+        'q_a',
+        'full_width_banner_alert',
+        'checklist',
+        'media_list_images',
+        'media_list_videos',
+        'support_resources_detail_page',
+        'faq_multiple_q_a',
+        'step_by_step',
+      ],
+    ];
+    $definition_keys = array_keys($this->getSpecificDefinitions($desiredDefinitions));
     foreach ($routes as $route_name => $route) {
+      // Filter out routes that don't match our definition keys.
+      $route_matches = FALSE;
+      foreach ($definition_keys as $key) {
+        if (strpos($route_name, "$key.")) {
+          $route_matches = TRUE;
+        }
+      }
+      if (!$route_matches) {
+        continue;
+      }
+
+      $route_type = $this->getRouteTypeByName($route_name);
       if ($read_only_mode_is_enabled === TRUE) {
         $supported_methods = $route->getMethods();
         assert(count($supported_methods) > 0, 'JSON:API routes always have a method specified.');
@@ -58,6 +112,15 @@ class VAOpenApiGenerator extends JsonApiGenerator {
       $entity_type_id = $resource_type->getEntityTypeId();
       $bundle_name = $resource_type->getBundle();
       if (!$this->includeEntityTypeBundle($entity_type_id, $bundle_name)) {
+        continue;
+      }
+      // Custom.
+      // Deflect relationship routes.
+      if (in_array($route_type, ['relationship', 'related'])) {
+        continue;
+      }
+      // Menu link relationships are not correctly linked to a schema.
+      if (strpos($route_name, 'menu_link') !== FALSE) {
         continue;
       }
       $api_path = [];
@@ -88,6 +151,79 @@ class VAOpenApiGenerator extends JsonApiGenerator {
       $api_paths[$path] = NestedArray::mergeDeep(empty($api_paths[$path]) ? [] : $api_paths[$path], $api_path);
     }
     return $api_paths;
+  }
+
+  /**
+   * Get requested schema defs as well as relationship schemas.
+   *
+   * @param array $desiredDefinitions
+   *   Definitions in the form 'entitytype' => ['bundle names'].
+   *
+   * @return array
+   *   Definition structures.
+   */
+  public function getSpecificDefinitions(array $desiredDefinitions) {
+    static $definitions = [];
+    foreach ($desiredDefinitions as $entity_type_id => $bundles) {
+      $entity_type = $this->entityTypeManager->getDefinition($entity_type_id);
+      if ($entity_type instanceof ContentEntityTypeInterface) {
+        foreach ($bundles as $bundle_name) {
+          if ($bundle_name == 'banner') {
+            $foo = 'bar';
+          }
+          // Should check that bundle exists on entity type.
+          if ($this->includeEntityTypeBundle($entity_type_id, $bundle_name)) {
+            $definition_key = $this->getEntityDefinitionKey($entity_type_id, $bundle_name);
+            // Continue if we already have this definition.
+            if (array_key_exists($definition_key, $definitions)) {
+              continue;
+            }
+            $json_schema = $this->getJsonSchema('api_json', $entity_type_id, $bundle_name);
+            $json_schema = $this->fixReferences($json_schema, '#/definitions/' . $definition_key);
+            $definitions[$definition_key] = $json_schema;
+            // Extract relationships from $json_schema and get definitions.
+            // The following is trash, fix it.
+            $relationships = $json_schema['properties']['data']['properties']['relationships'] ?: NULL;
+            if (count($relationships['properties'])) {
+              foreach ($relationships['properties'] as $relationship) {
+                $keys = (
+                  isset($relationship['properties']) &&
+                  isset($relationship['properties']['data']) &&
+                  isset($relationship['properties']['data']['properties']) &&
+                  isset($relationship['properties']['data']['properties']['type']) &&
+                  isset($relationship['properties']['data']['properties']['type']['enum'])
+                ) ? $relationship['properties']['data']['properties']['type']['enum'] : [];
+                foreach ($keys as $key) {
+                  list($entity_type, $bundle_name) = explode("--", $key);
+                  if (strpos($entity_type, 'menu') !== FALSE) {
+                    $foo = 'bar';
+                  }
+                  $desiredDefinitions = [$entity_type => [$bundle_name]];
+                  $added_definitions = $this->getSpecificDefinitions($desiredDefinitions);
+                  $definitions = array_merge($definitions, $added_definitions);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return $definitions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  private function fixReferences(array $schema, string $prefix) {
+    foreach ($schema as $name => $item) {
+      if (is_array($item)) {
+        $schema[$name] = $this->fixReferences($item, $prefix);
+      }
+      if ($name === '$ref' && is_string($item) && strpos($item, '#/') !== FALSE) {
+        $schema[$name] = preg_replace('/#\//', $prefix . '/', $item);
+      }
+    }
+    return $schema;
   }
 
 }
