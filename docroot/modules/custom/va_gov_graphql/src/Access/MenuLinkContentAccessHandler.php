f6ebcb390774aa486839ef800fc09bac91776206
Tim Cosgrove <tim.cosgrove@agile6.com>
2021-12-21 16:31:04 -0800

    VACMS-7258: fix custom access check for menu link content.

--- docroot/modules/custom/va_gov_graphql/src/Access/MenuLinkContentAccessHandler.php
+++ docroot/modules/custom/va_gov_graphql/src/Access/MenuLinkContentAccessHandler.php
@@ -8,8 +8,7 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\menu_link_content\MenuLinkContentAccessControlHandler;
 
 /**
- * An access handler to allow menu links to be viewed if the path doesn't exist
- * in Drupal.
+ * Allow menu links to be viewed if the path doesn't exist in Drupal.
  */
 class MenuLinkContentAccessHandler extends MenuLinkContentAccessControlHandler {
 
@@ -17,11 +16,12 @@ class MenuLinkContentAccessHandler extends MenuLinkContentAccessControlHandler {
    * Paths which are allowed to have view access.
    *
    * @return string[]
+   *   Paths we want to allow access for.
    */
   protected function getAllowedPaths() : array {
     return [
       '/find-locations',
-      '/health-care/apply/application'
+      '/health-care/apply/application',
     ];
   }
 
@@ -33,7 +33,7 @@ class MenuLinkContentAccessHandler extends MenuLinkContentAccessControlHandler {
       return parent::checkAccess($entity, $operation, $account);
     }
 
-    if ($operation !== 'view' &&
+    if ($operation === 'view' &&
       in_array($entity->getUrlObject()->toString(), $this->getAllowedPaths())) {
 
       return AccessResult::allowed()
