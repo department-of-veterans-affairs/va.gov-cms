d8f0b2a433d872b56afbf2a7d99d736c9bf6d2aa
Tim Cosgrove <tim.cosgrove@agile6.com>
2022-01-05 17:22:58 -0800

    VACMS-7082: refactor BannerAlerts and add facility banner logic to it.

--- docroot/modules/custom/va_gov_api/src/Resource/BannerAlerts.php
+++ docroot/modules/custom/va_gov_api/src/Resource/BannerAlerts.php
@@ -2,155 +2,258 @@
 
 namespace Drupal\va_gov_api\Resource;
 
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\Path\PathMatcher;
 use Drupal\jsonapi\JsonApiResource\LinkCollection;
 use Drupal\jsonapi\JsonApiResource\ResourceObject;
-use Drupal\jsonapi\JsonApiResource\ResourceObjectData;
-use Drupal\jsonapi_resources\Resource\EntityResourceBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\jsonapi\ResourceResponse;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\jsonapi\ResourceType\ResourceType;
+use Drupal\node\NodeInterface;
+use Drupal\Core\Cache\CacheableMetadata;
 
 /**
  * Get a response for Banner resource.
  */
-class BannerAlerts extends EntityResourceBase implements ContainerInjectionInterface {
+class BannerAlerts extends VaGovApiEntityResourceBase {
 
   /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   * {@inheritDoc}
    */
-  protected $entityTypeManager;
+  protected function collectResourceData(Request $request, ResourceType $resource_type) {
+    // Doing this as a case/switch for now. There are almost certainly better
+    // ways to do this.
+    switch ($resource_type->getTypeName()) {
+      case 'node--banner':
+        $this->collectBannerData($request, $resource_type);
+        break;
 
-  /**
-   * PatchMatcher utility.
-   *
-   * @var \Drupal\Core\Path\PathMatcher
-   */
-  protected $pathMatcher;
+      case 'node--full_width_banner_alert':
+        $this->collectFullWidthBannerAlertData($request, $resource_type);
+        break;
+    }
 
-  /**
-   * Constructs a new EntityResourceBase object.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   Tne entity type manager.
-   * @param \Drupal\Core\Path\PathMatcher $path_matcher
-   *   Drupal's internal path matching service.
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, PathMatcher $path_matcher) {
-    $this->entityTypeManager = $entity_type_manager;
-    $this->pathMatcher = $path_matcher;
+    // The endpoint must vary on the item-path; therefore, it must be added as
+    // a cache context.
+    $item_path_context = (new CacheableMetadata())->addCacheContexts(['url.query_args:item-path']);
+    $this->addCacheableDependency($item_path_context);
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('entity_type.manager'),
-      $container->get('path.matcher'),
-    );
-  }
-
-  /**
-   * Process the resource request.
+   * Collect `banner` entities to be returned in the response.
    *
-   * THIS IS POC CODE AND IS NOT PRODUCTION READY.
+   * Given a path, retrieves any `banner` that should show there, constructs a
+   * ResponseObject for it, and adds it to cacheableDependencies.
    *
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   The request.
-   * @param \Drupal\jsonapi\ResourceType\ResourceType[] $resource_types
-   *   The route resource types.
-   *
-   * @return \Drupal\jsonapi\ResourceResponse
-   *   The response.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @param \Drupal\jsonapi\ResourceType\ResourceType $resource_type
+   *   The ResourceType we want to collect data for.
    */
-  public function process(Request $request, array $resource_types) : ResourceResponse {
-    // Given a path, return any banner nodes that should display on it.
+  private function collectBannerData(Request $request, ResourceType $resource_type) {
     $path = $request->get('item-path');
+    if (is_null($path)) {
+      return;
+    }
 
-    // We need to load the banners in order to test the path.
+    // The business logic for displaying a full width alert is: each full width
+    // alert has a paths field which indicates where it should or should not be
+    // displayed. We need to load all `banner` entities in order to do this
+    // check.
     $node_storage = $this->entityTypeManager->getStorage('node');
     $query = $node_storage->getQuery();
+    // The machine name for this entity is `banner`.
     $query->condition('type', 'banner')->condition('status', TRUE);
     $banner_nids = $query->execute();
     /** @var \Drupal\node\NodeInterface[] $banners */
     $banners = [];
-    /** @var \Drupal\node\NodeInterface[] $matchedBanners */
-    $matchedBanners = [];
-    $primary_data = [];
     if (count($banner_nids)) {
       $banners = $node_storage->loadMultiple(array_values($banner_nids));
     }
+
+    // Check each banner to see if it should display, using Drupal's built-in
+    // PathMatcher class.
     /** @var \Drupal\node\NodeInterface[] $banners */
-    foreach ($banners as $banner) {
+    foreach ($banners as $idx => $banner) {
       $patterns = '';
+      // Convert values to a string that PathMatcher expects. field_target_paths
+      // is a multiple-value single line text field, while PathMatcher expects
+      // a single multiline value.
       $pathChecks = $banner->field_target_paths->getValue();
-      // Convert values to a string that PathMatcher expects.
       foreach ($pathChecks as $pathCheck) {
         $patterns = $patterns . $pathCheck['value'] . "\n";
       }
-      if ($this->pathMatcher->matchPath($path, $patterns)) {
-        $matchedBanners[] = $banner;
+      if (!$this->pathMatcher->matchPath($path, $patterns)) {
+        unset($banners[$idx]);
       }
     }
-    $resource_type = reset($resource_types);
-    // $cache_metadata = [];
-    $datum = NULL;
-    foreach ($matchedBanners as $entity) {
-      // Following line commented; need to figure out cache_metadata for > 1.
-      // $cache_metadata[] = CacheableMetadata::createFromObject($entity);
-      // $datum = CacheableMetadata::createFromObject($entity);
-      // Currently assumes Banner ctype.
-      /** @var \Drupal\taxonomy\TermInterface $section_term */
-      $section_term = $entity->field_administration->entity;
-      // We need to transform multivalue field output because Drupal.
-      $data = [
-        'nid' => $entity->id(),
-        'uuid' => $entity->uuid(),
-        'langcode' => $entity->language()->getId(),
-        'status' => $entity->isPublished(),
-        'bundle' => $entity->getType(),
-        'heading' => $entity->label(),
-        'moderation_state' => $entity->get('moderation_state')->getString(),
-        'alert_type' => $entity->field_alert_type->value,
-        // @phpstan-ignore-next-line
-        'text' => $entity->body->processed,
-        'dismissible' => $entity->field_dismissible_option->value,
-        'section_name' => $section_term->label(),
-        'paths' => array_column($entity->field_target_paths->getValue(), 'value'),
-      ];
 
-      $primary_data[] = new ResourceObject(
-        $entity,
-        $resource_type,
-        $entity->uuid(),
-        NULL,
-        $data,
-        new LinkCollection([])
-      );
+    // Construct and add a ResourceObject for each matching entity found, and
+    // add the entity as a cacheableDependency.
+    foreach ($banners as $entity) {
+      $resource_object = $this->createBannerResourceObject($entity, $resource_type);
+      $this->addResourceObject($resource_object);
+      $this->addCacheableDependency($entity);
+    }
+  }
+
+  /**
+   * Create a ResourceObject from a `banner` entity.
+   *
+   * @param \Drupal\node\NodeInterface $entity
+   *   The `banner` entity.
+   * @param \Drupal\jsonapi\ResourceType\ResourceType $resource_type
+   *   The ResourceType for `banner` entities.
+   *
+   * @return \Drupal\jsonapi\JsonApiResource\ResourceObject
+   *   A ResourceObject constructed from a `banner` entity.
+   */
+  private function createBannerResourceObject(NodeInterface $entity, ResourceType $resource_type) {
+    /** @var \Drupal\taxonomy\TermInterface $section_term */
+    $section_term = $entity->field_administration->entity;
+    $data = [
+      'nid' => $entity->id(),
+      'uuid' => $entity->uuid(),
+      'langcode' => $entity->language()->getId(),
+      'status' => $entity->isPublished(),
+      'bundle' => $entity->getType(),
+      'heading' => $entity->label(),
+      'moderation_state' => $entity->get('moderation_state')->getString(),
+      'alert_type' => $entity->field_alert_type->value,
+      // @phpstan-ignore-next-line
+      'text' => $entity->body->processed,
+      'dismissible' => $entity->field_dismissible_option->value,
+      'section_name' => $section_term->label(),
+      'paths' => array_column($entity->field_target_paths->getValue(), 'value'),
+    ];
+
+    return new ResourceObject(
+      $entity,
+      $resource_type,
+      $entity->uuid(),
+      NULL,
+      $data,
+      new LinkCollection([])
+    );
+  }
+
+  /**
+   * Collect `full_width_banner_alert` entities to be returned in the response.
+   *
+   * Given a path, retrieves any `full_width_banner_alert` that should show
+   * there, constructs a ResponseObject for it, and adds it to
+   * cacheableDependencies.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   * @param \Drupal\jsonapi\ResourceType\ResourceType $resource_type
+   *   The ResourceType we want to collect data for.
+   */
+  private function collectFullWidthBannerAlertData(Request $request, ResourceType $resource_type) {
+    $path = $request->get('item-path');
+    if (is_null($path)) {
+      return;
     }
 
-    $top_level_data = new ResourceObjectData($primary_data);
-    /** @var \Drupal\jsonapi\ResourceResponse $response */
-    $response = $this->createJsonapiResponse($top_level_data, $request);
+    // Find the first fragment of the path; this will correspond to a facility,
+    // if this is a facility page of some kind.
+    $path_pieces = explode("/", $path);
+    if (count($path_pieces) > 1) {
+      $region_fragment = "/" . $path_pieces[1];
+    }
+    else {
+      // We don't have an identifiable url fragment, so exit.
+      return;
+    }
 
-    // Unsure of how we're meant to handle this, but: each returned item should
-    // be able to invalidate cache.
-    // Question: How do we invalidate cache in the case where a new banner is
-    // published?
-    /* foreach ($cache_metadata as $datum) { */
-    // @phpstan-ignore-next-line
-    $response->addCacheableDependency($datum);
-    /* } */
+    // The business logic for displaying a facility alert is, take the initial
+    // url fragment of a url, which in the case of facilities corresponds to a
+    // facility system entity. Check whether that facility system entity is
+    // connected to a facility alert through the alert's operating status and
+    // alert reference. The current author is aware of the state of this logic.
+    $node_storage = $this->entityTypeManager->getStorage('node');
+    $system_query = $node_storage->getQuery()
+      ->condition('type', 'health_care_region_page')->condition('status', TRUE);
+    $system_nodes = $node_storage->loadMultiple($system_query->execute());
+    foreach ($system_nodes as $system_node) {
+      // Try to find a system_node that matches our current region_fragment.
+      // The following url dance is here to avoid early rendering.
+      $url = $system_node->toUrl()->toString(TRUE);
+      $url_string = $url->getGeneratedUrl();
+      if ($region_fragment === $url_string) {
+        // Find all operating status nodes which have this system node as their
+        // office.
+        $operating_status_query = $node_storage->getQuery()
+          ->condition('type', 'vamc_operating_status_and_alerts')
+          ->condition('status', TRUE)
+          ->condition('field_office', $system_node->id());
+        $operating_status_node_ids = array_values($operating_status_query->execute());
+        // Find any facility banners which have these operating status nodes
+        // connected to them.
+        $facility_banner_query = $node_storage->getQuery()
+          ->condition('type', 'full_width_banner_alert')
+          ->condition('status', TRUE)
+          ->condition('field_banner_alert_vamcs', $operating_status_node_ids, 'IN');
+        /** @var \Drupal\node\NodeInterface[] $facility_banners */
+        $facility_banners = $node_storage->loadMultiple($facility_banner_query->execute());
 
-    return $response;
+        // Construct and add a ResourceObject for each matching entity found,
+        // and add the entity as a cacheableDependency.
+        foreach ($facility_banners as $entity) {
+          $resource_object = $this->createFullWidthBannerAlertResourceObject($entity, $resource_type);
+          $this->addResourceObject($resource_object);
+          $this->addCacheableDependency($entity);
+        }
+        // Facility url fragments are unique, so if we have matched, we can stop
+        // checking against the rest of the facility system pages.
+        break;
+      }
+    }
+  }
+
+  /**
+   * Create a ResourceObject from a `full_width_banner_alert` entity.
+   *
+   * @param \Drupal\node\NodeInterface $entity
+   *   The `full_width_banner_alert` entity.
+   * @param \Drupal\jsonapi\ResourceType\ResourceType $resource_type
+   *   The ResourceType for `full_width_banner_alert` entities.
+   *
+   * @return \Drupal\jsonapi\JsonApiResource\ResourceObject
+   *   A ResourceObject constructed from a `full_width_banner_alert` entity.
+   */
+  private function createFullWidthBannerAlertResourceObject(NodeInterface $entity, ResourceType $resource_type) {
+    /** @var \Drupal\taxonomy\TermInterface $section_term */
+    $section_term = $entity->field_administration->entity;
+    $data = [
+      'nid' => $entity->id(),
+      'uuid' => $entity->uuid(),
+      'langcode' => $entity->language()->getId(),
+      'status' => $entity->isPublished(),
+      'bundle' => $entity->getType(),
+      'heading' => $entity->label(),
+      'moderation_state' => $entity->get('moderation_state')->getString(),
+      // @phpstan-ignore-next-line
+      'text' => $entity->field_body->processed,
+      'alert_type' => $entity->field_alert_type->value,
+      'dismissable' => $entity->field_alert_dismissable->value,
+      'section_name' => $section_term->label(),
+      // Paths is not relevant to the facility banner, but it's in spec.
+      'paths' => '',
+      // @phpstan-ignore-next-line
+      'situational_info' => $entity->field_banner_alert_situationinfo->processed,
+      'show_find_facilities_cta' => $entity->field_alert_find_facilities_cta->value,
+      'show_operating_status_cta' => $entity->field_alert_operating_status_cta->value,
+      'show_email_update_button' => $entity->field_alert_email_updates_button->value,
+      'limit_subpages' => $entity->field_alert_inheritance_subpages->value,
+      'send_email_updates' => $entity->field_operating_status_sendemail->value,
+      'situation_updates' => 'String for POC; this should point at a schema rather than being a string.',
+    ];
+    return new ResourceObject(
+      $entity,
+      $resource_type,
+      $entity->uuid(),
+      NULL,
+      $data,
+      new LinkCollection([])
+    );
   }
 
 }
---
+++ docroot/modules/custom/va_gov_api/src/Resource/VaGovApiEntityResourceBase.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Drupal\va_gov_api\Resource;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Path\PathMatcher;
+use Drupal\jsonapi\JsonApiResource\ResourceObject;
+use Drupal\jsonapi\JsonApiResource\ResourceObjectData;
+use Drupal\jsonapi_resources\Resource\EntityResourceBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\jsonapi\ResourceResponse;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\jsonapi\ResourceType\ResourceType;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
+/**
+ * Get a response for Banner resource.
+ */
+abstract class VaGovApiEntityResourceBase extends EntityResourceBase implements ContainerInjectionInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * PatchMatcher utility.
+   *
+   * @var \Drupal\Core\Path\PathMatcher
+   */
+  protected $pathMatcher;
+
+  /**
+   * Logger Channel.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * An array of ResourceObjects to use to construct the response.
+   *
+   * @var \Drupal\jsonapi\JsonApiResource\ResourceObject[]
+   */
+  private $resourceObjects = [];
+
+  /**
+   * An array of CacheableDependency objects to use to construct the response.
+   *
+   * @var \Drupal\Core\Cache\CacheableDependencyInterface[]
+   */
+  private $cacheableDependencies = [];
+
+  /**
+   * Constructs a new EntityResourceBase object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Tne entity type manager.
+   * @param \Drupal\Core\Path\PathMatcher $path_matcher
+   *   Drupal's internal path matching service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
+   *   The logger factory.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entity_type_manager,
+    PathMatcher $path_matcher,
+    LoggerChannelFactoryInterface $logger
+  ) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->pathMatcher = $path_matcher;
+    $this->logger = $logger->get('va_gov_api');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('path.matcher'),
+      $container->get('logger.factory'),
+    );
+  }
+
+  /**
+   * Add a response object to be used in constructing our response.
+   *
+   * @param \Drupal\jsonapi\JsonApiResource\ResourceObject $resource_object
+   *   The ResourceObject to add to our response.
+   */
+  protected function addResourceObject(ResourceObject $resource_object) {
+    $this->resourceObjects[] = $resource_object;
+  }
+
+  /**
+   * Add a CacheableDependency object to be used in constructing our response.
+   *
+   * @param mixed $cacheable_dependency
+   *   The dependency object to add to our response.
+   */
+  protected function addCacheableDependency($cacheable_dependency) {
+    if (!($cacheable_dependency instanceof CacheableMetadata)) {
+      $cacheable_dependency = CacheableMetadata::createFromObject($cacheable_dependency);
+    }
+    $this->cacheableDependencies[] = $cacheable_dependency;
+  }
+
+  /**
+   * Construct the ResourceResponse.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   *
+   * @return \Drupal\jsonapi\ResourceResponse
+   *   The response.
+   */
+  protected function constructJsonapiResponse(Request $request) {
+    $resource_object_data = new ResourceObjectData($this->resourceObjects);
+    /** @var \Drupal\jsonapi\ResourceResponse $response */
+    $response = $this->createJsonapiResponse($resource_object_data, $request);
+
+    // Add any entities to the response cacheable dependencies.
+    foreach ($this->cacheableDependencies as $cacheable_dependency) {
+      $response->addCacheableDependency($cacheable_dependency);
+    }
+    return $response;
+  }
+
+  /**
+   * Process the resource request.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   * @param \Drupal\jsonapi\ResourceType\ResourceType[] $resource_types
+   *   The route resource types.
+   *
+   * @return \Drupal\jsonapi\ResourceResponse
+   *   The response.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function process(Request $request, array $resource_types) : ResourceResponse {
+    foreach ($resource_types as $resource_type) {
+      $this->collectResourceData($request, $resource_type);
+    }
+    $response = $this->constructJsonapiResponse($request);
+
+    return $response;
+  }
+
+  /**
+   * For a given ResourceType, collect its resource data.
+   *
+   * Each ResourceType may have its own logic for retrieval, and will have its
+   * own logic for its ResourceObject.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   * @param \Drupal\jsonapi\ResourceType\ResourceType $resource_type
+   *   The ResourceType we want to collect data for.
+   */
+  protected function collectResourceData(Request $request, ResourceType $resource_type) {
+    // To be implemented by Resource-specific classes.
+    $this->logger->warning('Extending classes must implement method collectResourceData for themselves.');
+  }
+
+}
