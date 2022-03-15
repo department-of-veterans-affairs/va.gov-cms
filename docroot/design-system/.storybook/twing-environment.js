e7c55d7a64e1bb4a7b8aa8d478a035a73c7f28a5
Tanner Heffner <tanner.heffner@agile6.com>
2022-03-15 13:53:30 -0700

    VACMS-8345 render twig in storybook

--- docroot/design-system/.storybook/twing-environment.js
+++ docroot/design-system/.storybook/twing-environment.js
@@ -1,8 +1,8 @@
-const { TwingEnvironment, TwingLoaderRelativeFileSystem } = require('twing');
+const { TwingEnvironment, TwingLoaderRelativeFilesystem } = require('twing');
 const { addDrupalExtensions } = require('drupal-twig-extensions/twing');
 
 const twing = new TwingEnvironment(
-  new TwingLoaderRelativeFileSystem(), {autoescape: false}
+  new TwingLoaderRelativeFilesystem(), {autoescape: false}
 );
 
 // See: https://www.npmjs.com/package/drupal-twig-extensions for details.
