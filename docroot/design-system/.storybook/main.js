95c6863a79b8c0e3a2a351d2ffeba059b5f479cb
Tanner Heffner <tanner.heffner@agile6.com>
2022-03-15 10:09:03 -0700

    VACMS-8345 add modules for twig support in storybook, fix audit warnings

---
+++ docroot/design-system/.storybook/main.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  stories: [
+    "../components/**/*.stories.js",
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  framework: "@storybook/html",
+
+  webpackFinal: async config => {
+    config.experiments = {
+      ...(config.experiments ? config.experiments : {}),
+      topLevelAwait: true,
+    };
+
+    // add twig support to storybook
+    config.modules.rules.push({
+      test: /\.twig/,
+      use: [
+        {
+          loader: 'twing-loader',
+          options: {
+            environmentModulePath: path.resolve(__dirname, 'twing-environment.js'),
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '..', 'components')
+    });
+
+    return config;
+  },
+};
