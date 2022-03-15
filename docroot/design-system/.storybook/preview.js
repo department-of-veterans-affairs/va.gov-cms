fb9357b1fda4cddd712390a02f15c980fee851bf
Tanner Heffner <tanner.heffner@agile6.com>
2022-03-15 13:53:30 -0700

    VACMS-8345 render twig in storybook

--- docroot/design-system/.storybook/preview.js
+++ docroot/design-system/.storybook/preview.js
@@ -1,3 +1,20 @@
+// bring default claro variables into storybook so we can override properly
+import '../../core/themes/claro/css/base/elements.css';
+
+// // bring token styles to make available for all pages
+// import '../components/tokens/_variables.scss';
+
+// Adds support for twing and storybook... issue with promises in Storybook HTML
+// see: https://github.com/NightlyCommit/twing-loader/issues/33#issuecomment-889409418
+export const loaders = [
+  async ({ args, originalStoryFn }) => {
+    if (originalStoryFn.render) {
+      const component = await originalStoryFn.render(args);
+      return { component };
+    }
+  }
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
