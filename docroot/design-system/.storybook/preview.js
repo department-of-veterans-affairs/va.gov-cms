639ada6b544e97dc31c93a9482e40142b4b8b512
Tanner Heffner <tanner.heffner@agile6.com>
2022-03-16 11:22:53 -0700

    VACMS-8345 add scss support to storybook

--- docroot/design-system/.storybook/preview.js
+++ docroot/design-system/.storybook/preview.js
@@ -2,7 +2,7 @@
 import '../../core/themes/claro/css/base/elements.css';
 
 // // bring token styles to make available for all pages
-// import '../components/tokens/_variables.scss';
+import '../components/tokens/_variables.scss';
 
 // Adds support for twing and storybook... issue with promises in Storybook HTML
 // see: https://github.com/NightlyCommit/twing-loader/issues/33#issuecomment-889409418
