22653f3001f272ce1c42b47a3c64f7b9706d3705
Tanner Heffner <tanner.heffner@agile6.com>
2022-03-16 15:19:05 -0700

    VACMS-8345 make storybook controls work with twig. split out sb-specific styles

--- docroot/design-system/.storybook/preview.js
+++ docroot/design-system/.storybook/preview.js
@@ -1,17 +1,18 @@
-// bring default claro variables into storybook so we can override properly
+// Add default claro variables into storybook so we can override properly
 import '../../core/themes/claro/css/base/elements.css';
 
-// // bring token styles to make available for all pages
+// Add token styles to make available for all pages
 import '../components/tokens/_variables.scss';
+
+// Add storybook-specific styles we don't need to include in the drupal bundle
+import './storybook-styles.scss';
 
 // Adds support for twing and storybook... issue with promises in Storybook HTML
 // see: https://github.com/NightlyCommit/twing-loader/issues/33#issuecomment-889409418
 export const loaders = [
   async ({ args, originalStoryFn }) => {
     if (originalStoryFn.render) {
-      console.log(args);
       const component = await originalStoryFn.render(args);
-      console.log(component);
       return { component };
     }
   }
