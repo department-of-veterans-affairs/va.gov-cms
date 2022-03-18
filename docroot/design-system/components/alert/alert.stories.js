b52fd75adc39de3798ce72d1a2c0953f3ffbc6ff
Tanner Heffner <tanner.heffner@agile6.com>
2022-03-18 16:14:59 -0700

    VACMS-8346 tokens in js stories for webpack 5 and CSF 3

--- docroot/design-system/components/alert/alert.stories.js
+++ docroot/design-system/components/alert/alert.stories.js
@@ -1,54 +1,68 @@
-// import alert from './alert.twig';
-// import './alert.scss';
-// import './index.js';
-// import DrupalAttribute from '../../DrupalAttribute';
-//
-// export default { title: 'Components/Alerts' }
-//
-// export const Status = (_, { loaded: { component } }) => component;
-//
-// Status.args = {
-//   attributes: new DrupalAttribute(),
-//   type: 'status',
-//   title_ids: ['status', 'warning', 'error'],
-//   status_headings: ['status', 'warning', 'error'],
-//   messages: ['this is an example alert message']
-// }
-//
-// Status.parameters.render = async args => {
-//   return await alert({
-//     ...Status.args, ...args
-//   });
-// }
-//
-// export const Warning = (_, { loaded: { component } }) => component;
-//
-// Warning.args = {
-//   attributes: new DrupalAttribute(),
-//   type: 'warning',
-//   title_ids: ['status', 'warning', 'error'],
-//   status_headings: ['status', 'warning', 'error'],
-//   messages: ['this is an example alert message']
-// }
-//
-// Warning.parameters.render = async args => {
-//   return await alert({
-//     ...Warning.args, ...args
-//   });
-// }
-//
-// export const Error = (_, { loaded: { component } }) => component;
-//
-// Error.args = {
-//   attributes: new DrupalAttribute(),
-//   type: 'error',
-//   title_ids: ['status', 'warning', 'error'],
-//   status_headings: ['status', 'warning', 'error'],
-//   messages: ['this is an example alert message']
-// }
-//
-// Error.parameters.render = async args => {
-//   return await alert({
-//     ...Error.args, ...args
-//   });
-// }
+import alert from './alert.twig';
+import DrupalAttribute from '../../DrupalAttribute';
+import './alert.scss';
+import './index.js';
+
+export default { component: alert };
+
+const shared = {
+  parameters: {},
+  // Don't show drupal attributes in storybook controls
+  argTypes: {
+    attributes: {
+      table: {
+        disable: true
+      },
+    },
+  },
+}
+
+export const Status = {
+  args: {
+    attributes: new DrupalAttribute(),
+    type: 'status',
+    title_ids: ['status', 'warning', 'error'],
+    status_headings: ['status', 'warning', 'error'],
+    messages: ['this is an example alert message']
+  },
+  ...shared
+};
+
+Status.parameters.render = async args => {
+  return await alert({
+    ...Status.args, ...args
+  });
+}
+
+export const Warning = {
+  args: {
+    attributes: new DrupalAttribute(),
+    type: 'warning',
+    title_ids: ['status', 'warning', 'error'],
+    status_headings: ['status', 'warning', 'error'],
+    messages: ['this is an example alert message']  },
+  ...shared
+};
+
+Warning.parameters.render = async args => {
+  return await alert({
+    ...Warning.args, ...args
+  });
+}
+
+export const Error = {
+  args: {
+    attributes: new DrupalAttribute(),
+    type: 'error',
+    title_ids: ['status', 'warning', 'error'],
+    status_headings: ['status', 'warning', 'error'],
+    messages: ['this is an example alert message']
+  },
+  ...shared
+};
+
+Error.parameters.render = async args => {
+  return await alert({
+    ...Error.args, ...args
+  });
+}
