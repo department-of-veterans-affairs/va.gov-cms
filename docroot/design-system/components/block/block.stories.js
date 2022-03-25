d5e6f2d6a240d329793b7831805415095c9d3545
Tanner Heffner <tanner.heffner@agile6.com>
2022-03-25 11:36:49 -0700

    VACMS 8346 storybook + design system infrastructure, take 3 (#8482)
    * VACMS-8345 add modules for twig support in storybook, fix audit warnings
    
    * VACMS-8345 render twig in storybook
    
    * VACMS-8345 add support for drupal attribute object in storybook
    
    * VACMS-8345 add scss support to storybook
    
    * VACMS-8345 stub out storybook structure, update gitignore for compiled files
    
    * VACMS-8345 make storybook controls work with twig. split out sb-specific styles
    
    * VACMS-8346 add dependencies for drupal consumption
    
    * VACMS-8346 first pass at webpack config & updating pattern js to connect to drupal behaviors
    
    * VACMS-8346 verify namespace works and design system styles are present
    
    * VACMS-8346 storybook compat with webpack5
    
    * VACMS-8346 tokens in js stories for webpack 5 and CSF 3
    
    * VACMS-8345 generate json file containing css var data for storybook.
    
    * VACMS-8346 clean up stories, some controls, comments. move generated file to dist/
    
    * VACMS-8346 add composer commands for design system
    
    * VACMS-8345 add ddev support for storybook
    
    * VACMS-8346 clean up new composer command names
    
    * VACMS-8346 fix composer deps in this branch. add namespace to info
    
    * VACMS-8346 components work in drupal
    
    * VACMS-8346 export config enabling the components module
    
    * VACMS-8346 update composer
    
    * VACMS-8346 update css generation config for tugboat
    
    * VACMS-8346 build storybook in build and update in tugboat
    
    * VACMS-8346 update css generation for tugboat
    
    * VACMS-8346 update path to css vars
    
    * VACMS-8346 remove block test styles
    
    * VACMS-8346 make sure dependencies get installed
    
    * Setup Tugboat URL storybook-* prefix and routing for /design-system/storybook-static
    
    * Add storybook alias for real this time
    
    * Fix path for static storybook
    
    * VACMS-8346 update yarn lock to fix dep warning
    
    * VACMS-8346 move storybook build step to online
    
    * Add pretty 403 error handling for when storybook isn't built yet.
    
    * VACMS-8346 update 403 page
    
    * VACMS-8346 add dependencies for drupal consumption
    
    * VACMS-8346 fix composer deps in this branch. add namespace to info
    
    * VACMS-8346 these files shouldnt be here. not sure why i committed them accidentally. sorry
    
    Co-authored-by: Elijah Lynn <elijah@elijahlynn.net>

---
+++ docroot/design-system/components/block/block.stories.js
@@ -0,0 +1,40 @@
+import block from './block.twig';
+import DrupalAttribute from '../../DrupalAttribute';
+import './block.scss';
+import './index.js';
+
+export default { title: 'Components/Block', component: block };
+export const Block = {
+  args: {
+    attributes: new DrupalAttribute(),
+    title_attributes: new DrupalAttribute(),
+    plugin_id: "Some plugin",
+    title_prefix: "",
+    title_suffix: "",
+    label: "I'm a block!",
+    content: "Lorem ipsum dolor sit amet.",
+    configuration: {
+      provider: "Some module"
+    }
+  },
+  parameters: {},
+  // Don't show drupal attributes in storybook controls
+  argTypes: {
+    attributes: {
+      table: {
+        disable: true
+      },
+    },
+    title_attributes: {
+      table: {
+        disable: true
+      }
+    }
+  },
+};
+
+Block.parameters.render = async args => {
+  return await block({
+    ...Block.args, ...args
+  })
+}
