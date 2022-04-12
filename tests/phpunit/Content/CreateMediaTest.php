2a334207eabc86e952790c35e5c57c2441f0978a
Nathan/Nate Douglas <ndouglas@users.noreply.github.com>
2022-04-12 14:24:25 -0400

    VACMS-8085: Removes logspam from performance tests. (#8731)

--- tests/phpunit/Content/CreateMediaTest.php
+++ tests/phpunit/Content/CreateMediaTest.php
@@ -60,9 +60,6 @@ class CreateMediaTest extends ExistingSiteBase {
     // Test assertion.
     $secs = number_format($microsecs, 3);
     $this->assertLessThan($benchmark, $secs, __METHOD__ . "\nOperation took " . $secs . " seconds which is longer than the benchmark of " . $benchmark . " seconds.\n");
-
-    $message = __METHOD__ . "\nOperation took " . $secs . " seconds compared to the benchmark of " . $benchmark . " seconds.\n";
-    fwrite(STDERR, print_r($message, TRUE));
   }
 
   /**
