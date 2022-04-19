85083d4f711d8afa81cea0a6d0560887eaf0bdaa
Nathan Douglas <github@darkdell.net>
2022-04-19 08:33:58 -0400

    Then!

--- tests/behavioral/cypress/integration/common/the_path_should_contain.js
+++ tests/behavioral/cypress/integration/common/the_path_should_contain.js
@@ -1,14 +1,13 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
+import { Then } from "cypress-cucumber-preprocessor/steps";
 
-
-Given(`the path should contain {string}`, (string) => {
+Then(`the path should contain {string}`, (string) => {
   cy.window().then((window) => {
     const pagePath = window.location.pathname;
     expect(pagePath).to('contain', string);
   });
 });
 
-Given(`the path should equal {string}`, (string) => {
+Then(`the path should equal {string}`, (string) => {
   cy.window().then((window) => {
     const pagePath = window.location.pathname;
     expect(pagePath).to('equal', string);
