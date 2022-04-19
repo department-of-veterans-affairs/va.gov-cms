57818d31f46d3ae1da4834f38539203af1cd2c06
Nathan Douglas <github@darkdell.net>
2022-04-19 08:17:55 -0400

    Fix capitalization in step definitions.

--- tests/behavioral/cypress/integration/common/the_path_should_contain.js
+++ tests/behavioral/cypress/integration/common/the_path_should_contain.js
@@ -1,14 +1,14 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
 
 
-Given(`The path should contain {string}`, (string) => {
+Given(`the path should contain {string}`, (string) => {
   cy.window().then((window) => {
     const pagePath = window.location.pathname;
     expect(pagePath).to('contain', string);
   });
 });
 
-Given(`The path should equal {string}`, (string) => {
+Given(`the path should equal {string}`, (string) => {
   cy.window().then((window) => {
     const pagePath = window.location.pathname;
     expect(pagePath).to('equal', string);
