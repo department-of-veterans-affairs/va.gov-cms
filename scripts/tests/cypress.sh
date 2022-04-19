fa6bd121498d831fa83e2f8d69c420a696ce910e
Nathan Douglas <github@darkdell.net>
2022-04-19 11:43:21 -0400

    Permits Cypress to run on BRD.

--- scripts/tests/cypress.sh
+++ scripts/tests/cypress.sh
@@ -11,12 +11,6 @@ set -e
 # Allow script to be run anywhere in git repo
 cd "$(git rev-parse --show-toplevel)"
 
-# Do not attempt to run on BRD environment.
-if [ -n "$CMS_IS_BRD" ]; then
-  echo "Detected BRD environment, skipping cypress-axe tests."
-  exit 0
-fi
-
 npm install
 ./node_modules/.bin/cypress install
 npm run test:${1} "${@:2}"
