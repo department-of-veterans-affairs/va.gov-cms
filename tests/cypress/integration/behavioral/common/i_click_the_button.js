5c000da9191bc16abec92f9594dea43667c607ec
Nate Douglas <ndouglas@users.noreply.github.com>
2022-04-21 14:14:37 -0400

    VACMS-8675: Port Behat tests to Cypress. (#8746)
    * VACMS-8675: Port Behat tests to Cypress.
    
    * Removes dealing with the content lock for this crap test.
    
    * Rewrite Behat moderation state form element hook implementation test in PHPUnit.
    
    * Adds additional test for moderation state.
    
    * Another test moved over.
    
    * Confirm Generate automatic URL alias is unchecked after taxonomy term publish.
    
    * Removes Behat test.
    
    * Taxonomy term support.
    
    * Couple more tests.
    
    * Remove Behat tests we just added.
    
    * Moves HMS test from Behat to Cypress.
    
    * perms.feature
    
    * save_and_continue.feature
    
    * Content editing and publishing.
    
    * Permissions, cleanup decoupled test.
    
    * Ensure we navigate away from /node/add page when creating a node.
    
    * Wait a second before saving nodes and taxonomy terms.
    
    * Don't create second link teaser.
    
    * Package updates.
    
    * Update references to faker.
    
    * chdir to correct directory before running build.
    
    * Can't timeout content-release at 60 seconds, lol.
    
    * Show full stdout and stderr on content build failure.
    
    * Effectively log failures in content-build.
    
    * Clicking links should always stay in the same tab.
    
    * Fix capitalization in step definitions.
    
    * Then!
    
    * Fixes path step definitions.
    
    * Run all tests again.
    
    * Permits Cypress to run on BRD.
    
    * Lowers timeout on content-build, times individual test steps.
    
    * Update tests/behavioral/cypress/support/commands.js
    
    * Merges accessibility and behavioral Cypress tests.
    
    * Fix file_upload test.
    
    * just run npm install, lmao
    
    * Permit va-gov-cms.ddev.site as an allowed domain on dev/stg/prod.

---
+++ tests/cypress/integration/behavioral/common/i_click_the_button.js
@@ -0,0 +1,9 @@
+import { Given } from "cypress-cucumber-preprocessor/steps";
+
+Given(`I click the {string} button`, (text) => {
+  cy.contains(text).click({ force: true });
+});
+
+Given(`I click the button with selector {string}`, (selector) => {
+  cy.get(selector).click({ force: true });
+});
---
+++ tests/cypress/integration/behavioral/common/i_click_the_revisions_tab.js
@@ -0,0 +1,6 @@
+import { Given } from "cypress-cucumber-preprocessor/steps";
+
+Given("I click the revisions tab", () => {
+  cy.scrollTo('top', { ensureScrollable: false });
+  cy.get(".tabs__tab a").contains("Revisions").click({ force: true });
+});
