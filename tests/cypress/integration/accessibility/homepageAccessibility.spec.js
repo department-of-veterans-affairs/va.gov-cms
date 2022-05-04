ccca3631d244d58dce567328f866393849975484
Nathan Douglas <github@darkdell.net>
2022-05-04 12:22:08 -0400

    Fiddling around, run a bunch of tests.

--- tests/cypress/integration/accessibility/homepageAccessibility.spec.js
+++ tests/cypress/integration/accessibility/homepageAccessibility.spec.js
@@ -1,19 +1,17 @@
+const axeRuntimeOptions = {
+  runOnly: {
+    type: 'tag',
+    values: ['wcag2a', 'wcag2aa']
+  }
+};
+
 describe('Component accessibility test', () => {
-  // Test homepage while logged out.
   it('Login page has no detectable accessibility violations on load.', () => {
     cy.visit('/');
     cy.injectAxe();
-
-    const axeRuntimeOptions = {
-      runOnly: {
-        type: 'tag',
-        values: ['wcag2a', 'wcag2aa']
-      }
-    };
-
-    cy.get('body').each((element, index) => {
-      // Excluding JSD widget from A11y scan, since we cannot control its code.
-      cy.checkA11y({ exclude: ['#jsd-widget']}, null, cy.terminalLog);
-    });
+    cy.checkA11y({
+      include: 'body',
+      exclude: ['#jsd-widget'],
+    }, null, cy.terminalLog);
   });
 });
