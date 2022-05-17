6ae62d0aadf5683951cc5d76f9a0f8937453ddfb
Lynn Stahl <lynn.stahl@agile6.com>
2022-05-17 09:18:41 -0400

    VACMS-3969: Move to a js approach instead of preprocess.

--- docroot/modules/custom/va_gov_backend/js/media_library_set_media_owner.es6.js
+++ docroot/modules/custom/va_gov_backend/js/media_library_set_media_owner.es6.js
@@ -5,16 +5,19 @@
 (($, Drupal) => {
   Drupal.behaviors.vaGovMediaSetOwner = {
     attach: () => {
-        const section = document.getElementById('edit-field-administration')
-        const owner = document.getElementById('edit-field-media-0-inline-entity-form-field-owner')
-        //on page load
-          if (owner.value){ //check for owner
-            owner.value = section.value //set value of owner equal to section
-
-            section.addEventListener('change',function handleChange(event) { //on section blur
-              owner.value = section.value //set value of owner equal to section
-            });
-          }
+      const section = document.getElementById("edit-field-administration");
+      const owner = document.getElementById(
+        "edit-field-media-0-inline-entity-form-field-owner"
+      );
+      // on page load
+      if (owner.value) {
+        // check for owner
+        owner.value = section.value; // set value of owner equal to section
+        section.addEventListener("change", function handleChange() {
+          // on section blur/change
+          owner.value = section.value; // set value of owner equal to section
+        });
+      }
     },
   };
 })(jQuery, window.Drupal);
