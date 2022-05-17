4ac1dd6bdf901cfaf0d96f89f4cada3fb34cd164
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2022-05-17 10:45:44 -0700

    Revert "VACMS-8682 storybook button + inputs examples (#8994)"
    This reverts commit 269fc59e608c5e514c73ded28e082aa624c7002a.

--- docroot/design-system/components/icon/index.js
+++ docroot/design-system/components/icon/index.js
@@ -1,5 +1,5 @@
 // fontawesome api methods
-import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
 // import specific icons as needed to keep final bundle small. fa icon name in comment.
 import {
   faBan, // ban
@@ -13,7 +13,7 @@ import {
   faPencil, // pencil
   faPlus, // plus
   faThumbsDown, // thumbs down
-  faThumbsUp, // thumbs up
+  faThumbsUp, // trash
   faTrash, // trash
   faTriangleExclamation // warning
 } from '@fortawesome/free-solid-svg-icons';
@@ -23,9 +23,6 @@ import './icon.scss';
 
 // Import module template
 import './icon.twig';
-
-// Allow font awesome icons in pseudo elements.
-config.searchPseudoElements = true;
 
 // Add icons to our library.
 library.add(
