02a495fa337a70da0ea4dfa7dfa99fe7cbea5dff
Lynn Stahl <lynn.stahl@agile6.com>
2022-05-31 16:47:15 -0400

    VACMS-9242 Benefits Hubs meta title length (#9246)
    * VACMS-9242: Update title character counts.
    
    * VACMS-9242: Update meta title character counts.
    
    * VACMS-9242: revision log update.
    
    * VACMS-9242: Update default metatags title to use node title.
    
    * VACMS-9242: Remove meta title fields fro,m Benefits Hubs content types.
    
    * VACMS-9242: Update Behat tests for Benefits Hubs content types.
    
    * VACMS-9242: Update cypress tests for creating nodes.
    
    * VACMS-9242: Remove remaining meta title fields and update tests.
    
    * VACMS-9242: Update Behat/behavioral tests.
    
    * VACMS-9242: Update behavioral tests.
    
    * VACMS-9242: Add update hook to remove stale data from deleted field.
    
    * Update core.entity_view_display.node.vamc_system_billing_insurance.default.yml

--- tests/cypress/integration/behavioral/common/i_create_a_node.js
+++ tests/cypress/integration/behavioral/common/i_create_a_node.js
@@ -70,7 +70,6 @@ const creators = {
     cy.findAllByLabelText('Page title').type('[Test Data] ' + faker.lorem.sentence(), { force: true });
     cy.findAllByLabelText('Page introduction').type(faker.lorem.sentence(), { force: true });
     cy.findAllByLabelText('Section').select('VACO', { force: true });
-    cy.findAllByLabelText('Meta title tag').type('[Test Data] ' + faker.lorem.sentence(), { force: true });
     cy.findAllByLabelText('Meta description').type(faker.lorem.sentence(), { force: true });
     cy.findAllByLabelText('Provide a menu link').check({ force: true });
     cy.findAllByLabelText('Menu link title').type('[Test Data] ' + faker.lorem.sentence(), { force: true });
@@ -84,7 +83,6 @@ const creators = {
     cy.visit('/node/add/office');
     cy.scrollTo('top');
     cy.findAllByLabelText('Name').type('[Test Data] ' + faker.lorem.sentence(), { force: true });
-    cy.findAllByLabelText('Meta title tag').type('[Test Data] ' + faker.lorem.sentence(), { force: true });
     cy.findAllByLabelText('Section').select('VACO', { force: true });
     cy.findAllByLabelText('Provide a menu link').check({ force: true });
     cy.findAllByLabelText('Menu link title').type('[Test Data] ' + faker.lorem.sentence(), { force: true });
