0f58c6d4607de43f37d8b620e79a05452d3c9c07
Jay Darnell <jay.darnell@civicactions.com>
2022-06-07 09:51:38 -0400

    VACMS-7279: add facility_url and Lovell overrides to Facility API

--- docroot/modules/custom/va_gov_post_api/src/Service/PostFacilityStatus.php
+++ docroot/modules/custom/va_gov_post_api/src/Service/PostFacilityStatus.php
@@ -179,6 +179,7 @@ class PostFacilityStatus extends PostFacilityBase {
 
     if ($this->shouldPush($forcePush)) {
       $payload = [
+        'facility_url' => 'https://www.va.gov' . $this->facilityNode->toUrl()->toString(),
         'operating_status' => [
           'code' => strtoupper($this->statusToPush),
           'additional_info' => (strtoupper($this->statusToPush) != 'NORMAL') ? $this->additionalInfoToPush : NULL,
@@ -187,6 +188,7 @@ class PostFacilityStatus extends PostFacilityBase {
 
       $this->addSupplementalStatus($payload);
       $this->getRelatedSystemInfo($payload);
+      $this->getSystemOverrides($payload);
     }
 
     return $payload;
@@ -256,6 +258,43 @@ class PostFacilityStatus extends PostFacilityBase {
           'covid_url' => 'https://www.va.gov' . $systemUrl . '/programs/covid-19-vaccines',
           'va_health_connect_phone' => $systemNode->get('field_va_health_connect_phone')->value,
         ];
+      }
+    }
+  }
+
+  /**
+   * Update payload array with overrides for specific systems.
+   *
+   * @param array $payload
+   *   Payload array.
+   */
+  protected function getSystemOverrides(array &$payload) {
+    // If this facility references a system, include system information.
+    if ($this->facilityNode->hasField('field_region_page')) {
+      $systemId = $this->facilityNode->get('field_region_page')->target_id;
+      // System url overrides: Lovell VAMC System - 15007.
+      if ($systemId === '15007') {
+        $payload['facility_url'] = 'https://www.lovell.fhcc.va.gov';
+        $payload['system']['url'] = 'https://www.lovell.fhcc.va.gov';
+        $payload['system']['covid_url'] = 'https://www.lovell.fhcc.va.gov/services/covid-19-vaccines.asp';
+      }
+      // Facility url overrides.
+      $facility_id = $this->facilityNode->hasField('field_facility_locator_api_id') ? $this->facilityNode->get('field_facility_locator_api_id')->value : NULL;
+      // Evanston VA clinic - vha_556GA.
+      if ($facility_id === 'vha_556GA') {
+        $payload['facility_url'] = 'https://www.lovell.fhcc.va.gov/locations/Evanston_Community_Based_Outpatient_Clinic.asp';
+      }
+      // Kenosha VA Clinic - vha_556GD.
+      if ($facility_id === 'vha_556GD') {
+        $payload['facility_url'] = 'https://www.lovell.fhcc.va.gov/locations/Kenosha_Community_Based_Outpatient_Clinic.asp';
+      }
+      // McHenry VA Clinic - vha_556GC.
+      if ($facility_id === 'vha_556GC') {
+        $payload['facility_url'] = 'https://www.lovell.fhcc.va.gov/locations/McHenry_Community_Based_Outpatient_Clinic.asp';
+      }
+      // Manila VA Clinic - vha_358.
+      if ($facility_id === 'vha_358') {
+        $payload['facility_url'] = 'https://www.visn21.va.gov/locations/manila.asp';
       }
     }
   }
