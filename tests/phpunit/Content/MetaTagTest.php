c1439c090af19684f3fbbc9459de7ab52b8ca641
Nathan Douglas <github@darkdell.net>
2022-06-15 15:51:22 -0400

    Fixes flaky test.

--- tests/phpunit/Content/MetaTagTest.php
+++ tests/phpunit/Content/MetaTagTest.php
@@ -24,7 +24,7 @@ class MetaTagTest extends ExistingSiteBase {
     $name = uniqid();
     $node = $this->createNode([
       'title' => "$name test",
-      'type' => 'health_care_local_facility',
+      'type' => 'health_care_region_page',
       'uid' => $author->id(),
     ]);
     $node->get('field_media')->generateSampleItems(1);
