43d82ac0575c363bba9915527f61f15a8b01ad01
Cameron Eagans <me@cweagans.net>
2022-06-17 11:42:43 -0600

    touchups after rebase

--- docroot/modules/custom/va_gov_build_trigger/src/Plugin/VAGov/Environment/Local.php
+++ docroot/modules/custom/va_gov_build_trigger/src/Plugin/VAGov/Environment/Local.php
@@ -3,12 +3,7 @@
 namespace Drupal\va_gov_build_trigger\Plugin\VAGov\Environment;
 
 use Drupal\va_gov_build_trigger\Environment\EnvironmentPluginBase;
-use Drupal\va_gov_build_trigger\WebBuildCommandBuilder;
-use Drupal\va_gov_build_trigger\WebBuildStatusInterface;
-use Drupal\va_gov_build_trigger\Command\CommandRunner;
 use Drupal\va_gov_build_trigger\Form\LocalBuildTriggerForm;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Local Plugin for Environment.
@@ -19,67 +14,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class Local extends EnvironmentPluginBase {
-  use CommandRunner;
-  use QueueHelper;
-
-  /**
-   * The queue storage manager.
-   *
-   * @var \Drupal\Core\Entity\EntityStorageInterface
-   */
-  protected $queueLoader;
-
-  /**
-   * {@inheritDoc}
-   */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    LoggerInterface $logger,
-    WebBuildStatusInterface $webBuildStatus,
-    WebBuildCommandBuilder $webBuildCommandBuilder,
-    EntityStorageInterface $queueLoader
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $logger, $webBuildStatus, $webBuildCommandBuilder);
-    $this->queueLoader = $queueLoader;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('logger.factory')->get('va_gov_build_trigger'),
-      $container->get('va_gov.build_trigger.web_build_status'),
-      $container->get('va_gov.build_trigger.web_build_command_builder'),
-      $container->get('entity_type.manager')->getStorage('advancedqueue_queue')
-    );
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function triggerFrontendBuild() : void {
-    // phpcs:disable
-    // See issue https://github.com/department-of-veterans-affairs/va.gov-cms/issues/8796
-    // $message = $this->t('Build not dispatched because the content build is not currently working on ddev.');
-    // $this->messenger()->addStatus($message);
-    // $this->logger->info($message);
-    // return;
-    // phpcs:enable
-    parent::triggerFrontendBuild();
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function contentEditsShouldTriggerFrontendBuild(): bool {
-    return FALSE;
-  }
 
   /**
    * {@inheritDoc}
