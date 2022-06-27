5b792683ed9cb7ed787841818b55a97501593b64
Jay Darnell <jay.darnell@civicactions.com>
2022-06-27 16:45:08 -0400

    VACMS-6528: auto-archive related services and events when a facility is archived

--- docroot/modules/custom/va_gov_facilities/src/EventSubscriber/FacilitiesSubscriber.php
+++ docroot/modules/custom/va_gov_facilities/src/EventSubscriber/FacilitiesSubscriber.php
@@ -2,9 +2,14 @@
 
 namespace Drupal\va_gov_facilities\EventSubscriber;
 
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\core_event_dispatcher\EntityHookEvents;
 use Drupal\core_event_dispatcher\Event\Entity\EntityPresaveEvent;
-use Drupal\Core\Entity\EntityInterface;
+use Drupal\core_event_dispatcher\Event\Entity\EntityUpdateEvent;
 use Drupal\node\NodeInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -12,6 +17,50 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  * VA.gov VA Facilities Entity Event Subscriber.
  */
 class FacilitiesSubscriber implements EventSubscriberInterface {
+  use StringTranslationTrait;
+
+  /**
+   * The active user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   *  The user object.
+   */
+  private $currentUser;
+
+  /**
+   * The entity manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   *  The entity manager.
+   */
+  private $entityTypeManager;
+
+  /**
+   * The Messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs the EventSubscriber object.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   * @param \Drupal\Core\Entity\EntityTypeManager $entity_type_manager
+   *   The string entity type service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   */
+  public function __construct(
+    AccountInterface $current_user,
+    EntityTypeManager $entity_type_manager,
+    MessengerInterface $messenger
+  ) {
+    $this->currentUser = $current_user;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->messenger = $messenger;
+  }
 
   /**
    * {@inheritdoc}
@@ -19,6 +68,7 @@ class FacilitiesSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents(): array {
     return [
       EntityHookEvents::ENTITY_PRE_SAVE => 'entityPresave',
+      EntityHookEvents::ENTITY_UPDATE => 'entityUpdate',
     ];
   }
 
@@ -31,6 +81,76 @@ class FacilitiesSubscriber implements EventSubscriberInterface {
   public function entityPresave(EntityPresaveEvent $event): void {
     $entity = $event->getEntity();
     $this->clearNormalStatusDetails($entity);
+  }
+
+  /**
+   * Entity update Event call.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Entity\EntityUpdateEvent $event
+   *   The event.
+   */
+  public function entityUpdate(EntityUpdateEvent $event): void {
+    $entity = $event->getEntity();
+    if ($entity instanceof NodeInterface && $entity->bundle() === 'health_care_local_facility') {
+      // When a facility is archived auto archive related services and events.
+      if (isset($entity->original) && ($entity->original instanceof NodeInterface)) {
+        $currentState = $entity->get('moderation_state')->value;
+        $oldState = $entity->original->get('moderation_state')->value;
+        if ($oldState === 'archived' || $currentState !== 'archived') {
+          return;
+        }
+        $facilityID = $entity->id();
+        $relatedTypes = [
+          'event',
+          'health_care_local_health_service',
+          'vha_facility_nonclinical_service',
+        ];
+
+        // Find all related published services and events.
+        $query = \Drupal::entityQuery('node');
+        $query->condition('type', $relatedTypes, 'IN')
+          ->condition('field_facility_location', $facilityID)
+          ->condition('moderation_state', 'published');
+        $nids = $query->execute();
+
+        if (count($nids)) {
+          $nodes = $this->entityTypeManager->getStorage('node')->loadMultiple($nids);
+          $updated = [
+            'event' => 0,
+            'health_care_local_health_service' => 0,
+            'vha_facility_nonclinical_service' => 0,
+          ];
+          foreach ($nodes as $node) {
+            if ($node instanceof NodeInterface) {
+              // Make this change a new revision.
+              $node->setNewRevision(TRUE);
+              // Set as archived and unpublished.
+              $node->set('moderation_state', 'archived');
+              $node->setUnpublished();
+              // Set revision author to current user.
+              $node->setRevisionUserId($this->currentUser->id());
+              $node->setChangedTime(time());
+              $node->isDefaultRevision(TRUE);
+              $node->setRevisionCreationTime(time());
+              // Set revision log message.
+              $node->setRevisionLogMessage('Automatically archived when parent facility was archived.');
+              $node->save();
+              // Increment updated count.
+              $updated[$node->bundle()] += 1;
+            }
+          }
+
+          // Status message so editor knows the extent of the changes.
+          $message = $this->t('%services health services, %nonclinicals non-clinical services, and %events events were also archived.', [
+            '%services' => $updated['health_care_local_health_service'],
+            '%nonclinicals' => $updated['vha_facility_nonclinical_service'],
+            '%events' => $updated['event'],
+          ]);
+          $this->messenger->addStatus($message);
+        }
+
+      }
+    }
   }
 
   /**
