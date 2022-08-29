5698c8094a606978db9388ebb8cb294556d31b41
Jay Darnell <jay.darnell@civicactions.com>
2022-08-29 08:40:25 -0400

    VACMS-9950: Add alternate alias to nodes that are Lovell both (#10336)
    * VACMS-9950: add alternate alias to nodes that are Lovell both
    
    * VACMS-9950: fix phpstan errors
    
    * VACMS-9950: add phpstan ignore for magic setter method
    
    * VACMS-9950: update base url for Lovell VA based on UX proposal
    
    * VACMS-9950: updates from code review
    
    * Update comment for getExistingNodeAliases
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>
    
    Co-authored-by: Steve Wirt <swirtSJW@users.noreply.github.com>

--- docroot/modules/custom/va_gov_lovell/src/EventSubscriber/LovellEventSubscriber.php
+++ docroot/modules/custom/va_gov_lovell/src/EventSubscriber/LovellEventSubscriber.php
@@ -8,13 +8,24 @@ use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\core_event_dispatcher\EntityHookEvents;
 use Drupal\core_event_dispatcher\Event\Entity\EntityPresaveEvent;
+use Drupal\core_event_dispatcher\Event\Entity\EntityInsertEvent;
+use Drupal\core_event_dispatcher\Event\Entity\EntityUpdateEvent;
 use Drupal\menu_link_content\MenuLinkContentInterface;
+use Drupal\node\NodeInterface;
+use Drupal\path_alias\Entity\PathAlias;
+use Drupal\pathauto\PathautoGenerator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * VA.gov Lovell Entity Event Subscriber.
  */
 class LovellEventSubscriber implements EventSubscriberInterface {
+  const LOVELL_SECTIONS = [
+    '1040' => 'va',
+    '1039' => 'tricare',
+    '347' => 'both',
+  ];
+
   use StringTranslationTrait;
 
   /**
@@ -24,6 +35,14 @@ class LovellEventSubscriber implements EventSubscriberInterface {
    *  The entity manager.
    */
   private $entityTypeManager;
+
+  /**
+   * The pathauto generator.
+   *
+   * @var \Drupal\pathauto\PathautoGenerator
+   *  The pathauto generator.
+   */
+  private $pathautoGenerator;
 
   /**
    * The Messenger service.
@@ -37,14 +56,18 @@ class LovellEventSubscriber implements EventSubscriberInterface {
    *
    * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
    *   The string entity type service.
+   * @param \Drupal\pathauto\PathautoGenerator $pathautoGenerator
+   *   The pathauto generator service.
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger service.
    */
   public function __construct(
     EntityTypeManager $entityTypeManager,
+    PathautoGenerator $pathautoGenerator,
     MessengerInterface $messenger
   ) {
     $this->entityTypeManager = $entityTypeManager;
+    $this->pathautoGenerator = $pathautoGenerator;
     $this->messenger = $messenger;
   }
 
@@ -54,6 +77,8 @@ class LovellEventSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents(): array {
     return [
       EntityHookEvents::ENTITY_PRE_SAVE => 'entityPresave',
+      EntityHookEvents::ENTITY_INSERT => 'entityInsert',
+      EntityHookEvents::ENTITY_UPDATE => 'entityUpdate',
     ];
   }
 
@@ -66,6 +91,49 @@ class LovellEventSubscriber implements EventSubscriberInterface {
   public function entityPresave(EntityPresaveEvent $event): void {
     $entity = $event->getEntity();
     $this->setMenuSection($entity);
+    $this->blockLovellPathauto($entity);
+  }
+
+  /**
+   * Entity insert Event call.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Entity\EntityInsertEvent $event
+   *   The event.
+   */
+  public function entityInsert(EntityInsertEvent $event): void {
+    $entity = $event->getEntity();
+    $this->updatePathAliases($entity);
+  }
+
+  /**
+   * Entity update Event call.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Entity\EntityUpdateEvent $event
+   *   The event.
+   */
+  public function entityUpdate(EntityUpdateEvent $event): void {
+    $entity = $event->getEntity();
+    $this->updatePathAliases($entity);
+  }
+
+  /**
+   * Disable pathauto for Lovell nodes.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   Entity.
+   */
+  protected function blockLovellPathauto(EntityInterface $entity): void {
+    if (($entity instanceof NodeInterface)
+    && ($entity->hasField('path'))
+    && ($entity->hasField('field_administration'))) {
+      $section_id = $entity->get('field_administration')->target_id;
+      if (!array_key_exists($section_id, self::LOVELL_SECTIONS)) {
+        return;
+      }
+      // If this node is in a Lovell section disable pathauto pattern.
+      // @phpstan-ignore-next-line
+      $entity->path->pathauto = 0;
+    }
   }
 
   /**
@@ -88,15 +156,143 @@ class LovellEventSubscriber implements EventSubscriberInterface {
         /** @var \Drupal\node\NodeInterface $node*/
         if ($node->hasField('field_administration')) {
           $section_id = $node->get('field_administration')->target_id;
-          $section_map = [
-            '1040' => 'va',
-            '1039' => 'tricare',
-            '347' => 'both',
-          ];
-
           // Set section for this menu item.
-          if (!empty($section_map[$section_id])) {
-            $entity->set('field_menu_section', $section_map[$section_id]);
+          if (!empty(self::LOVELL_SECTIONS[$section_id])) {
+            $entity->set('field_menu_section', self::LOVELL_SECTIONS[$section_id]);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Update path aliases for Lovell content.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   Entity.
+   */
+  protected function updatePathAliases(EntityInterface $entity): void {
+    if (($entity instanceof NodeInterface)
+    && ($entity->isDefaultRevision())
+    && ($entity->hasField('path'))
+    && ($entity->hasField('field_administration'))) {
+      // If this content does not belong to a Lovell section do nothing.
+      $section_id = $entity->get('field_administration')->target_id;
+      if (!array_key_exists($section_id, self::LOVELL_SECTIONS)) {
+        return;
+      }
+
+      // Determine which prefixes are valid based on section.
+      $valid_prefixes = $this->getValidPrefixes($section_id);
+      $valid_aliases = $this->generateValidAliases($valid_prefixes, $entity);
+
+      // Get any existing aliases for this node.
+      $existing_aliases = $this->getExistingNodeAliases($entity);
+
+      // Omit existing aliases with a valid match from further processing.
+      $this->omitAliasMatches($valid_aliases, $existing_aliases);
+
+      // Commit any remaining valid aliases to the database.
+      foreach ($valid_aliases as $key => $valid_alias) {
+        $valid_alias->save();
+        unset($valid_aliases[$key]);
+      }
+
+      // Remove unused existing aliases.
+      foreach ($existing_aliases as $existing_alias) {
+        $existing_alias->delete();
+      }
+    }
+  }
+
+  /**
+   * Generate an array of valid Pathalias objects for this node.
+   *
+   * @param array $prefixes
+   *   An array of prefix strings.
+   * @param \Drupal\node\NodeInterface $node
+   *   Node.
+   *
+   * @return array
+   *   An array of Pathalias objects.
+   */
+  protected function generateValidAliases(array $prefixes, NodeInterface $node): array {
+    $new_url = $this->pathautoGenerator->createEntityAlias($node, 'return');
+    $url_pieces = explode('/', $new_url);
+    $new_aliases = [];
+    foreach ($prefixes as $section => $prefix) {
+      $new_alias = PathAlias::Create([
+        'path' => '/node/' . $node->id(),
+        'alias' => '/' . $prefix . '/' . implode('/', array_slice($url_pieces, 2)),
+        'langcode' => $node->language()->getId(),
+      ]);
+      $new_aliases[] = $new_alias;
+    }
+    return $new_aliases;
+  }
+
+  /**
+   * Generate valid lovell url prefixes for the provided section.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   Node.
+   *
+   * @return array
+   *   An array of existing Pathalias objects for this node.
+   */
+  protected function getExistingNodeAliases(NodeInterface $node): array {
+    // Retrieve existing aliases for this node.
+    $path = '/node/' . $node->id();
+    $path_alias_manager = $this->entityTypeManager->getStorage('path_alias');
+    $existing_aliases = $path_alias_manager->loadByProperties([
+      'path'     => $path,
+      'langcode' => 'en',
+    ]);
+
+    return $existing_aliases;
+  }
+
+  /**
+   * Generate valid lovell url prefixes for the provided section.
+   *
+   * @param string $section_id
+   *   An id for a section taxonomy term.
+   *
+   * @return array
+   *   An array of valid url prefixes .
+   */
+  protected function getValidPrefixes(string $section_id): array {
+    // Define valid url prefixes for Lovell content.
+    $valid_prefixes = [
+      '1039' => 'lovell-federal-tricare-health-care',
+      '1040' => 'lovell-federal-va-health-care',
+    ];
+
+    // If section is not both remove invalid prefixes.
+    if ($section_id !== '347') {
+      $valid_prefixes = array_intersect_key($valid_prefixes, [$section_id => 'keep']);
+    }
+
+    return $valid_prefixes;
+  }
+
+  /**
+   * Omit aliases common to the supplied arrays.
+   *
+   * @param array $valid_aliases
+   *   An array of aliases a node should have.
+   * @param array $existing_aliases
+   *   An array of aliases a node currently has.
+   */
+  protected function omitAliasMatches(array &$valid_aliases, array &$existing_aliases): void {
+    if (count($valid_aliases) && count($existing_aliases)) {
+      foreach ($valid_aliases as $id => $valid_alias) {
+        foreach ($existing_aliases as $section => $existing_alias) {
+          if ($valid_alias->alias->value === $existing_alias->alias->value) {
+            // Remove matched elements.
+            unset($valid_aliases[$id]);
+            unset($existing_aliases[$section]);
+            break;
           }
         }
       }
