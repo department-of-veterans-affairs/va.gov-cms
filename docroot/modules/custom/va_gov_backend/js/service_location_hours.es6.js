2a8ac7f75cb23622947b82626f42c0b1064c3c91
Christian Burk <christian.burk@civicactions.com>
2022-08-30 17:28:07 -0500

    Removes unnecessary js and adds tooltip

--- docroot/modules/custom/va_gov_backend/js/service_location_hours.es6.js
+++ docroot/modules/custom/va_gov_backend/js/service_location_hours.es6.js
@@ -2,11 +2,33 @@
  * @file
  */
 
-((Drupal) => {
+(($, Drupal) => {
   const displayHours = (value, toggle, table) => {
     if (toggle.checked) {
       if (toggle.value === value) {
         table.style.display = "block";
+        if (value = "0") {
+          $(table).once("button-build").each(function() {
+            const button = document.createElement("button");
+            button.className = "tooltip-toggle";
+            button.value =
+              "Why can't I edit this? VHA keeps these descriptions standardized to help Veterans identify the services they need.";
+            button.type = "button";
+            // Add css formatting from "tippy" css library.
+            button.ariaLabel = "tooltip";
+            button.setAttribute(
+              "data-tippy",
+              "Why can't I edit this?\nVHA keeps these descriptions standardized to help Veterans identify the services they need."
+            );
+            button.setAttribute("data-tippy-pos", "right");
+            button.setAttribute("data-tippy-animate", "fade");
+            button.setAttribute("data-tippy-size", "large");
+            table.className = `no-content health_service_text_container field-group-tooltip tooltip-layout centralized css-tooltip`;
+            // Smash it all together.
+            table.appendChild(button);
+          });
+
+        }
       } else {
         table.style.display = "none";
       }
@@ -14,15 +36,22 @@
   };
 
   Drupal.behaviors.vaGovServiceLocationHours = {
-    attach() {
+    attach(context) {
       // Grab our hour selects.
       const hourSelects = document.querySelectorAll(
         ".field--name-field-hours input"
       );
       hourSelects.forEach((hourSelect) => {
         // Grab our closest hours table.
-        const hours = hourSelect.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling;
-        const facilityHours = hourSelect.parentElement.parentElement.parentElement.parentElement.nextElementSibling;
+        const hours =
+                hourSelect.parentElement.parentElement.parentElement.parentElement
+                .parentElement.nextElementSibling;
+        const facilityHours =
+                hourSelect.parentElement.parentElement.parentElement.parentElement
+                .nextElementSibling;
+        // Build up our "?" button and tooltip text.
+
+
 
         window.addEventListener("load", () => {
           // Determine whether or not to display on load.
@@ -38,4 +67,4 @@
       });
     },
   };
-})(Drupal);
+})(jQuery, Drupal);
--- docroot/modules/custom/va_gov_backend/js/service_location_hours.js
+++ docroot/modules/custom/va_gov_backend/js/service_location_hours.js
@@ -5,11 +5,28 @@
 * @preserve
 **/
 
-(function (Drupal) {
+(function ($, Drupal) {
   var displayHours = function displayHours(value, toggle, table) {
     if (toggle.checked) {
       if (toggle.value === value) {
         table.style.display = "block";
+        if (value = "0") {
+          $(table).once("button-build").each(function () {
+            var button = document.createElement("button");
+            button.className = "tooltip-toggle";
+            button.value = "Why can't I edit this? VHA keeps these descriptions standardized to help Veterans identify the services they need.";
+            button.type = "button";
+
+            button.ariaLabel = "tooltip";
+            button.setAttribute("data-tippy", "Why can't I edit this?\nVHA keeps these descriptions standardized to help Veterans identify the services they need.");
+            button.setAttribute("data-tippy-pos", "right");
+            button.setAttribute("data-tippy-animate", "fade");
+            button.setAttribute("data-tippy-size", "large");
+            table.className = "no-content health_service_text_container field-group-tooltip tooltip-layout centralized css-tooltip";
+
+            table.appendChild(button);
+          });
+        }
       } else {
         table.style.display = "none";
       }
@@ -17,11 +34,12 @@
   };
 
   Drupal.behaviors.vaGovServiceLocationHours = {
-    attach: function attach() {
+    attach: function attach(context) {
       var hourSelects = document.querySelectorAll(".field--name-field-hours input");
       hourSelects.forEach(function (hourSelect) {
         var hours = hourSelect.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling;
         var facilityHours = hourSelect.parentElement.parentElement.parentElement.parentElement.nextElementSibling;
+
 
         window.addEventListener("load", function () {
           displayHours("2", hourSelect, hours);
@@ -35,4 +53,4 @@
       });
     }
   };
-})(Drupal);
+})(jQuery, Drupal);
