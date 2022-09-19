f8e663854b5e6d8a245e64c975bfa76258fe5731
Tim Cosgrove <tim.cosgrove@agile6.com>
2022-09-19 12:43:05 -0700

    Remove fallback.

--- docroot/sites/default/settings/settings.flysystem.php
+++ docroot/sites/default/settings/settings.flysystem.php
@@ -9,16 +9,9 @@ $bucket = getenv('CMS_S3_FILES_BUCKET') ?: '';
 $user_key = getenv('CMS_S3_FILES_KEY') ?: '';
 $user_secret = getenv('CMS_S3_FILES_SECRET') ?: '';
 
-// Set up a local fallback scheme.
-$schemes = [
-  'dsva-cms-s3-files' => [
-    'driver' => 'local',
-    'config' => [
-      'root' => 'sites/default/files/flysystem',
-      'public' => TRUE,
-    ],
-  ],
-];
+// We do not provide a fallback; if the connection cannot be made, the stream
+// wrapper should fail.
+$schemes = [];
 
 // If we have the information we need, set up an s3 scheme.
 if (!empty($bucket) && !empty($user_key) && !empty($user_secret)) {
