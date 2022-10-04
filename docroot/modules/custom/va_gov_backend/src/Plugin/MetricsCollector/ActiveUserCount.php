8ff797c8d7c45685ce8ec9894c1166f01605a1e0
Nate Douglas <ndouglas@users.noreply.github.com>
2022-10-04 12:10:28 -0400

    VACMS-10989: Adds new user count metrics (#10992)
    * VACMS-10989: Adds new user count metrics.
    
    * Support looking in specific section IDs.
    
    * Back to email.

--- docroot/modules/custom/va_gov_backend/src/Plugin/MetricsCollector/ActiveUserCount.php
+++ docroot/modules/custom/va_gov_backend/src/Plugin/MetricsCollector/ActiveUserCount.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\va_gov_backend\Plugin\MetricsCollector;
 
-use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Site\Settings;
 use Drupal\prometheus_exporter\Plugin\BaseMetricsCollector;
+use Drupal\workbench_access\WorkbenchAccessManager;
 use PNX\Prometheus\Gauge;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -28,9 +30,9 @@ class ActiveUserCount extends BaseMetricsCollector implements ContainerFactoryPl
   /**
    * The user type storage.
    *
-   * @var \Drupal\Core\Entity\EntityStorageInterface
+   * @var \Drupal\Core\Database\Connection
    */
-  protected $userStorage;
+  protected $database;
 
   /**
    * The settings service.
@@ -38,6 +40,13 @@ class ActiveUserCount extends BaseMetricsCollector implements ContainerFactoryPl
    * @var \Drupal\Core\Site\Settings
    */
   protected $settingsService;
+
+  /**
+   * The section IDs.
+   *
+   * @var string[]
+   */
+  protected $sectionIds;
 
   /**
    * UserCount constructor.
@@ -48,15 +57,29 @@ class ActiveUserCount extends BaseMetricsCollector implements ContainerFactoryPl
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityStorageInterface $user_storage
-   *   The user type storage.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The Drupal database connection.
    * @param \Drupal\Core\Site\Settings $settings
    *   The site settings.
+   * @param \Drupal\workbench_access\WorkbenchAccessManager $workbench_access_manager
+   *   The workbench access manager.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityStorageInterface $user_storage, Settings $settings) {
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    Connection $database,
+    Settings $settings,
+    WorkbenchAccessManager $workbench_access_manager,
+    EntityTypeManagerInterface $entity_type_manager
+  ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->userStorage = $user_storage;
+    $this->database = $database;
     $this->settingsService = $settings;
+    $sectionScheme = $entity_type_manager->getStorage('access_scheme')->load('section');
+    $this->sectionIds = $workbench_access_manager->getAllSections($sectionScheme);
   }
 
   /**
@@ -67,25 +90,73 @@ class ActiveUserCount extends BaseMetricsCollector implements ContainerFactoryPl
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager')->getStorage('user'),
-      $container->get('settings')
+      $container->get('database'),
+      $container->get('settings'),
+      $container->get('plugin.manager.workbench_access.scheme'),
+      $container->get('entity_type.manager')
     );
   }
 
   /**
-   * Gets a count for this metric.
+   * Gets a query of "real users".
+   *
+   * This should return the users who are not administrators, API users, or
+   * "ghost" users (users with duplicate accounts).
+   *
+   * @return \Drupal\Core\Entity\Query\QueryInterface
+   *   A query, not yet completed.
+   */
+  protected function getRealUserQuery() {
+    $disallowedRoles = [
+      'administrator',
+      'content_api_consumer',
+    ];
+    $query = $this->database->select('users_field_data', 'ufd');
+    // Email will lead the query to return 1 on non-prod environments, because
+    // of database sanitization.  It should work as expected on prod.
+    $query->addField('ufd', 'mail');
+    $query->innerJoin('user__roles', 'ur', 'ufd.uid = ur.entity_id AND ur.deleted = 0');
+    $query->innerJoin('users', 'u', 'ufd.uid = u.uid');
+    $query->leftJoin('section_association__user_id', 'suid', 'u.uid = suid.user_id_target_id');
+    $query->condition('ur.roles_target_id', $disallowedRoles, 'NOT IN');
+    // $query->condition('suid.entity_id', $this->sectionIds, 'IN');
+    $query->isNotNull('suid.entity_id');
+    $query->isNotNull('ufd.access');
+    $query->condition('status', 1);
+    return $query;
+  }
+
+  /**
+   * Gets a count of "real users".
+   *
+   * Total number of users who are not:
+   * - administrators.
+   * - API accounts, or.
+   * - “ghost” accounts.
    *
    * @return int
-   *   The count of active users in the most recent session write interval.
+   *   The count of real users.
    */
   protected function getCount() {
-    $query = $this->userStorage->getQuery();
-    $sessionWriteInterval = $this->settingsService->get('session_write_interval', 180);
-    $sinceTime = time() - $sessionWriteInterval;
+    $query = $this->getRealUserQuery();
+    $count = $query->distinct()->countQuery()->execute()->fetchField();
+    return $count;
+  }
+
+  /**
+   * Gets a count of users active since a specified time.
+   *
+   * @param int $timestamp
+   *   A cutoff timestamp for considering a user active.
+   *
+   * @return int
+   *   The count of active users in the specified time interval.
+   */
+  protected function getCountSinceTime($timestamp) {
+    $query = $this->getRealUserQuery();
     $query
-      ->accessCheck(TRUE)
-      ->condition('access', $sinceTime, '>=');
-    $count = $query->count()->execute();
+      ->condition('access', $timestamp, '>=');
+    $count = $query->distinct()->countQuery()->execute()->fetchField();
     return $count;
   }
 
@@ -93,8 +164,18 @@ class ActiveUserCount extends BaseMetricsCollector implements ContainerFactoryPl
    * {@inheritdoc}
    */
   public function collectMetrics() {
+    $now = time();
     $gauge = new Gauge($this->getNamespace(), 'total', $this->getDescription());
+    $sessionWriteInterval = $this->settingsService->get('session_write_interval', 180);
+    $sessionWriteIntervalTimestamp = $now - $sessionWriteInterval;
+    // 60 days in seconds
+    $sixtyDaysAgoTimestamp = $now - (60 * 86400);
+    // 180 days in seconds
+    $sixMonthsAgoTimestamp = $now - (180 * 86400);
     $gauge->set($this->getCount());
+    $gauge->set($this->getCountSinceTime($sessionWriteIntervalTimestamp), ['last' => 'session_write_interval']);
+    $gauge->set($this->getCountSinceTime($sixtyDaysAgoTimestamp), ['last' => '60days']);
+    $gauge->set($this->getCountSinceTime($sixMonthsAgoTimestamp), ['last' => '180days']);
     $metrics[] = $gauge;
     return $metrics;
   }
