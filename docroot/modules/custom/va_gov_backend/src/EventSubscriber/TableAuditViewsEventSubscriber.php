04ddc03808008bc746aa32b2ce7f77e7d98f85c4
Edmund Dunn <edmund.dunn@agile6.com>
2022-12-13 10:14:44 -0800

    VACMS-11415: orphaned paragraphs report

--- docroot/modules/custom/va_gov_backend/src/EventSubscriber/TableAuditViewsEventSubscriber.php
+++ docroot/modules/custom/va_gov_backend/src/EventSubscriber/TableAuditViewsEventSubscriber.php
@@ -7,7 +7,6 @@ use Drupal\Core\Link;
 use Drupal\Core\Url;
 use Drupal\entity_reference_revisions\EntityReferenceRevisionsOrphanPurger;
 use Drupal\va_gov_backend\Service\VaGovUrlInterface;
-use Drupal\views_event_dispatcher\Event\Views\ViewsPostExecuteEvent;
 use Drupal\views_event_dispatcher\Event\Views\ViewsPreRenderEvent;
 use Drupal\views_event_dispatcher\ViewsHookEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -67,17 +66,6 @@ class TableAuditViewsEventSubscriber implements EventSubscriberInterface {
   }
 
   /**
-   * Post execute event handler.
-   *
-   * @param \Drupal\views_event_dispatcher\Event\Views\ViewsPostExecuteEvent $event
-   *   The event.
-   */
-  public function postExecute(ViewsPostExecuteEvent $event): void {
-    $view = $event->getView();
-
-  }
-
-  /**
    * Pre render event handler.
    *
    * @param \Drupal\views_event_dispatcher\Event\Views\ViewsPreRenderEvent $event
@@ -87,31 +75,12 @@ class TableAuditViewsEventSubscriber implements EventSubscriberInterface {
    */
   public function preRender(ViewsPreRenderEvent $event): void {
     $view = $event->getView();
-    $orphan_count = 0;
-    if ($view->id() === 'orphaned_paragraphs') {
-      foreach ($view->result as $key => $value) {
-        $parent = va_gov_backend_get_top_parent_entity($value->_entity);
-        if (!$parent) {
-          $link = va_gov_backend_get_top_parent_entity_link($value->_entity);
-        }
-        $used = $this->purger->isUsed($value->_entity);
-        if (!$used) {
-          $orphan_count++;
-          $str = 'This is an orphan paragraph. ' . $orphan_count . ' orphaned paragraphs found.';
-          $value->_entity->set('revision_id', $str);
-        }
-        else {
-          unset($view->result[$key]);
-        }
-      }
-    }
     if ($view->id() === 'tables') {
       foreach ($view->result as $key => $value) {
         $node = va_gov_backend_get_parent_node($value->_entity);
         if (empty($node)) {
           $str = 'This is an orphan paragraph.';
           $value->_entity->set('parent_field_name', $str);
-
         }
         else {
           $link = Link::fromTextAndUrl($node->getTitle(), $node->toUrl())->toRenderable();
@@ -136,7 +105,6 @@ class TableAuditViewsEventSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents(): array {
     return [
       ViewsHookEvents::VIEWS_PRE_RENDER => 'preRender',
-      ViewsHookEvents::VIEWS_POST_EXECUTE => 'postExecute',
     ];
   }
 
