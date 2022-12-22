b11835b645435f768b705d5028475a1601ada34d
Christian Burk <christian.burk@civicactions.com>
2022-12-22 14:45:16 -0600

    VACMS-11980: Updates Cypress test, reusing pre-existing steps

--- tests/cypress/integration/behavioral/common/the_option_from_dropdown_should_be_selected.js
+++ tests/cypress/integration/behavioral/common/the_option_from_dropdown_should_be_selected.js
@@ -7,11 +7,3 @@ Given('the option {string} from dropdown {string} should be selected', (option, 
 Given('the option {string} from dropdown with selector {string} should be selected', (option, selector) => {
   cy.get(`${selector} option:selected`).should('have.text', option);
 });
-
-Given('an option with the text {string} from dropdown with selector {string} should be selectable', (text, selector) => {
-  cy.get(`${selector} option:not([class*="hidden-option"])`).should('contain.text', text);
-});
-
-Given('an option with the text {string} from dropdown with selector {string} should not be selectable', (text, selector) => {
-  cy.get(`${selector} option:not([class*="hidden-option"])`).should('not.contain.text', text);
-});
