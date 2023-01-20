380add573637c92df064cc024cbd7953b54aff0a
Christia Troyer <christia.troyer@civicactions.com>
2023-01-20 15:43:46 -0800

    VACMS-11609: Initial new definitions

---
+++ tests/cypress/integration/behavioral/common/i_should_ssee.js
@@ -0,0 +1,22 @@
+import { Then } from 'cypress-cucumber-preprocessor/steps';
+
+Then('I should ssee an element with the selector {string}', (selector) => cy.get(selector).should('be.visible'));
+Then('I should not ssee an element with the selector {string}', (selector) => cy.get(selector).should('not.be.visible'));
+
+Then('I should ssee {string}', (text) => cy.get('div.page-wrapper').contains(text).should('be.visible'));
+Then('I should not ssee {string}', (text) => cy.get('div.page-wrapper').contains(text).should('not.be.visible'));
+
+Then('I should ssee an element with the xpath {string}', (expression) => cy.xpath(expression).should('be.visible'));
+Then('I should not ssee an element with the xpath {string}', (expression) => cy.xpath(expression).should('not.be.visible'));
+
+Then('I should ssee xpath {string}', (expression) => cy.xpath(expression).should('be.visible'));
+Then('I should not ssee xpath {string}', (expression) => cy.xpath(expression).should('not.be.visible'));
+
+Then('I should ssee an image with the selector {string}', (selector) => {
+  cy.get(selector)
+    .should('be.visible')
+    .and(($img) => {
+      expect($img[0].naturalWidth).to.be.greaterThan(0);
+      expect($img[0].naturalHeight).to.be.greaterThan(0);
+    });
+});
