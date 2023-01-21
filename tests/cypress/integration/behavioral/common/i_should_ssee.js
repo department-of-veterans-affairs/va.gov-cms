b848d06ad679d163aa093bada6188a3d49b4cb21
Christia Troyer <christia.troyer@civicactions.com>
2023-01-20 17:28:59 -0800

    VACMS-11609: Add dropdown options to should see

--- tests/cypress/integration/behavioral/common/i_should_ssee.js
+++ tests/cypress/integration/behavioral/common/i_should_ssee.js
@@ -12,6 +12,14 @@ Then('I should not ssee an element with the xpath {string}', (expression) => cy.
 Then('I should ssee xpath {string}', (expression) => cy.xpath(expression).should('be.visible'));
 Then('I should not ssee xpath {string}', (expression) => cy.xpath(expression).should('not.be.visible'));
 
+Given('I should ssee an option with the text {string} from dropdown with selector {string}', (text, selector) => {
+  cy.get(`${selector} option:not([class*="hidden-option"])`).should('contain.text', text);
+});
+
+Given('I should not ssee an option with the text {string} from dropdown with selector {string}', (text, selector) => {
+  cy.get(`${selector} option:not([class*="hidden-option"])`).should('not.contain.text', text);
+});
+
 Then('I should ssee an image with the selector {string}', (selector) => {
   cy.get(selector)
     .should('be.visible')
