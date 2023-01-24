130d6b564a016b02a59b8bb05cb87114c54631db
Nathan Douglas <github@darkdell.net>
2023-01-24 15:56:37 -0500

    Test no-log approach.

--- tests/scripts/ci-wrapper.sh
+++ tests/scripts/ci-wrapper.sh
@@ -15,16 +15,13 @@ set +o allexport
 test_name="${1}"
 composer_name="${2:-va:test:${test_name}}"
 status_name="${3:-va/tests/${test_name}}"
-log_name="${test_name}-ci.log"
 
 echo "Test name: ${test_name}"
 echo "Composer name: ${composer_name}"
 echo "Status name: ${status_name}"
 
-result="$(time composer "${composer_name}" 2>&1 > "${log_name}")"
+result="$(time composer "${composer_name}" 2>&1)"
 exit_code=$?
-
-cat "${log_name}"
 
 if [ -n "${GITHUB_TOKEN}" ]; then 
   if [ "${exit_code}" -eq 0 ]; then
