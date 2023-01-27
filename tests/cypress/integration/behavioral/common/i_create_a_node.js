bcfa7206ccf2e82c3a43a3c10bb3e6fb3c0c46b8
Nate Douglas <ndouglas@users.noreply.github.com>
2023-01-27 15:34:23 -0500

    VACMS-11631: Create and save media entities in functional tests. (#12349)
    * VACMS-11631: Create and save media entities in functional tests.
    
    * Stop ignoring test.

--- tests/cypress/integration/behavioral/common/i_create_a_node.js
+++ tests/cypress/integration/behavioral/common/i_create_a_node.js
@@ -128,7 +128,7 @@ Given('I create a {string} node', (contentType) => {
 
 Given('I create a {string} node and continue', (contentType) => {
   let creator = creators[contentType];
-  assert.isNotNull(creator, `I do not know how to create ${contentType} nodes yet.  Please add a definition in ${__filename}.`);
+  assert.isDefined(creator, `I do not know how to create ${contentType} nodes yet.  Please add a definition in ${__filename}.`);
   creator().then(() => {
     cy.get('form.node-form').find('input#edit-save-continue').click();
     cy.location('pathname', {timeout: 10000}).should('not.include', '/node/add');
