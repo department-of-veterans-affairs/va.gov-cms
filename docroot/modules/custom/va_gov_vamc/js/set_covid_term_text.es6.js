2622e2e9793ea385db007482026ab2ea09060cff
Christian Burk <christian.burk@civicactions.com>
2023-01-30 13:20:20 -0600

    VACMS-10708: Adds js with proper string interpolation

--- docroot/modules/custom/va_gov_vamc/js/set_covid_term_text.es6.js
+++ docroot/modules/custom/va_gov_vamc/js/set_covid_term_text.es6.js
@@ -5,21 +5,6 @@
 ((Drupal) => {
   let statusId;
   const textSetter = () => {
-    // The targeted tooltip fieldset.
-    const fieldset = document.getElementById(
-      "covid-safety-guidelines-status-text"
-    );
-
-    // Remove the previous setting = sanity reset.
-    if (document.getElementById("covid-safety-guidelines-status-text-target")) {
-      document
-        .getElementById("covid-safety-guidelines-status-text-target")
-        .remove();
-      document
-        .getElementById("covid-safety-guidelines-status-text-prefix")
-        .remove();
-    }
-
     const covidStatusValue = document.querySelectorAll(
       ".form-item--field-supplemental-status input"
     );
@@ -29,33 +14,21 @@
         statusId = element.value;
       }
     });
-
-    // We don't want the tooltip if a status isn't set.
-    fieldset.style.display = "none";
-
-    if (statusId) {
-      fieldset.style.display = "block";
-
-      // The text that is placed in the covid tooltip status box.
-      const covidStatusTextDiv = document.createElement("div");
-      covidStatusTextDiv.id = "covid-safety-guidelines-status-text-target";
-      covidStatusTextDiv.innerHTML =
-        drupalSettings.vamcCovidStatusTermText[statusId].name +
-        drupalSettings.vamcCovidStatusTermText[statusId].description;
-      fieldset.append(covidStatusTextDiv);
-          iframeDocument = document.querySelector("iframe").contentDocument;
-          iframeDocument.body.innerHTML =
-      drupalSettings.vamcCovidStatusTermText[statusId].name +
-        drupalSettings.vamcCovidStatusTermText[statusId].description;
-
-
-      // Covid guidelines legend and description.
-      const covidStatusTextDivPrefix = document.createElement("div");
-      covidStatusTextDivPrefix.id =
-        "covid-safety-guidelines-status-text-prefix";
-      covidStatusTextDivPrefix.innerHTML =
-        '<h5>Guidelines</h5><div class="fieldset__description">Site visitors will see the following message for the level you selected.</div>';
-      fieldset.before(covidStatusTextDivPrefix);
+    // If there's nothing in the COVID Details field
+    // or when the COVID status radio button is changed,
+    // change the COVID Details field to the appropriate COVID status term description
+    let iframeDocument = "";
+    if (
+      document.querySelector(
+        "#cke_edit-field-supplemental-status-more-i-0-value iframe"
+      )
+    ) {
+      iframeDocument = document.querySelector(
+        "#cke_edit-field-supplemental-status-more-i-0-value iframe"
+      ).contentDocument;
+      iframeDocument.body.innerHTML = `<p>
+        ${drupalSettings.vamcCovidStatusTermText[statusId].name}</p>
+        ${drupalSettings.vamcCovidStatusTermText[statusId].description}`;
     }
   };
 
@@ -63,23 +36,15 @@
     attach() {
       // Let's set the text on page load, and whenever radios are clicked.
       window.addEventListener("DOMContentLoaded", textSetter);
-      setTimeout(() => {
-
-      const supplemental_status_choices = document.querySelectorAll("[id^='edit-field-supplemental-status-']");
-      function attachListener() {
-        // check if an interval has already been set up
-        supplemental_status_choices.forEach(choice => {
-          document
+      const supplementalStatusChoices = document.querySelectorAll(
+        ".form-item--field-supplemental-status [id^='edit-field-supplemental-status-']"
+      );
+      // check if an interval has already been set up
+      supplementalStatusChoices.forEach((choice) => {
+        document
           .getElementById(choice.id)
           .addEventListener("click", textSetter);
       });
-    }
-  }, 10000);
-  // TODO: fix the null issue
-  /* set_covid_term_text.js?rnkeh2:35 Uncaught TypeError: Cannot read properties of null (reading 'contentDocument')
-    at textSetter (set_covid_term_text.js?rnkeh2:35:56)
-    */
-
     },
   };
 })(Drupal);
--- docroot/modules/custom/va_gov_vamc/js/set_covid_term_text.js
+++ docroot/modules/custom/va_gov_vamc/js/set_covid_term_text.js
@@ -8,13 +8,6 @@
 (function (Drupal) {
   var statusId = void 0;
   var textSetter = function textSetter() {
-    var fieldset = document.getElementById("covid-safety-guidelines-status-text");
-
-    if (document.getElementById("covid-safety-guidelines-status-text-target")) {
-      document.getElementById("covid-safety-guidelines-status-text-target").remove();
-      document.getElementById("covid-safety-guidelines-status-text-prefix").remove();
-    }
-
     var covidStatusValue = document.querySelectorAll(".form-item--field-supplemental-status input");
 
     covidStatusValue.forEach(function (element) {
@@ -23,37 +16,21 @@
       }
     });
 
-    fieldset.style.display = "none";
-
-    if (statusId) {
-      fieldset.style.display = "block";
-
-      var covidStatusTextDiv = document.createElement("div");
-      covidStatusTextDiv.id = "covid-safety-guidelines-status-text-target";
-      covidStatusTextDiv.innerHTML = drupalSettings.vamcCovidStatusTermText[statusId].name + drupalSettings.vamcCovidStatusTermText[statusId].description;
-      fieldset.append(covidStatusTextDiv);
-      iframeDocument = document.querySelector("iframe").contentDocument;
-      iframeDocument.body.innerHTML = drupalSettings.vamcCovidStatusTermText[statusId].name + drupalSettings.vamcCovidStatusTermText[statusId].description;
-
-      var covidStatusTextDivPrefix = document.createElement("div");
-      covidStatusTextDivPrefix.id = "covid-safety-guidelines-status-text-prefix";
-      covidStatusTextDivPrefix.innerHTML = '<h5>Guidelines</h5><div class="fieldset__description">Site visitors will see the following message for the level you selected.</div>';
-      fieldset.before(covidStatusTextDivPrefix);
+    var iframeDocument = "";
+    if (document.querySelector("#cke_edit-field-supplemental-status-more-i-0-value iframe")) {
+      iframeDocument = document.querySelector("#cke_edit-field-supplemental-status-more-i-0-value iframe").contentDocument;
+      iframeDocument.body.innerHTML = "<p>\n        " + drupalSettings.vamcCovidStatusTermText[statusId].name + "</p>\n        " + drupalSettings.vamcCovidStatusTermText[statusId].description;
     }
   };
 
   Drupal.behaviors.vaGovSetCovidTermText = {
     attach: function attach() {
       window.addEventListener("DOMContentLoaded", textSetter);
-      setTimeout(function () {
+      var supplementalStatusChoices = document.querySelectorAll(".form-item--field-supplemental-status [id^='edit-field-supplemental-status-']");
 
-        var supplemental_status_choices = document.querySelectorAll("[id^='edit-field-supplemental-status-']");
-        function attachListener() {
-          supplemental_status_choices.forEach(function (choice) {
-            document.getElementById(choice.id).addEventListener("click", textSetter);
-          });
-        }
-      }, 10000);
+      supplementalStatusChoices.forEach(function (choice) {
+        document.getElementById(choice.id).addEventListener("click", textSetter);
+      });
     }
   };
 })(Drupal);
