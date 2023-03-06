3a637340c02bbd5b78981edfc37ebb584c11c8a0
Christian Burk <christian.burk@civicactions.com>
2023-03-06 16:44:13 -0600

    VACMS-12619: Adds validation to Facility to VAMC system health service

--- docroot/modules/custom/va_gov_vamc/src/Plugin/Validation/Constraint/SectionMatcher.php
+++ docroot/modules/custom/va_gov_vamc/src/Plugin/Validation/Constraint/SectionMatcher.php
@@ -16,26 +16,11 @@ use Symfony\Component\Validator\Constraint;
 class SectionMatcher extends Constraint {
 
   /**
-   * The message that will be shown if the Facility is not in the chosen Section.
-   *
-   * @see \Drupal\va_gov_vamc\Plugin\Validation\Constraint\SectionMatcherValidator
-   */
-  public $notSectionMatchFacility = 'The Facility does not match the Section. Please select a matching option.';
-
-  /**
-   * The message that will be shown if the VAMC system health service is not in the chosen Section.
-   *
-   * @see \Drupal\va_gov_backend\Plugin\Validation\Constraint\SectionMatcherValidator
-   */
-  public $notSectionMatchService = 'The VAMC system health service does not match the Section. Please select a matching option.';
-
-  /**
-   * The message that will be shown if the value is not a valid tty.
+   * Shown if Facility or VAMC system health service do not match Section.
    *
    * @var string
-   * @see \Drupal\va_gov_backend\Plugin\Validation\Constraint\SectionMatcherValidator
+   * @see \Drupal\va_gov_vamc\Plugin\Validation\Constraint\SectionMatcherValidator
    */
-  public $notSectionMatchEither = 'The Facility and VAMC system health service do not match the section. Please select matching options.';
-
+  public $notSectionMatch = 'The selected option for <strong>%fieldLabel</strong> is not part of the <strong>%section</strong> section. Please choose a different option or change the section settings to match.';
 
 }
