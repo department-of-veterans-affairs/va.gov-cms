6e618a00e1056d6f78049e6e9c1b015391cec501
Christian Burk <omahane@gmail.com>
2023-03-07 11:49:32 -0600

    VACMS-12619: Chaged from snake_case to camelCase.

--- docroot/modules/custom/va_gov_vamc/src/Plugin/Validation/Constraint/SectionMatcherValidator.php
+++ docroot/modules/custom/va_gov_vamc/src/Plugin/Validation/Constraint/SectionMatcherValidator.php
@@ -17,12 +17,12 @@ class SectionMatcherValidator extends ConstraintValidator {
     $entity = $items->getEntity();
     $sectionTermID = $entity->field_administration->target_id;
     $fieldLabel = $items->getFieldDefinition()->getLabel();
-    $node_storage = \Drupal::entityTypeManager()->getStorage('node');
-    $term_storage = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
-    $sectionName = $term_storage->load($sectionTermID)->getName();
+    $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+    $termStorage = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
+    $sectionName = $termStorage->load($sectionTermID)->getName();
     foreach ($items as $item) {
       /** @var \Drupal\va_gov_vamc\Plugin\Validation\Constraint\SectionMatcher $constraint */
-      $refEnt = $node_storage->load($item->target_id);
+      $refEnt = $nodeStorage->load($item->target_id);
       if ($refEnt->field_administration->target_id !== $sectionTermID) {
         $this->context->addViolation($constraint->notSectionMatch, [
           '%section' => $sectionName,
