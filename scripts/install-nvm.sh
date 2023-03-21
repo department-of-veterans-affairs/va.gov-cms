d696941792c84ee889fac25257ce28ef211af924
Edmund Dunn <edmund.dunn@agile6.com>
2023-03-21 13:58:33 -0700

    VACMS-12870: removed mouf installer from composer

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -9,11 +9,6 @@ nvm install 14.15.0
 # Install latest version of node.js v16
 nvm install 16.19.1
 
-echo "export NVM_DIR=\"$HOME/.nvm\"" >> $BASH_ENV
-echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
-echo "[ -s \"$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"" >> $BASH_ENV
-echo "nvm use 16.19.1" >> $BASH_ENV
-
 # Verify node is installed.
 node -v
 
