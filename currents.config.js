0bfd242c7d330ed1d21b058247d33ae1f8ae90f9
Nathan Douglas <github@darkdell.net>
2023-03-23 09:45:21 -0400

    Couple other tweaks for Sorry-Cypress.

---
+++ currents.config.js
@@ -0,0 +1,6 @@
+// currents.config.js is used by Sorry-Cypress to orchestrate your Cypress tests.
+module.exports = {
+  projectId: "va_cms",
+  recordKey: "platform-cms",
+  cloudServiceUrl: "http://sorry-cypress-director:1234/",
+};
