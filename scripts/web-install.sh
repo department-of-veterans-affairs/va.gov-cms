0350f1caf90d733282daef09afe582374fa7234b
Edmund Dunn <edmund.dunn@agile6.com>
2023-04-25 11:12:19 -0700

    VACMS-13494: added nvm commands to web:build and web:install

--- scripts/web-install.sh
+++ scripts/web-install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source ~/.bashrc
+
 # Installs the content-build dependencies.
 
 repo_root="$(git rev-parse --show-toplevel)"
@@ -8,7 +13,7 @@ pushd "${repo_root}" > /dev/null
 pushd "./bin"
 ln -sf ../docroot/libraries/yarn/bin/yarn ./yarn
 popd
-
+nvm install
 echo "Node $(node -v)"
 echo "NPM $(npm -v)"
 echo "Yarn $(yarn -v)"
@@ -16,6 +21,7 @@ echo "Yarn $(yarn -v)"
 ./scripts/composer/check_yarn_version.sh
 
 pushd "./web"
+nvm install
 yarn run install-repos
 export NODE_EXTRA_CA_CERTS=/etc/pki/tls/certs/ca-bundle.crt
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=TRUE
