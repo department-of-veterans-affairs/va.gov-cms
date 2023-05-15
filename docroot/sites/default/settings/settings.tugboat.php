36ba8cc2cf18510925e8bc2074f71cf5986eb2f9
Nate Douglas <ndouglas@users.noreply.github.com>
2023-05-15 15:30:55 -0400

    Update settings.tugboat.php

--- docroot/sites/default/settings/settings.tugboat.php
+++ docroot/sites/default/settings/settings.tugboat.php
@@ -80,8 +80,8 @@ $settings['cms_datadog_api_key'] = getenv('CMS_DATADOG_API_KEY');
 // However, we want to _preserve_ the PIV login interface on Tugboat PR
 // environments so that we can test the login page and the logic behind it
 // as it would run on staging and production.
-$tugboat_preview_type = getenv('TUGBOAT_PREVIEW_TYPE');
-$is_pull_request = isset($tugboat_preview_type) && $tugboat_preview_type !== 'pullrequest';
+$tugboat_preview_type = getenv('TUGBOAT_PREVIEW_TYPE') ?? '';
+$is_pull_request = $tugboat_preview_type === 'pullrequest';
 if (!$is_pull_request) {
   // Disable SSO variation of the user login form.
   $config['simplesamlphp_auth.settings']['activate'] = FALSE;
