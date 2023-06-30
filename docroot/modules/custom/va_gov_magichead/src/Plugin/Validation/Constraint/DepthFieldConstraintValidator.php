b746d4b4854125f4a3db8eb260886547a78ca8fb
Daniel Sasser <daniel.sasser@agile6.com>
2023-06-30 12:46:26 -0700

    VACMS-13756: Updates the constraint to apply validation logic.

--- docroot/modules/custom/va_gov_magichead/src/Plugin/Validation/Constraint/DepthFieldConstraintValidator.php
+++ docroot/modules/custom/va_gov_magichead/src/Plugin/Validation/Constraint/DepthFieldConstraintValidator.php
@@ -15,33 +15,26 @@ class DepthFieldConstraintValidator extends ConstraintValidator {
    * This one minus previous one is 1 or less, can be negative
    *
    * @param $value
+   *   The field values.
    * @param \Symfony\Component\Validator\Constraint $constraint
    *   The constraint for the validation.
    */
   public function validate($value, Constraint $constraint) {
-//    // Get field's parent entity to access other fields on this entity.
-//    /** @var \Drupal\entity_reference_hierarchy_revisions\Entity\STUFF $entity */
-//    $entity = $field->getEntity();
-//
-//    if ($entity &&
-//      $entity->hasField('field_va_benefit_eligibility[0][depth]') &&
-//      $entity->hasField('ALSO THAT but the next one (or previous one)?')
-//    ) {
-//      // Get the value of the first depth field.
-//      /** @var  */
-//      $thisDepth = $entity->STUFF;
-//      // Get the value of the previous depth field.
-//      /** @var  */
-//      $thatDepth = $entity->STUFF;
-//
-//      // Display an error message, if difference is more than 1.
-//      if ( $thisDepth - $thatDepth >= '1') {
-        $this->context->buildViolation($constraint->errorMessage)
-          ->atPath('field_test')
-          ->addViolation();
-//      }
-//    }
-
+    // Return early if we don't have more than 1 value.
+    if (count($value) < 2) {
+      return;
+    }
+    foreach ($value as $delta => $item) {
+      $values = $item->getValue();
+      $currentItemDepth = $values['depth'];
+      if ($delta > 0) {
+        if (($currentItemDepth - $lastItemDepth) > 1) {
+          $this->context->buildViolation($constraint->errorMessage)
+            ->atPath((string) $delta)
+            ->addViolation();
+        }
+      }
+      $lastItemDepth = $currentItemDepth;
+    }
   }
-
 }
