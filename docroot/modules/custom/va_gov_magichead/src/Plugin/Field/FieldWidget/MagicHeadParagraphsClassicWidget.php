6280dfb9ce85bb31f155d5a9351ab342fdda81f6
Christia Troyer <christia.troyer@civicactions.com>
2023-07-05 13:08:18 -0700

    VACMS-14042: Small fixes

--- docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsClassicWidget.php
+++ docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsClassicWidget.php
@@ -1,12 +1,11 @@
 <?php
 
-
 namespace Drupal\va_gov_magichead\Plugin\Field\FieldWidget;
 
 use Drupal\entity_reference_hierarchy_paragraphs\Plugin\Field\FieldWidget\InlineParagraphsHierarchyWidget;
 
 /**
- * Plugin implementation of the 'entity_reference_hierarchy_autocomplete' widget.
+ * Plugin implementation of 'entity_reference_hierarchy_autocomplete' widget.
  *
  * @FieldWidget(
  *   id = "magichead_paragraphs_classic",
