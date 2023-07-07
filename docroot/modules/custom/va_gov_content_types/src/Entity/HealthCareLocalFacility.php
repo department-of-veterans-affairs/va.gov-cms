867ba488b56b8ee1dd4813bdd7e0e7e22fd4b087
Nathan Douglas <github@darkdell.net>
2023-07-07 14:44:12 -0400

    Removes redundant trait from HealthCareLocalFacility.

--- docroot/modules/custom/va_gov_content_types/src/Entity/HealthCareLocalFacility.php
+++ docroot/modules/custom/va_gov_content_types/src/Entity/HealthCareLocalFacility.php
@@ -2,13 +2,9 @@
 
 namespace Drupal\va_gov_content_types\Entity;
 
-use Drupal\va_gov_content_types\Traits\DidChangeOperatingStatusTrait;
-
 /**
  * Provides a class for the health_care_local_facility content type.
  */
 class HealthCareLocalFacility extends VaNode implements HealthCareLocalFacilityInterface {
-
-  use DidChangeOperatingStatusTrait;
 
 }
--- docroot/modules/custom/va_gov_content_types/src/Entity/HealthCareLocalFacilityInterface.php
+++ docroot/modules/custom/va_gov_content_types/src/Entity/HealthCareLocalFacilityInterface.php
@@ -2,13 +2,9 @@
 
 namespace Drupal\va_gov_content_types\Entity;
 
-use Drupal\va_gov_content_types\Interfaces\DidChangeOperatingStatusInterface;
-
 /**
  * Provides an interface for health_care_local_facility nodes.
  */
-interface HealthCareLocalFacilityInterface extends
-  VaNodeInterface,
-  DidChangeOperatingStatusInterface {
+interface HealthCareLocalFacilityInterface extends VaNodeInterface {
 
 }
