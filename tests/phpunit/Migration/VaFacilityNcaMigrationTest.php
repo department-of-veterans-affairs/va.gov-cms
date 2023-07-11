286315061c0fb17fd445ef05a0e0ec1bdef0ff5a
Christian Burk <omahane@gmail.com>
2023-07-11 10:36:30 -0500

    VACMS 14237: fix str replace process plugin (#14310)
    * VACMS-14237: Adds patch and removes old ones.
    
    * VACMS-14237: Adds patch file downloaded from project
    
    * VACMS-14237: Adds a test of phone and when phone is null
    
    * VACMS-14237: Updates lock file

--- tests/phpunit/Migration/VaFacilityNcaMigrationTest.php
+++ tests/phpunit/Migration/VaFacilityNcaMigrationTest.php
@@ -13,19 +13,19 @@ use Tests\Support\Classes\VaGovExistingSiteBase;
  * @group functional
  * @group all
  */
-class VaHealthCareLocalFacilityMigrationTest extends VaGovExistingSiteBase {
+class VaFacilityNcaMigrationTest extends VaGovExistingSiteBase {
 
   /**
-   * Test the VA Health Care Local Facility Migration.
+   * Test the VA National Cemetery Administration Facility Migration.
    *
    * This test first imports a new facility
    * and verifies that a new node is created.
    * It then re-runs the same migration with updated data and verifies that the
    * updated data has been saved to the node.
    *
-   * @dataProvider vaHealthCareLocalFacilityDataProvider
+   * @dataProvider vaFacilityNcaDataProvider
    */
-  public function testVaLocalHealthCareFacilityMigration(
+  public function testVaFacilityNcaMigration(
     string $migration_id,
     string $bundle,
     string $json,
@@ -45,30 +45,34 @@ class VaHealthCareLocalFacilityMigrationTest extends VaGovExistingSiteBase {
   }
 
   /**
-   * Data provider for testVaLocalHealthCareFacilityMigration.
+   * Data provider for testVaFacilityNcaMigration.
    *
    * @return \Generator
    *   Test assertion data.
    */
-  public function vaHealthCareLocalFacilityDataProvider() : \Generator {
+  public function vaFacilityNcaDataProvider() : \Generator {
     yield 'Initial migration completes successfully' => [
-      'va_node_health_care_local_facility',
-      'health_care_local_facility',
-      file_get_contents(__DIR__ . '/fixtures/health_care_local_facility.json'),
+      'va_node_facility_nca',
+      'nca_facility',
+      file_get_contents(__DIR__ . '/fixtures/nca_facility.json'),
       [
-        'field_facility_locator_api_id' => 'vha_999999',
-        'field_phone_number' => '309-827-4090',
+        'field_facility_locator_api_id' => 'nca_000',
+        'field_phone_number' => '9137584106',
       ],
       1,
       FALSE,
     ];
     yield 'Updated migration completes successfully' => [
-      'va_node_health_care_local_facility',
-      'health_care_local_facility',
-      file_get_contents(__DIR__ . '/fixtures/health_care_local_facility_updated.json'),
+      'va_node_facility_nca',
+      'nca_facility',
+      file_get_contents(__DIR__ . '/fixtures/nca_facility_updated.json'),
       [
-        'field_facility_locator_api_id' => 'vha_999999',
-        'field_phone_number' => '309-827-4091',
+        'field_facility_locator_api_id' => 'nca_000',
+        // In the updated file, the phone number is null,
+        // but we can't test a null field (no data)
+        // so we test that the migration itself was successful
+        // by also changing the fax number and checking it.
+        'field_fax_number' => '9137584137',
       ],
       1,
       TRUE,
