cf494970f3b944aa4fc094706e742d7e284cf683
Daniel Sasser <daniel.sasser@agile6.com>
2023-07-13 15:52:18 -0700

    VACMS-13143: Fixes library attachment in widget class.

--- docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsClassicWidget.php
+++ docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsClassicWidget.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\va_gov_magichead\Plugin\Field\FieldWidget;
 
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\entity_reference_hierarchy_paragraphs\Plugin\Field\FieldWidget\InlineParagraphsHierarchyWidget;
 
 /**
- * Plugin implementation of 'entity_reference_hierarchy_autocomplete' widget.
+ * Plugin implementation of 'magichead_paragraphs_classic' widget.
  *
  * @FieldWidget(
  *   id = "magichead_paragraphs_classic",
@@ -17,9 +19,14 @@ use Drupal\entity_reference_hierarchy_paragraphs\Plugin\Field\FieldWidget\Inline
  * )
  */
 class MagicHeadParagraphsClassicWidget extends InlineParagraphsHierarchyWidget {
-  public function formElement(FieldItemListInterface $items, $delta, array &$element, array &$form,
-    FormStateInterface $form_state) {
-    $element['magichead_paragraphs_classic']['#attached']['library'][] = 'va_gov_magichead/magichead_tree_lines';
+
+  /**
+   * {@inheritDoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+    $element['#attached']['library'][] = 'va_gov_magichead/magichead_tree_lines';
+    return $element;
   }
 
 }
