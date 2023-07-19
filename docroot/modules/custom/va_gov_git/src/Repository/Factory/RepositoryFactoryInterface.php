64223af9d789e98366e35c2bdf02aa0e9c6ec398
Nathan Douglas <github@darkdell.net>
2023-07-19 08:44:18 -0400

    VACMS-14423: Adds `va_gov_git` module.

---
+++ docroot/modules/custom/va_gov_git/src/Repository/Factory/RepositoryFactoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\va_gov_git\Repository\Factory;
+
+/**
+ * Interface for the repository factory.
+ *
+ * This service provides a way to create services corresponding to specific Git
+ * repositories.
+ *
+ * At this time, we're primarily interested in two repositories:
+ * - The `va.gov-cms` repository.
+ * - The `content-build` repository.
+ */
+interface RepositoryFactoryInterface {
+
+}
