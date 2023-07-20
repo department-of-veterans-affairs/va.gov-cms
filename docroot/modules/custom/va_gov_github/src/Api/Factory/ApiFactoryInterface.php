8751f3fc2bbf7a822ab3eee82dcb849c615c9398
Nathan Douglas <github@darkdell.net>
2023-07-20 10:33:11 -0400

    VACMS-14460: Adds `va_gov_github` module.

---
+++ docroot/modules/custom/va_gov_github/src/Api/Factory/ApiFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\va_gov_github\Api\Factory;
+
+/**
+ * An interface for the GitHub Api factory service.
+ *
+ * This service is used to create GitHub Api instances, and to make these
+ * operations testable.
+ */
+interface ApiFactoryInterface {
+
+}
