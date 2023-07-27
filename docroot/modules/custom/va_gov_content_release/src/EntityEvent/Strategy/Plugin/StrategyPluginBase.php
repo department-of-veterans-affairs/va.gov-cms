058a9ceb16cf590ec42117ea5ec8ec7de11faac8
Nate Douglas <ndouglas@users.noreply.github.com>
2023-07-27 17:22:08 -0400

    VACMS-13852: Creates EntityEvent Subscriber. (#14337)
    * VACMS-13852: Creates EntityEvent Subscriber.
    
    * Tweaks to prevent test failures.
    
    * Yeah, no clue what I'm going to do here.
    
    * Test all event types.
    
    * Adds unit test coverage for LocalFilesystemBuildFileTest.
    
    * Test coverage.
    
    * Fix trait.
    
    * IsFacilityTraitTest.
    
    * Ignore coverage on plugin managers and plugin base classes.
    
    * Unit tests for the OnDemand entity event strategy plugin.
    
    * Make internal logic a little more consistent.
    
    * Disable current Entity Event Subscriber :eyes:
    
    * Adds VerboseFalse entity event strategy plugin.
    
    * Oops, my plugins didn't have a logger :anguished:
    
    * Adds mocks for OnDemand test.
    
    * Tests for VerboseFalse.
    
    * ContentReleaseTriggerTrait::hasTriggeringChanges() did the Bad Thing™
    
    * D'oh.
    
    * Fixes test.
    
    * Fix tests.
    
    * VerboseFalse and tests.
    
    * Specify an exception occurred when getting a node detail threw.
    
    * Further tweaks.
    
    * Ugh, I'm an idiot.
    
    * Updates to VerboseFalse and VerboseFalseTest to support better debugging messages.
    
    * Removes bypass from test.
    
    * 'exception occurred' => 'Not Applicable'

--- docroot/modules/custom/va_gov_content_release/src/EntityEvent/Strategy/Plugin/StrategyPluginBase.php
+++ docroot/modules/custom/va_gov_content_release/src/EntityEvent/Strategy/Plugin/StrategyPluginBase.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\va_gov_content_release\EntityEvent\Strategy\Plugin;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginBase;
 use Drupal\Core\StringTranslation\TranslationInterface;
@@ -20,20 +21,33 @@ abstract class StrategyPluginBase extends PluginBase implements StrategyPluginIn
   use StringTranslationTrait;
 
   /**
+   * The logger service.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
    * {@inheritDoc}
+   *
+   * @codeCoverageIgnore
    */
   public function __construct(
     array $configuration,
     $plugin_id,
     $plugin_definition,
-    TranslationInterface $stringTranslation
+    TranslationInterface $stringTranslation,
+    LoggerChannelFactoryInterface $loggerFactory
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->stringTranslation = $stringTranslation;
+    $this->logger = $loggerFactory->get('va_gov_content_release');
   }
 
   /**
    * {@inheritDoc}
+   *
+   * @codeCoverageIgnore
    */
   public static function create(
     ContainerInterface $container,
@@ -45,7 +59,8 @@ abstract class StrategyPluginBase extends PluginBase implements StrategyPluginIn
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('string_translation')
+      $container->get('string_translation'),
+      $container->get('logger.factory')
     );
   }
 
@@ -53,5 +68,10 @@ abstract class StrategyPluginBase extends PluginBase implements StrategyPluginIn
    * {@inheritDoc}
    */
   abstract public function shouldTriggerContentRelease(VaNodeInterface $node) : bool;
+
+  /**
+   * {@inheritDoc}
+   */
+  abstract public function getReasonMessage(VaNodeInterface $node) : string;
 
 }
---
+++ tests/phpunit/va_gov_content_types/unit/ModuleTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace tests\phpunit\va_gov_content_types\unit\Traits;
+
+use Drupal\Tests\Traits\Core\GeneratePermutationsTrait;
+use Drupal\va_gov_content_types\Entity\HealthCareLocalFacility;
+use Drupal\va_gov_content_types\Entity\VaNode;
+use Tests\Support\Classes\VaGovUnitTestBase;
+
+require __DIR__ . '/../../../../docroot/modules/custom/va_gov_content_types/va_gov_content_types.module';
+
+/**
+ * Unit test of the ContentReleaseTriggerTrait trait.
+ *
+ * @group unit
+ * @group all
+ */
+class ModuleTest extends VaGovUnitTestBase {
+
+  use GeneratePermutationsTrait;
+
+  /**
+   * Test the hasTriggeringChanges() method.
+   *
+   * @param bool $contentType
+   *   The principle content type.
+   * @param bool $bundleClass
+   *   The expected bundle class.
+   *
+   * @covers va_gov_content_types_entity_bundle_info_alter()
+   * @dataProvider entityBundleInfoAlterDataProvider
+   */
+  public function testEntityBundleInfoAlter(string $contentType, string $bundleClass) {
+    $bundles = [
+      'node' => [
+        $contentType => [
+          'class' => $bundleClass,
+        ],
+      ],
+    ];
+    \va_gov_content_types_entity_bundle_info_alter($bundles);
+    $this->assertEquals($bundles['node'][$contentType]['class'], $bundleClass);
+  }
+
+  /**
+   * Data provider for testEntityBundleInfoAlter.
+   *
+   * @return array
+   *   An array of arrays.
+   */
+  public function entityBundleInfoAlterDataProvider() {
+    return [
+      'page' => [
+        'page',
+        VaNode::class,
+      ],
+      'health_care_local_facility' => [
+        'health_care_local_facility',
+        HealthCareLocalFacility::class,
+      ],
+    ];
+  }
+
+}
