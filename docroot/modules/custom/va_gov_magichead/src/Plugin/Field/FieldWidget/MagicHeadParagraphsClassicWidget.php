56a1f5e133508d09f8b4d632c440b83b43e92f5e
Daniel Sasser <daniel.sasser@agile6.com>
2023-08-04 13:48:11 -0700

    VACMS-14290: Adds max_depth to magichead field settings.

--- docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsClassicWidget.php
+++ docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsClassicWidget.php
@@ -21,11 +21,43 @@ use Drupal\entity_reference_hierarchy_paragraphs\Plugin\Field\FieldWidget\Inline
 class MagicHeadParagraphsClassicWidget extends InlineParagraphsHierarchyWidget {
 
   /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    $settings = parent::defaultSettings();
+    return $settings + [
+      'max_depth' => 3,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+    $form['max_depth'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Max Depth'),
+      '#description' => $this->t('The maximum depth of a magichead item.'),
+      '#default_value' => $this->getSetting('max_depth'),
+    ];
+    return $form;
+  }
+
+  /**
    * {@inheritDoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $element = parent::formElement($items, $delta, $element, $form, $form_state);
     $element['#attached']['library'][] = 'va_gov_magichead/magichead_tree_lines';
+    $element['#attached']['library'][] = 'va_gov_magichead/magichead';
+    $name = str_replace("_", "-", "{$this->fieldDefinition->getName()}-values");
+    $settings = [
+      'tablefield_target' => $name,
+      'field_name' => $this->fieldDefinition->getName(),
+      'max_depth' => $this->getSetting('max_depth'),
+    ];
+    $element['#attached']['drupalSettings']['magichead'][] = $settings;
     return $element;
   }
 
