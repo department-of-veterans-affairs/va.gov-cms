ae1f5a1a29f96726f24debb679c65cee5a458eba
Daniel Sasser <daniel.sasser@gmail.com>
2023-08-10 08:20:50 -0700

    VACMS-14290 implement max depth magichead (#14709)
    * VACMS-14290: Fixes typo.
    
    * VACMS-14290: Adds max_depth to magichead field settings.
    
    * VACMS-14290: Stubs new magichead javascript behavior.
    
    * VACMS-14290: Adds missing field settings schema.
    
    * VACMS-14290: Adds a border around table elements that have errors.
    
    * VACMS-14290: Adds max_depth to magichead field.
    
    * VACMS-14290: Exposes the max_depth to tableDrag.
    - Moves widget code to formMultipleElements so the code isn't called for each field item instance.
    - Cleans up javascript and un-wires it from the element. It is only demo code.
    
    * VACMS-14290: Refactor comment for clarity.
    
    * VACMS-14290: Removes unwanted css change.
    
    * VACMS-14290: Adds functional unit test.
    
    ---------
    
    Co-authored-by: Daniel Sasser <daniel.sasser@agile6.com>

--- docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsClassicWidget.php
+++ docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsClassicWidget.php
@@ -21,12 +21,47 @@ use Drupal\entity_reference_hierarchy_paragraphs\Plugin\Field\FieldWidget\Inline
 class MagicHeadParagraphsClassicWidget extends InlineParagraphsHierarchyWidget {
 
   /**
-   * {@inheritDoc}
+   * {@inheritdoc}
    */
-  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    $element = parent::formElement($items, $delta, $element, $form, $form_state);
-    $element['#attached']['library'][] = 'va_gov_magichead/magichead_tree_lines';
-    return $element;
+  public static function defaultSettings() {
+    return [
+      'max_depth' => 3,
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+    $form['max_depth'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Max Depth'),
+      '#description' => $this->t('The maximum depth of a magichead item.'),
+      '#default_value' => $this->getSetting('max_depth'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @see va_gov_magichead_preprocess_field_multiple_value_form()
+   */
+  public function formMultipleElements(FieldItemListInterface $items, array &$form, FormStateInterface $form_state) {
+    $elements = parent::formMultipleElements($items, $form, $form_state);
+
+    // Attach our library.
+    $elements['#attached']['library'][] = 'va_gov_magichead/magichead_tree_lines';
+
+    // Set an arbitrary value for easy targeting in preprocess hook.
+    $elements['#magichead'] = TRUE;
+
+    // Set the max depth on the element. This allows for direct access of max
+    // depth in preprocess hooks.
+    $elements['#magichead_max_depth'] = $this->getSetting('max_depth');
+
+    return $elements;
   }
 
 }
