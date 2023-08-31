2f77ea4cd72d638827fcd817c9fe048642fdd222
Daniel Sasser <daniel.sasser@agile6.com>
2023-08-31 08:33:21 -0700

    This stubs out using the stable paragraphs field widget.
    It is somewhat funcitonal, in that the field widget appears to function as expected.
    
    But, the tabledrag is broken. Nesting does not work, and the depth field is missing the form element.

---
+++ docroot/modules/custom/va_gov_magichead/src/Plugin/Field/FieldWidget/MagicHeadParagraphsWidget.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\va_gov_magichead\Plugin\Field\FieldWidget;
+
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Plugin\Field\FieldWidget\ParagraphsWidget;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+
+/**
+ * Plugin implementation of the 'magichead paragraphs' widget.
+ *
+ * @FieldWidget(
+ *   id = "magichead_paragraphs_stable",
+ *   label = @Translation("Magichead Paragraphs (stable)"),
+ *   description = @Translation("The stable magichead paragraphs inline form widget."),
+ *   field_types = {
+ *     "magichead"
+ *   }
+ * )
+ */
+class MagicHeadParagraphsWidget extends ParagraphsWidget {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @see va_gov_magichead_preprocess_field_multiple_value_form()
+   */
+  public function formMultipleElements(FieldItemListInterface $items, array &$form, FormStateInterface $form_state) {
+    $elements = parent::formMultipleElements($items, $form, $form_state);
+
+    // Attach our libraries.
+    $elements['#attached']['library'][] = 'va_gov_magichead/magichead_tree_lines';
+    $elements['#attached']['library'][] = 'va_gov_magichead/magichead';
+
+    // Set an arbitrary value for easy targeting in preprocess hook.
+    $elements['#magichead'] = TRUE;
+
+    // Set the max depth on the element. This allows for direct access of max
+    // depth in preprocess hooks.
+    $elements['#magichead_max_depth'] = $items->getFieldDefinition()->getSetting('max_depth');
+    $elements['#entity_reference_hierarchy_paragraphs'] = TRUE;
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function errorElement(array $element, ConstraintViolationInterface $error, array $form, FormStateInterface $form_state) {
+    $error_element = NestedArray::getValue($element, $error->arrayPropertyPath);
+    return is_array($error_element) ? $error_element : $element;
+  }
+
+}
