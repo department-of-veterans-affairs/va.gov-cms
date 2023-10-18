6f5ec533ebcf83ce77e2bd434fec7f63df5f51e7
Ari Perez <ari@coa.solutions>
2023-10-18 15:37:36 -0400

    Update cypress-ci-tests.sh
    Update facilities_api.feature

--- tests/scripts/cypress-ci-tests.sh
+++ tests/scripts/cypress-ci-tests.sh
@@ -5,12 +5,12 @@ set -x
 # This runs the Cypress test suites with some additional functionality for CI.
 
 repo_root="$(git rev-parse --show-toplevel)"
-pushd "${repo_root}" > /dev/null
+pushd "${repo_root}" >/dev/null
 
 ./tests/scripts/cypress-tests.sh "${@}"
 exit_code=$?
 node tests/report_cypress_accessibility_errors.js
 
-popd > /dev/null
+popd >/dev/null
 
 exit "${exit_code}"
