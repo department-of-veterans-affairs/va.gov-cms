c29d83fb51a846e6c921870370c4bb9ad3666b38
John Luo <john.luo@agile6.com>
2023-11-08 14:18:16 -0500

    Verify unit tests

--- tests/phpunit/Content/PreventPreviewUrlsAsPathsInLinksValidatorTest.php
+++ tests/phpunit/Content/PreventPreviewUrlsAsPathsInLinksValidatorTest.php
@@ -69,7 +69,7 @@ class PreventPreviewUrlsAsPathsInLinksValidatorTest extends VaGovUnitTestBase {
   public function validateDataProvider() {
     return [
       [
-        TRUE,
+        FALSE,
         'Normal string_long text should not fail validation.',
       ],
       [
