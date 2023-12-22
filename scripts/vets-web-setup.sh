915ac6df64dea731a574acde73ab00b51e7c2bc1
Tanner Heffner <tanner.heffner@agile6.com>
2023-12-22 11:18:31 -0800

    clone into correct new dir

--- scripts/vets-web-setup.sh
+++ scripts/vets-web-setup.sh
@@ -8,7 +8,7 @@ source ~/.bashrc
 # Installs & builds vets-website dependencies for next-build preview.
 #if [ ! -d docroot/vendor/va-gov/vets-website ]; then
 if [ ! -d vets-website ]; then
-  git clone --single-branch --depth 1 https://github.com/department-of-veterans-affairs/vets-website.git docroot/vendor/va-gov/vets-website
+  git clone --single-branch --depth 1 https://github.com/department-of-veterans-affairs/vets-website.git vets-website
 else
   echo "Repo vets-website already cloned."
 fi
