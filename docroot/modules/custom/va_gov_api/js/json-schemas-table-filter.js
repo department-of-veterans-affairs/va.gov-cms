a60b1207e6b1c8179f97337aaef02de9209983c8
Alex Finnarn <alex.finnarn@gmail.com>
2023-12-22 11:48:33 -0500

    convert one event subscriber to another

--- docroot/modules/custom/va_gov_api/js/json-schemas-table-filter.js
+++ docroot/modules/custom/va_gov_api/js/json-schemas-table-filter.js
@@ -1,18 +1,22 @@
 /**
  * Attach a behavior to filter out unwanted schemas for OpenAPI UI viewing.
  */
-document.addEventListener('DOMContentLoaded', function () {
-  // Get all the rows in the table body.
-  const rows = document.querySelectorAll('#block-vagovclaro-content table tbody tr');
+(function ($, Drupal, drupalSettings) {
+  Drupal.behaviors.vaGovJsonSchemasTableFilter = {
+    attach: function (context, settings) {
+      // Get all the rows in the table body.
+      const rows = document.querySelectorAll('#block-vagovclaro-content table tbody tr');
 
-  rows.forEach(function (row) {
-    // Get the first cell (td) of the row, which contains the schema label.
-    const schemaLabel = row.cells[0];
+      rows.forEach(function(row) {
+        // Get the first cell (td) of the row, which contains the schema label.
+        const schemaLabel = row.cells[0];
 
-    // Remove all but 'VA.gov JSON:API'.
-    if (schemaLabel.textContent.trim() !== 'VA.gov JSON:API') {
-      row.remove();
+        // Remove all but 'VA.gov JSON:API'.
+        if (schemaLabel.textContent.trim() !== 'VA.gov JSON:API') {
+          row.remove();
+        }
+      });
     }
-  });
-});
+  }
+})(jQuery, Drupal, drupalSettings);
 
--- docroot/modules/custom/va_gov_api/src/EventSubscriber/AddJsEventSubscriber.php
+++ docroot/modules/custom/va_gov_api/src/EventSubscriber/AddJsEventSubscriber.php
@@ -2,11 +2,10 @@
 
 namespace Drupal\va_gov_api\EventSubscriber;
 
-use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\core_event_dispatcher\Event\Theme\PageAttachmentsEvent;
+use Drupal\core_event_dispatcher\PageHookEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Add JS to the OpenAPI UI selector page so only one schema is displayed.
@@ -14,15 +13,15 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class AddJsEventSubscriber implements EventSubscriberInterface {
 
   /**
-   * The module handler.
+   * The request.
    */
-  private ModuleHandlerInterface $moduleHandler;
+  protected RequestStack $requestStack;
 
   /**
    * Constructor.
    */
-  public function __construct(ModuleHandlerInterface $moduleHandler) {
-    $this->moduleHandler = $moduleHandler;
+  public function __construct(RequestStack $requestStack) {
+    $this->requestStack = $requestStack;
   }
 
   /**
@@ -30,42 +29,17 @@ class AddJsEventSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents(): array {
     return [
-      KernelEvents::RESPONSE => ['onResponse'],
+      PageHookEvents::PAGE_ATTACHMENTS => ['onPageAttachments'],
     ];
   }
 
   /**
-   * Modify response to add JS script.
-   *
-   * @throws \DOMException
+   * Modify response to add JS script to a route.
    */
-  public function onResponse(ResponseEvent $event): void {
-    // Only run this code on the OpenAPI UI selector page.
-    $request = $event->getRequest();
-    $path = $request->getPathInfo();
-    if ($event->getRequestType() == HttpKernelInterface::MAIN_REQUEST && $path === '/admin/config/services/openapi') {
-
-      $script = file_get_contents(
-        DRUPAL_ROOT . '/'
-        . $this->moduleHandler->getModule('va_gov_api')->getPath()
-        . '/js/json-schemas-table-filter.js');
-
-      // Create a DOMDocument object and load the response content into it.
-      $responseContent = $event->getResponse()->getContent();
-      $dom = new \DOMDocument();
-      // Need to suppress errors due to <nav> tags.
-      libxml_use_internal_errors(TRUE);
-      $dom->loadHTML($responseContent);
-      libxml_clear_errors();
-
-      // Add the script to the DOMDocument object before the closing body tag.
-      $dom->getElementsByTagName('body')
-        ->item(0)
-        ->appendChild($dom->createElement('script', $script));
-
-      // Save the DOMDocument object back into the response content.
-      $responseContent = $dom->saveHTML();
-      $event->getResponse()->setContent($responseContent);
+  public function onPageAttachments(PageAttachmentsEvent $event): void {
+    if ($this->requestStack->getCurrentRequest()->get('_route') === 'openapi.downloads') {
+      $attachments = &$event->getAttachments();
+      $attachments['#attached']['library'][] = 'va_gov_api/json_schemas_table_filter';
     }
   }
 
