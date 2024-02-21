5e25a65722db137cf3a1ad12a5dc8c6a4f74b588
Daniel Sasser <daniel.sasser@agile6.com>
2024-02-21 13:38:18 -0800

    VACMS-17116: Rename the display.

--- docroot/modules/custom/va_gov_eca/src/Plugin/views/display/EcaViewsResultDisplay.php
+++ docroot/modules/custom/va_gov_eca/src/Plugin/views/display/EcaViewsResultDisplay.php
@@ -7,14 +7,14 @@ use Drupal\views\Plugin\views\display\DefaultDisplay;
 /**
  * A Views display plugin for ECA.
  *
- * "eca_view_display" is a custom property, used with
+ * "eca_views_display" is a custom property, used with
  * \Drupal\views\Views::getApplicableViews() to retrieve all views with an
- * 'ECA View' display.
+ * 'ECA Result' display.
  *
  * @ingroup views_display_plugins
  *
  * @ViewsDisplay(
- *   id = "va_gov_notifications_eca_view",
+ *   id = "eca_result",
  *   title = @Translation("ECA Result"),
  *   help = @Translation("Views diplay plugin for ECA."),
  *   theme = "views_view",
@@ -23,7 +23,7 @@ use Drupal\views\Plugin\views\display\DefaultDisplay;
  *   eca_views_display = TRUE
  * )
  */
-class EcaViewsResult extends DefaultDisplay {
+class EcaViewsResultDisplay extends DefaultDisplay {
 
   /**
    * {@inheritdoc}
