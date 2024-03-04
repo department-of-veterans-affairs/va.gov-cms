c72d78aea7a3119726bed702c3e1010f46989381
Daniel Sasser <daniel.sasser@agile6.com>
2024-03-04 12:39:35 -0800

    VACMS-16417: Updates QueueMessage to send a test email.

--- docroot/modules/custom/va_gov_eca/src/Plugin/Action/QueueMessage.php
+++ docroot/modules/custom/va_gov_eca/src/Plugin/Action/QueueMessage.php
@@ -4,6 +4,8 @@ namespace Drupal\va_gov_eca\Plugin\Action;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\eca\Plugin\Action\ConfigurableActionBase;
+use Drupal\message\Entity\Message;
+use Drupal\node\Entity\Node;
 
 /**
  * Action to queue an Email Message.
@@ -28,33 +30,35 @@ class QueueMessage extends ConfigurableActionBase {
       '#required' => TRUE,
       '#default_value' => $this->configuration['queue'] ?? 'va_gov_eca_message_worker',
     ];
+    $templates = $this->entityTypeManager->getStorage('message_template')->getQuery()->execute();
     $form['message_template'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t(''),
-      '#description' => $this->t(''),
+      '#type' => 'select',
+      '#title' => $this->t('Message Template'),
+      '#description' => $this->t('Select the Message Template to use for this action. The message template body will become the body of the email message.'),
       '#required' => TRUE,
-      '#default_value' => $this->configuration[''] ?? '',
+      '#default_value' => $this->configuration['message_template'] ?? '',
+      '#options' => $templates,
     ];
     $form['message_template_variables'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t(''),
-      '#description' => $this->t(''),
-      '#required' => TRUE,
-      '#default_value' => $this->configuration[''] ?? '',
+      '#type' => 'textarea',
+      '#title' => $this->t('Message Template Variables'),
+      '#description' => $this->t('Data to map to the template using template variables. One mapping per line, entered as [message-token-name]:[value]'),
+      '#required' => FALSE,
+      '#default_value' => $this->configuration['message_template_variables'] ?? '',
     ];
     $form['recipient'] = [
       '#type' => 'textfield',
-      '#title' => $this->t(''),
-      '#description' => $this->t(''),
+      '#title' => $this->t('Recipient'),
+      '#description' => $this->t('The recipient (to:) of the email message. Tokens allowed.'),
       '#required' => TRUE,
-      '#default_value' => $this->configuration[''] ?? '',
+      '#default_value' => $this->configuration['recipient'] ?? '',
     ];
     $form['subject'] = [
       '#type' => 'textfield',
-      '#title' => $this->t(''),
-      '#description' => $this->t(''),
+      '#title' => $this->t('Subject'),
+      '#description' => $this->t('The email subject. Tokens allowed.'),
       '#required' => TRUE,
-      '#default_value' => $this->configuration[''] ?? '',
+      '#default_value' => $this->configuration['subject'] ?? '',
     ];
     return $form;
   }
@@ -75,7 +79,19 @@ class QueueMessage extends ConfigurableActionBase {
    * {@inheritDoc}
    */
   public function execute() {
-    // TODO: Implement execute() method.
+    $node = Node::load(65970);
+    // TODO: Change to use a queue. We are sending directly for now for testing purposes.
+    $message = Message::create([
+      'template' => $this->configuration['message_template'],
+      'uid' => $node->getOwnerId(),
+    ]);
+    $message->set('field_expiration_date', '1/3/2024');
+    $message->set('field_target_entity', $node->id());
+    $message->save();
+
+    /** @var \Drupal\message_notify\MessageNotifier $notifier */
+    $notifier = \Drupal::service('message_notify.sender');
+    $notifier->send($message);
   }
 
 }
