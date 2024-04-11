5cda55df86833d0fc40af9d7f6fd5defae0806c1
Alex Finnarn <alex.finnarn@gmail.com>
2024-04-11 15:17:15 -0400

    initial commit for deployment status form

---
+++ docroot/modules/custom/va_gov_content_release/src/Form/ContentReleaseStatusForm.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Drupal\va_gov_content_release\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Site\Settings;
+use Drupal\va_gov_build_trigger\Traits\RunsDuringBusinessHours;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a VA.gov Content Release form.
+ */
+class ContentReleaseStatusForm extends FormBase {
+
+  use RunsDuringBusinessHours;
+
+  /**
+   * @var \GuzzleHttp\Client
+   */
+  private ClientInterface $httpClient;
+
+  /**
+   * @var \Drupal\Core\Site\Settings
+   */
+  private Settings $settings;
+
+  public function __construct(Settings $settings, ClientInterface $client) {
+    //    $this->client = new Client();
+    //    $this->client->authenticate($token, null, AuthMethod::ACCESS_TOKEN);
+    $this->settings = $settings;
+    $this->httpClient = $client;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('settings'),
+      $container->get('http_client')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'va_gov_content_release_next_simple';
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function buildForm(
+    array $form,
+    FormStateInterface $form_state
+  ): array {
+    // Check if it is in business hours to release content.
+    RunsDuringBusinessHours:
+
+
+    // Make a call to GitHub using http_client service to check workflow runs.
+    //    $response = $this->httpClient->request('GET', 'https://api.github.com/repos/department-of-veterans-affairs/content-build/actions/workflows');
+    $response = $this->httpClient->request('GET',
+      'https://api.github.com/repos/department-of-veterans-affairs/content-build/actions/workflows/11158212/runs');
+    $data = json_decode($response->getBody()->getContents());
+
+    // Get the latest run.
+    $latest_run = $data->workflow_runs[0];
+
+    // Get start of the run.
+    $start = $latest_run->run_started_at;
+
+    // Calculate the duration of the run so far.
+    $duration = (time() - strtotime($start));
+
+    $form['content_release_status_block'] = [
+      '#theme' => 'status_report_grouped',
+      '#grouped_requirements' => [
+        [
+          'title' => $this->t('Latest Content Release Run'),
+          'type' => 'content-release-status',
+          'items' => [
+            'status' => [
+              'title' => $this->t('Status'),
+              'value' => $latest_run->status,
+            ],
+            'run_start' => [
+              'title' => $this->t('Last Run Start Time'),
+              'value' => date('Y-m-d H:i:s', strtotime($start)),
+            ],
+            'duration' => [
+              'title' => $this->t('Duration'),
+              'value' => gmdate('H:i:s', $duration),
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $foo = 'bar';
+
+    // Add markup for a message before the form fields.
+    $form['message'] = [
+      '#markup' => $this->t('Any content you set to Published will...this will be the intro message part.'),
+    ];
+
+    $form['acknowledgement'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('I understand that all VA content set to Published will go live once the release is finished.'),
+      '#required' => TRUE,
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Release Content'),
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(
+    array &$form,
+    FormStateInterface $form_state
+  ): void {
+    // @todo Validate the form here.
+    // Example:
+    // @code
+    //   if (mb_strlen($form_state->getValue('message')) < 10) {
+    //     $form_state->setErrorByName(
+    //       'message',
+    //       $this->t('Message should be at least 10 characters.'),
+    //     );
+    //   }
+    // @endcode
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(
+    array &$form,
+    FormStateInterface $form_state
+  ): void {
+    $this->messenger()->addStatus($this->t('The message has been sent.'));
+    $form_state->setRedirect('<front>');
+  }
+
+}
