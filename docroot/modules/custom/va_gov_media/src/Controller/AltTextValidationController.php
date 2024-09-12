02271fb9f26f36390107b14480e38b14508539ec
Edmund Dunn <109987005+edmund-dunn@users.noreply.github.com>
2024-09-12 13:36:27 -0700

    VACMS-19177: turned off logging for alt text errors

--- docroot/modules/custom/va_gov_media/src/Controller/AltTextValidationController.php
+++ docroot/modules/custom/va_gov_media/src/Controller/AltTextValidationController.php
@@ -44,20 +44,20 @@ class AltTextValidationController extends ControllerBase {
    * Validate the alt text.
    */
   public function validate(Request $req) {
-    $logger = $this->loggerFactory->get('va_gov_media');
+    // $logger = $this->loggerFactory->get('va_gov_media');
     $value = $req->request->get('value');
     $value_length = MediaEventSubscriber::getLengthOfSubmittedValue($value);
     $res = TRUE;
     if ($value_length > 150) {
-      $logger->error("[CC] Alternative text ({$value}) cannot be longer than 150 characters. {$value_length} characters were submitted.");
+      // $logger->error("[CC] Alternative text ({$value}) cannot be longer than 150 characters. {$value_length} characters were submitted.");
       $res = $this->t('Alternative text cannot be longer than 150 characters.');
     }
     if (preg_match('/\.(jpg|jpeg|png|gif)$/i', $value)) {
-      $logger->error("[FN] Alternative text cannot contain file names. {$value} was submitted.");
+      // $logger->error("[FN] Alternative text cannot contain file names. {$value} was submitted.");
       $res = $this->t('Alternative text cannot contain file names.');
     }
     if (preg_match('/(image|photo|graphic|picture) of/i', $value)) {
-      $logger->error("[RP] Alternative text cannot contain repetitive phrases. {$value} was submitted.");
+      // $logger->error("[RP] Alternative text cannot contain repetitive phrases. {$value} was submitted.");
       $res = $this->t('Alternative text cannot contain phrases like “image of”, “photo of”, “graphic of”, “picture of”, etc.');
     }
     return new JsonResponse($res);
