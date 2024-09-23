bcb8246dc8abc8166ffaecce8bd1fff8882a7311
Christian Burk <omahane@gmail.com>
2024-09-23 11:22:07 -0600

    Updates the deploy, removing the new one

--- docroot/modules/custom/va_gov_vamc/va_gov_vamc.deploy.php
+++ docroot/modules/custom/va_gov_vamc/va_gov_vamc.deploy.php
@@ -2,20 +2,8 @@
 
 /**
  * @file
- * Deploy hooks for va_gov_vamc.
- *
- * This is a NAME.deploy.php file. It contains "deploy" functions. These are
- * one-time functions that run *after* config is imported during a deployment.
- * These are a higher level alternative to hook_update_n and
- * hook_post_update_NAME functions.
- *
- * See https://www.drush.org/latest/deploycommand/#authoring-update-functions
- * for a detailed comparison.
+ * Post deploy file VA Gov VAMC.
  */
-
-declare(strict_types=1);
-
-use Drupal\va_gov_batch\cbo_scripts\MigrateVamcFacilityMentalHealthPhoneFieldToParagraph;
 
 require_once __DIR__ . '/../../../../scripts/content/script-library.php';
 
@@ -28,11 +16,4 @@ function va_gov_vamc_deploy_build_va_police_9001(&$sandbox) {
   script_library_sandbox_init($sandbox, '_va_gov_vamc_get_systems_to_process', [$build_bundle]);
   _va_gov_vamc_create_system_content_pages($sandbox, $build_bundle, 'draft');
   return script_library_sandbox_complete($sandbox, "Created @total {$build_bundle} nodes.");
-}
-
-/**
- * Runs data migration for VAMC Facility Mental health phone field to paragraph.
- */
-function va_gov_vamc_deploy_move_phone_to_paragraph_10001(array &$sandbox): void {
-  \Drupal::classResolver(MigrateVamcFacilityMentalHealthPhoneFieldToParagraph::class)->run($sandbox, 'deploy');
 }
