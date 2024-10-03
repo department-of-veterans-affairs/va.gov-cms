fbfe7cf9eff55373fcbede68c08fc7460217f7ba
Ryan Koch <ryan.koch.0213@gmail.com>
2024-10-03 10:22:21 -0500

    Adds functional tests for controller.

---
+++ tests/phpunit/va_gov_form_builder/functional/Controller/VaGovFormBuilderControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace tests\phpunit\va_gov_form_builder\unit\Controller;
+
+use Drupal\Core\Url;
+use Drupal\va_gov_form_builder\Controller\VaGovFormBuilderController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Tests\Support\Classes\VaGovExistingSiteBase;
+
+/**
+ * Unit tests for the VaGovFormBuilderController class.
+ *
+ * @group unit
+ * @group all
+ *
+ * @coversDefaultClass \Drupal\va_gov_form_builder\Controller\VaGovFormBuilderController
+ */
+class VaGovFormBuilderControllerTest extends VaGovExistingSiteBase {
+
+  /**
+   * The controller instance.
+   *
+   * @var \Drupal\va_gov_form_builder\Controller\VaGovFormBuilderController
+   */
+  protected $controller;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
+
+    $this->drupalLogin($this->createUser(['edit any digital_form content']));
+    $this->controller = new VaGovFormBuilderController(\Drupal::formBuilder());
+  }
+
+  /**
+   * Tests the entry method redirects correctly.
+   */
+  public function testEntryRedirect() {
+    $response = $this->controller->entry();
+
+    $this->assertInstanceOf(RedirectResponse::class, $response);
+    $this->assertStringContainsString(Url::fromRoute('va_gov_form_builder.intro')->toString(), $response->getTargetUrl());
+  }
+
+  /**
+   * Tests the intro method returns an Intro form.
+   */
+  public function testIntro() {
+    $form = $this->controller->intro();
+
+    $this->assertArrayHasKey('#type', $form);
+    $this->assertArrayHasKey('working_with_form_builder_header', $form);
+  }
+
+  /**
+   * Tests the startConversion method returns a StartConversion form.
+   */
+  public function testStartConversion() {
+    $form = $this->controller->startConversion();
+
+    $this->assertArrayHasKey('#type', $form);
+    $this->assertArrayHasKey('start_new_conversion_header', $form);
+  }
+
+}
