d9a6cfbda707ac2d61e016f27b0d0a676b497e79
Ryan Koch <ryan.koch.0213@gmail.com>
2024-12-22 08:53:30 -0600

    VAGOV-TEAM-97922: Adds sidebar placeholder to Form Builder pages.

--- tests/phpunit/va_gov_form_builder/functional/templates/FormBuilderPageTemplateTest.php
+++ tests/phpunit/va_gov_form_builder/functional/templates/FormBuilderPageTemplateTest.php
@@ -51,6 +51,9 @@ class FormBuilderPageTemplateTest extends VaGovExistingSiteBase {
 
     $navbarElement = $this->cssSelect('.form-builder-navbar');
     $this->assertCount(1, $navbarElement);
+
+    $sidebarElement = $this->cssSelect('.form-builder-sidebar');
+    $this->assertCount(1, $sidebarElement);
   }
 
   /**
