23377e92e9e03d85b30a36a7ef84c36faa0094c1
Ryan Koch <ryan.koch.0213@gmail.com>
2025-01-28 11:03:12 -0600

    Adds getDigitalFormStepStatus to DigitalFormsService.

--- docroot/modules/custom/va_gov_form_builder/src/Service/DigitalFormsService.php
+++ docroot/modules/custom/va_gov_form_builder/src/Service/DigitalFormsService.php
@@ -103,4 +103,58 @@ class DigitalFormsService {
     return FALSE;
   }
 
+  /**
+   * Returns the status of a step on the Digital Form node.
+   *
+   * Completeness of the step varies by step, and is documented
+   * in the function body.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The Digital Form node.
+   * @param string $stepName
+   *   The step name of the step in question.
+   *
+   * @return 'complete'|'incomplete'
+   *   Returns 'complete' if step is complete.
+   *   Returns 'incomplete if step is incomplete
+   *   or if the node does not exist.
+   */
+  public function getDigitalFormStepStatus($node, $stepName) {
+    if (empty($node)) {
+      return 'incomplete';
+    }
+
+    if ($stepName === 'form_info') {
+      // If the node exists, this will necessarily be complete.
+      return 'complete';
+    }
+
+    if ($stepName === 'review_and_sign') {
+      // This is added automatically by the Forms Library.
+      return 'complete';
+    }
+
+    if (in_array($stepName, [
+      'intro',
+      'confirmation',
+    ])) {
+      // These haven't been handled yet.
+      // Return 'incomplete' for the time being.
+      return 'incomplete';
+    }
+
+    // Standard steps are complete if a corresponding chapter exists.
+    $standardSteps = [
+      'your_personal_info' => 'digital_form_your_personal_info',
+      'address_info' => 'digital_form_address',
+      'contact_info' => 'digital_form_phone_and_email',
+    ];
+    if (array_key_exists($stepName, $standardSteps)) {
+      $paragraphName = $standardSteps[$stepName];
+      return $this->digitalFormHasChapterOfType($node, $paragraphName)
+        ? 'complete'
+        : 'incomplete';
+    }
+  }
+
 }
