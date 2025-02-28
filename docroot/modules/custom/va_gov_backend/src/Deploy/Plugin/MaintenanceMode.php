b75385adba0a0c4e29d8b1692ec518252c3ccc78
Edmund Dunn <109987005+edmund-dunn@users.noreply.github.com>
2025-02-28 07:59:50 -0800

    VACMS-20672: fix deploy shield, added response

--- docroot/modules/custom/va_gov_backend/src/Deploy/Plugin/MaintenanceMode.php
+++ docroot/modules/custom/va_gov_backend/src/Deploy/Plugin/MaintenanceMode.php
@@ -4,6 +4,7 @@ namespace Drupal\va_gov_backend\Deploy\Plugin;
 
 use Drupal\va_gov_backend\Deploy\LoadMaintenanceFileTrait;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 
 /**
@@ -29,9 +30,13 @@ class MaintenanceMode implements DeployPluginInterface {
     $expected_update_duration = '10';
     $html = preg_replace('/\{\{\s*update_duration\s*\}\}/', $expected_update_duration, $html);
     $headers = [
-      'Content-Type' => 'text/html',
+        'Content-Type' => 'text/html',
+        'Retry-After' => 120,
     ];
-    throw new ServiceUnavailableHttpException(120, $html, NULL, 503, $headers);
+    $response = new Response($html, 503, $headers);
+    $response->send();
+    // We need to throw an exception to stop the request.
+    throw new ServiceUnavailableHttpException();
   }
 
 }
