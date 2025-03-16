dc8d685d883262d4e3818a4951ef7b83dfcdacfc
Ryan Koch <ryan.koch.0213@gmail.com>
2025-03-16 09:30:03 -0500

    Updates form-template naming to remove verbosity.

--- tests/phpunit/va_gov_form_builder/unit/ModuleTest.php
+++ tests/phpunit/va_gov_form_builder/unit/ModuleTest.php
@@ -211,8 +211,16 @@ class ModuleTest extends VaGovUnitTestBase {
     $form_themes = ['form_info'];
     foreach ($form_themes as $form_theme) {
       $this->assertArrayHasKey(self::FORM_THEME_PREFIX . $form_theme, $result);
+
+      $this->assertArrayHasKey('path', $result[self::FORM_THEME_PREFIX . $form_theme]);
       $this->assertEquals(self::FORM_TEMPLATE_PATH, $result[self::FORM_THEME_PREFIX . $form_theme]['path']);
+
+      $this->assertArrayHasKey('render element', $result[self::FORM_THEME_PREFIX . $form_theme]);
       $this->assertEquals('form', $result[self::FORM_THEME_PREFIX . $form_theme]['render element']);
+
+      $this->assertArrayHasKey('template', $result[self::FORM_THEME_PREFIX . $form_theme]);
+      $kebab_case_form_theme = str_replace('_', '-', $form_theme);
+      $this->assertEquals($kebab_case_form_theme, $result[self::FORM_THEME_PREFIX . $form_theme]['template']);
     }
   }
 
