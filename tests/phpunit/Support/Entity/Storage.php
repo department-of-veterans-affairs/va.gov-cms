38907bc0efb293afaa16b68bdc1fb8d6738b3c3e
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-17 14:00:12 -0700

    VACMS-20794: Migrate news.va.gov Articles to News Spotlight Blocks (#20872)
    * VACMS-20794: Initial homepage spotlight block migration.
    
    * VACMS-20794: Adds news spotlight image migration.
    
    * VACMS-20794: Add update hook to prepare directory for news spotlight images.
    
    * VACMS-20794: Adds news spotlight images migration to config.
    
    * VACMS-20794: Updates migration adding media and wiring all three migrations together as dependencies.
    
    * VACMS-20794: Adds homepage group.
    
    * VACMS-20794: Minor migration config updates.
    
    * VACMS-20794: Sync migration configs to va_gov_migrate module.
    
    * VACMS-20794: Adds test for news spotlight migration.
    
    * VACMS-20794: Removes unnecessary field from media migration.
    
    * VACMS-20794: Updates news spotlight migration to run all dependencies. Also updates Storage helper class to allow for empty bundle keys in order to load/delete entities that do not have a bundle, such as file entities.

--- tests/phpunit/Support/Entity/Storage.php
+++ tests/phpunit/Support/Entity/Storage.php
@@ -18,9 +18,11 @@ class Storage {
    *   The entity bundle.
    * @param array $conditions
    *   An associative array of field conditions.
+   * @param string $bundle_key
+   *   The entity bundle key.
    */
-  public static function deleteMatchingEntities(string $entity_type, string $bundle, array $conditions = []) : void {
-    $result = self::getMatchingEntities($entity_type, $bundle, $conditions);
+  public static function deleteMatchingEntities(string $entity_type, string $bundle, array $conditions = [], string $bundle_key = 'type') : void {
+    $result = self::getMatchingEntities($entity_type, $bundle, $conditions, $bundle_key);
     $storage = self::getEntityStorage($entity_type);
     $entities = $storage->loadMultiple($result);
     $storage->delete($entities);
@@ -32,15 +34,18 @@ class Storage {
    * @param string $entity_type
    *   The entity type.
    * @param string $bundle
-   *   The entity bundle.
+   *   The entity bundle, defaults to empty string. Some entities do not have
+   *   bundles.
    * @param array $conditions
    *   An associative array of field conditions.
+   * @param string $bundle_key
+   *   The entity bundle key.
    *
    * @return int
    *   The count of matching entities.
    */
-  public static function getMatchingEntityCount(string $entity_type, string $bundle, array $conditions = []) : int {
-    return count(self::getMatchingEntities($entity_type, $bundle, $conditions));
+  public static function getMatchingEntityCount(string $entity_type, string $bundle = '', array $conditions = [], string $bundle_key = 'type') : int {
+    return count(self::getMatchingEntities($entity_type, $bundle, $conditions, $bundle_key));
   }
 
   /**
@@ -49,16 +54,22 @@ class Storage {
    * @param string $entity_type
    *   The entity type.
    * @param string $bundle
-   *   The entity bundle.
+   *   The entity bundle, defaults to empty string. Some entities do not have
+   *   bundles.
    * @param array $conditions
    *   An associative array of field conditions.
+   * @param string $bundle_key
+   *   The entity bundle key.
    *
    * @return array
    *   The matching entities.
    */
-  public static function getMatchingEntities(string $entity_type, string $bundle, array $conditions = []) : array {
+  public static function getMatchingEntities(string $entity_type, string $bundle = '', array $conditions = [], string $bundle_key = 'type') : array {
     $storage = self::getEntityStorage($entity_type);
-    $query = $storage->getQuery()->condition('type', $bundle);
+    $query = $storage->getQuery();
+    if (!empty($bundle)) {
+      $query->condition($bundle_key, $bundle);
+    }
 
     foreach ($conditions as $key => $value) {
       $query->condition($key, $value);
