8846e8d0c1e3a55a0f7409fd6c00802325ee5884
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-28 16:17:46 -0700

    VACMS-14752: Styling adjustments.

--- docroot/modules/custom/va_gov_backend/src/Plugin/Field/FieldWidget/CheckboxMixedWidget.php
+++ docroot/modules/custom/va_gov_backend/src/Plugin/Field/FieldWidget/CheckboxMixedWidget.php
@@ -4,7 +4,7 @@ namespace Drupal\va_gov_backend\Plugin\Field\FieldWidget;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\options\Plugin\Field\FieldWidget\OptionsButtonsWidget;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsButtonsWidget;
 
 /**
  * Plugin implementation of the 'checkbox_mixed' widget.
@@ -28,9 +28,8 @@ class CheckboxMixedWidget extends OptionsButtonsWidget {
   /**
    * {@inheritdoc}
    */
-  public static function defaultSettings() {
+  public static function defaultSettings(): array {
     return [
-      'select_all' => FALSE,
       'select_all_text' => t('Select all'),
     ] + parent::defaultSettings();
   }
@@ -38,39 +37,22 @@ class CheckboxMixedWidget extends OptionsButtonsWidget {
   /**
    * {@inheritdoc}
    */
-  public function settingsForm(array $form, FormStateInterface $form_state) {
+  public function settingsForm(array $form, FormStateInterface $form_state): array {
     $element = parent::settingsForm($form, $form_state);
-
-    $element['select_all'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t("Include 'select all' option"),
-      '#default_value' => $this->getSetting('select_all'),
-    ];
-
     $element['select_all_text'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Select all text'),
       '#default_value' => $this->getSetting('select_all_text'),
-      '#states' => [
-        'visible' => [
-          ':input[name="fields[' . $this->fieldDefinition->getName() . '][settings_edit_form][settings][select_all]"]' => ['checked' => TRUE],
-        ],
-      ],
     ];
-
     return $element;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function settingsSummary() {
+  public function settingsSummary(): array {
     $summary = parent::settingsSummary();
-
-    if ($this->getSetting('select_all')) {
-      $summary[] = $this->t('Select all option enabled with text: @text', ['@text' => $this->getSetting('select_all_text')]);
-    }
-
+    $summary[] = $this->t('Select all text: @text', ['@text' => $this->getSetting('select_all_text')]);
     return $summary;
   }
 
@@ -78,32 +60,45 @@ class CheckboxMixedWidget extends OptionsButtonsWidget {
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    $parentElement = parent::formElement($items, $delta, $element, $form, $form_state);
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+    $element['#attached']['library'][] = 'va_gov_backend/checkbox_mixed';
+    $element['#theme_wrappers'] = [
+      'container' => [
+        '#attributes' => ['class' => ['checkbox-mixed-wrapper']],
+        '#title_display' => 'invisible',
+      ],
+    ];
+    $element['checkbox_mixed'] = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['checkbox-mixed']],
+      '#weight' => 0,
+    ];
 
-    if ($this->fieldDefinition->getFieldStorageDefinition()->isMultiple() && $this->getSetting('select_all')) {
-      $element['#attached']['library'][] = 'va_gov_backend/checkbox_mixed';
-
-      $element['checkbox_mixed'] = [
-        '#type' => 'container',
-        '#attributes' => ['class' => ['checkbox-mixed']],
-        '#weight' => 0,
-      ];
-
-      $element['checkbox_mixed']['checkbox_mixed_checkbox'] = [
-        '#type' => 'html_tag',
-        '#tag' => 'div',
-        '#value' => $this->getSetting('select_all_text'),
-        '#attributes' => [
-          'class' => [
-            'checkbox-mixed-checkbox',
-          ],
-          'role' => 'checkbox',
-          'aria-checked' => 'mixed',
-          'tabindex' => '0',
+//    $element['checkbox_mixed']['checkbox_mixed_checkbox'] = [
+//      '#type' => 'html_tag',
+//      '#tag' => 'div',
+//      '#value' => $this->getSetting('select_all_text'),
+//      '#attributes' => [
+//        'class' => [
+//          'checkbox-mixed-checkbox',
+//        ],
+//        'role' => 'checkbox',
+//        'aria-checked' => 'mixed',
+//        'tabindex' => '0',
+//      ],
+//    ];
+    $element['checkbox_mixed']['checkbox_mixed_checkbox'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->getSetting('select_all_text'),
+      '#attributes' => [
+        'class' => [
+          'checkbox-mixed-checkbox',
         ],
-      ];
-      $element['checkbox_mixed']['children'] = $parentElement;
-    }
+        'role' => 'checkbox',
+        'aria-checked' => 'mixed',
+        'tabindex' => '0',
+      ],
+    ];
 
     return $element;
   }
