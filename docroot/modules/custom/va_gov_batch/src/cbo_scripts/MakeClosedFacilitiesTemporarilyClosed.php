9eb4206a1e0123650387741538b35e0f12833199
Christian Burk <omahane@gmail.com>
2025-03-31 14:11:50 -0500

    VACMS-20738: Removes closed

---
+++ docroot/modules/custom/va_gov_batch/src/cbo_scripts/MakeClosedFacilitiesTemporarilyClosed.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\va_gov_batch\cbo_scripts;
+
+use Drupal\codit_batch_operations\BatchOperations;
+use Drupal\codit_batch_operations\BatchScriptInterface;
+
+/**
+ * @file
+ * For non-numerical characters in the phone_number paragraph extension field.
+ *
+ * For VACMS-20738.
+ * drush codit-batch-operations:run MakeClosedFacilitiesTemporarilyClosed .
+ */
+/**
+ * Makes Closed facilities have Temporary Closure status.
+ */
+class MakeClosedFacilitiesTemporarilyClosed extends BatchOperations implements BatchScriptInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle(): string {
+    return <<<TITLE
+    For:
+      - VACMS-20738: https://github.com/department-of-veterans-affairs/va.gov-cms/issues/20738.
+    TITLE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription():string {
+    return <<<ENDHERE
+    Updates facilities with closed status to have Temporary Closure status.
+    ENDHERE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCompletedMessage(): string {
+    return 'Facilities with "Closed" operating status have been updated with @total processed and @completed completed.';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function gatherItemsToProcess(): array {
+    return \Drupal::database()
+      ->select('node__field_operating_status_facility', 'nfosf')
+      ->fields('nfosf', ['entity_id'])
+      ->condition('field_operating_status_facility_value', 'closed', '=')
+      ->execute()
+      ->fetchCol();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processOne(string $key, mixed $item, array &$sandbox): string {
+    try {
+      /** @var \Drupal\node\NodeStorageInterface $nodeStorage */
+      $node_revisions = $this->getNodeDefaultAndForwardRevisions($item);
+      foreach ($node_revisions as $revision) {
+        $revision->set('field_operating_status_facility', 'temporary_closure');
+        $message = "Updated from Closed to Temporary Closure via script.";
+        $this->saveNodeRevision($revision, $message);
+      }
+      return "Updated node $item with Temporary Closure status.";
+    }
+    catch (\Exception $e) {
+      $message = "Exception during update of node $item: $e";
+      return $message;
+    }
+  }
+
+}
