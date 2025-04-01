a98e85145d12cdc29a008ce650560bb282d1ece3
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-03-31 23:09:19 -0700

    VACMS-14752: Fixes bug where the select all checkbox was being used as a field value.

--- docroot/modules/custom/va_gov_backend/src/Plugin/Field/FieldWidget/CheckboxMixedWidget.php
+++ docroot/modules/custom/va_gov_backend/src/Plugin/Field/FieldWidget/CheckboxMixedWidget.php
@@ -85,6 +85,8 @@ class CheckboxMixedWidget extends OptionsButtonsWidget {
         'aria-checked' => 'mixed',
         'tabindex' => '0',
       ],
+      // Prevent this element from being considered a field value.
+      '#input' => FALSE,
     ];
 
     return $element;
