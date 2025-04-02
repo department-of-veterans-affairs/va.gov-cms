6c28142748e975914e2f0b0b3e399b34e9f74795
Ryan Koch <ryan.koch.0213@gmail.com>
2025-04-02 09:53:00 -0500

    On step-style form submit, redirect to the new step-layout page.

--- docroot/modules/custom/va_gov_form_builder/src/Form/StepStyle.php
+++ docroot/modules/custom/va_gov_form_builder/src/Form/StepStyle.php
@@ -191,8 +191,9 @@ class StepStyle extends FormBuilderStepBase {
     // Clear the session variable after successful paragraph save.
     $this->session->set('form_builder:add_step:step_label', NULL);
 
-    $form_state->setRedirect('va_gov_form_builder.layout', [
+    $form_state->setRedirect('va_gov_form_builder.step.layout', [
       'nid' => $this->digitalForm->id(),
+      'stepParagraphId' => $this->stepParagraph->id(),
     ]);
   }
 
