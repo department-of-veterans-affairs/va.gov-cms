0fa55d877bb533899df578999ffa11a2dba90611
Ryan Koch <ryan.koch.0213@gmail.com>
2025-04-12 21:46:22 -0500

    - Adds custom ExpandedRadios and ExpandedRadio elements for rendering a radio input with arbitrary html content. - Utilizes the new custom element to render the three options on the ResponseKind form. - Adds a custom template for rendering expanded radios with helpt text and an optional image.

--- docroot/modules/custom/va_gov_form_builder/src/Form/ResponseKind.php
+++ docroot/modules/custom/va_gov_form_builder/src/Form/ResponseKind.php
@@ -25,44 +25,36 @@ class ResponseKind extends FormBuilderBase {
 
     $imageDir = '/modules/custom/va_gov_form_builder/images/';
     $form['response_kind'] = [
-      '#type' => 'radios',
+      '#type' => 'va_gov_form_builder__expanded_radios',
       '#title' => $this->t('What kind of response will the submitter provide?'),
       '#options' => [
         'choice' => $this->t('Choice'),
         'date' => $this->t('Date'),
         'text' => $this->t('Text and/or numbers'),
       ],
-      '#option_attributes' => [
+      '#options_expanded_content' => [
         'choice' => [
-          'class' => ['va-gov-form-builder__response-kind-choice'],
-        ],
-        'date' => [
-          'class' => ['va-gov-form-builder__response-kind-date'],
-        ],
-        'text' => [
-          'class' => ['va-gov-form-builder__response-kind-text'],
-        ],
-      ],
-      '#options_data' => [
-        'choice' => [
-          'help_text' => $this->t('This allows one or multiple items to be selected from a provided list.'),
-          'image' => [
+          '#theme' => 'page_element__va_gov_form_builder__expanded_radio__help_text_optional_image',
+          '#help_text' => $this->t('This allows one or multiple items to be selected from a provided list.'),
+          '#image' => [
             'alt_text' => 'Example of a list of choices',
             'caption' => 'Example of a list of choices',
             'url' => $imageDir . 'response-kind-choice.png',
           ],
         ],
         'date' => [
-          'help_text' => $this->t('This allows a specific or approximate date or date range to be input.'),
-          'image' => [
+          '#theme' => 'page_element__va_gov_form_builder__expanded_radio__help_text_optional_image',
+          '#help_text' => $this->t('This allows a specific or approximate date or date range to be input.'),
+          '#image' => [
             'alt_text' => 'Example of a date range',
             'caption' => 'Example of a date range',
             'url' => $imageDir . 'response-kind-date.png',
           ],
         ],
         'text' => [
-          'help_text' => $this->t('This allows the submitter to fill in the blank for the question you are asking.'),
-          'image' => [
+          '#theme' => 'page_element__va_gov_form_builder__expanded_radio__help_text_optional_image',
+          '#help_text' => $this->t('This allows the submitter to fill in the blank for the question you are asking.'),
+          '#image' => [
             'alt_text' => 'Example of input fields',
             'caption' => 'Example of input fields',
             'url' => $imageDir . 'response-kind-text.png',
