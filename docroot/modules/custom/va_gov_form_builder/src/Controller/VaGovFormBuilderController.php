7310e998fc453c4a4f95aea3a17a81e1158b5899
Ryan Koch <6863534+ryguyk@users.noreply.github.com>
2025-05-02 15:28:12 -0500

    VAGOV-TEAM-106485: Text-Area Response (#21278)
    * Adds routes and controller method for text-area pages.
    
    * Adds custom form for text-area-response page.
    
    * Adds theme and template for text-area-response page
    
    * Adds text-area preview image.
    
    * - Updates controller logic for navigating from text-area page title to text-area response.
    - Updates controller logic to handle edit mode of text-area-response page.
    
    * Updates logic for breadcrumb and page-url generation (won't fully work until we get rest of pages in).
    
    * Updates value of $pageComponentType passed to parent class from text-area class.

--- docroot/modules/custom/va_gov_form_builder/src/Controller/VaGovFormBuilderController.php
+++ docroot/modules/custom/va_gov_form_builder/src/Controller/VaGovFormBuilderController.php
@@ -327,6 +327,7 @@ class VaGovFormBuilderController extends ControllerBase {
       'step.question.custom.date.single_date.page_title',
       'step.question.custom.date.date_range.page_title',
       'step.question.custom.text.text_input.page_title',
+      'step.question.custom.text.text_area.page_title',
     ];
     if (in_array($page, $stepPages)) {
       if (!$this->digitalForm) {
@@ -542,6 +543,19 @@ class VaGovFormBuilderController extends ControllerBase {
       }
 
       $pageTitleUrl = $this->getPageUrl('step.question.custom.text.text_input.page_title');
+      $breadcrumbTrail = $this->generateBreadcrumbs(
+        'step.question.custom.text.type',
+        '',
+        $pageTitleUrl
+      );
+    }
+
+    elseif ($parent === 'step.question.custom.text.text_area.page_title') {
+      if (!$this->digitalForm || !$this->stepParagraph) {
+        return [];
+      }
+
+      $pageTitleUrl = $this->getPageUrl('step.question.custom.text.text_area.page_title');
       $breadcrumbTrail = $this->generateBreadcrumbs(
         'step.question.custom.text.type',
         '',
@@ -1329,6 +1343,10 @@ class VaGovFormBuilderController extends ControllerBase {
           $breadcrumbs = $this->generateBreadcrumbs('step.question.custom.text.type', 'Text-input question');
           break;
 
+        case CustomSingleQuestionPageType::TextArea:
+          $breadcrumbs = $this->generateBreadcrumbs('step.question.custom.text.type', 'Text-area question');
+          break;
+
         default:
           throw new NotFoundHttpException();
       }
@@ -1501,7 +1519,7 @@ class VaGovFormBuilderController extends ControllerBase {
   }
 
   /**
-   * Custom single-question date-range response page.
+   * Custom single-question text-input response page.
    *
    * @param string $nid
    *   The node id of the Digital Form.
@@ -1568,6 +1586,73 @@ class VaGovFormBuilderController extends ControllerBase {
   }
 
   /**
+   * Custom single-question text-area response page.
+   *
+   * @param string $nid
+   *   The node id of the Digital Form.
+   * @param string $stepParagraphId
+   *   The entity id of the step paragraph.
+   * @param string|null $pageParagraphId
+   *   The entity id of the page paragraph.
+   */
+  public function customSingleQuestionTextAreaResponse($nid, $stepParagraphId, $pageParagraphId = NULL) {
+    if (empty($this->digitalForm)) {
+      $this->loadDigitalForm($nid);
+    }
+    if (empty($this->stepParagraph)) {
+      $this->loadStepParagraph($stepParagraphId);
+    }
+
+    if (!empty($pageParagraphId)) {
+      // This is a page edit.
+      if (empty($this->pageParagraph)) {
+        $this->loadPageParagraph($pageParagraphId);
+      }
+
+      $breadcrumbs = $this->generateBreadcrumbs('step.question.page_title', 'Text-area response');
+    }
+    else {
+      // This is the second stage in the process
+      // of creating a new page. The previously
+      // entered page title and body should be in
+      // session storage. The page title is required.
+      // If it is not there, we should redirect back
+      // to the page-title page.
+      $sessionData = $this->session->get(FormBuilderPageBase::SESSION_KEY);
+      $pageTitle = $sessionData['title'] ?? NULL;
+      if (!$pageTitle) {
+        return $this->redirect(
+          'va_gov_form_builder.step.question.custom.text.text_area.page_title',
+          [
+            'nid' => $nid,
+            'stepParagraphId' => $stepParagraphId,
+          ],
+        );
+      }
+
+      // This is page creation.
+      $breadcrumbs = $this->generateBreadcrumbs(
+        'step.question.custom.text.text_area.page_title',
+        'Text-area response'
+      );
+    }
+
+    // Override the page title with "Date question".
+    $breadcrumbs[count($breadcrumbs) - 2]['label'] = 'Text-area question';
+
+    $formName = 'CustomSingleQuestionTextAreaResponse';
+    $subtitle = $this->digitalForm->getTitle();
+    $libraries = [
+      'two_column_with_buttons',
+      'expanded_radio',
+      'custom_single_question_response',
+      'repeatable_field_groups',
+    ];
+
+    return $this->getFormPage($formName, $subtitle, $breadcrumbs, $libraries);
+  }
+
+  /**
    * Custom single-question response page.
    *
    * This is a catch-all method for handling
@@ -1610,6 +1695,9 @@ class VaGovFormBuilderController extends ControllerBase {
 
       case CustomSingleQuestionPageType::TextInput:
         return $this->customSingleQuestionTextInputResponse($nid, $stepParagraphId, $pageParagraphId);
+
+      case CustomSingleQuestionPageType::TextArea:
+        return $this->customSingleQuestionTextAreaResponse($nid, $stepParagraphId, $pageParagraphId);
     }
   }
 
