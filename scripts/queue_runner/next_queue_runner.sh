9b2c3401cdff8b1fe38467dda13cc0dad8192c9d
Tim Cosgrove <tcosgrove@metrostar.com>
2025-06-17 18:59:23 -0700

    Remove unneeded root calculation.

--- scripts/queue_runner/next_queue_runner.sh
+++ scripts/queue_runner/next_queue_runner.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -l
 
-ROOT=${TUGBOAT_ROOT:-${DDEV_APPROOT:-/var/www/html}}
-cd "${ROOT}"
+cd "${TUGBOAT_ROOT}"
 [ -f "./docroot/sites/default/files/next-buildrequest.txt" ] && ./scripts/next-build-frontend.sh
 sleep 10s
