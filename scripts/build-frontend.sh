58a1e7cbe4de4fef64aa8e9f5122e805e879c200
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2025-09-24 17:24:13 -0700

    Ensure the directories for vets-website files exist prior to attempting to copy them over. (#22331)
    * Ensure the directories for vets-website files exist prior to attempting to copy them over.
    
    * Get back out of the vets-website directory when finished.

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -128,10 +128,15 @@ find \
   -exec sed -i "s#${assets_base_url}##g" {} \+;
 echo "Copy built files to content-build." >> ${logfile}
 shopt -s nullglob
+# Ensure the destination directories exist.
+mkdir -p /var/lib/tugboat/docroot/vendor/va-gov/content-build/build/vagovdev/generated/
+mkdir -p /var/lib/tugboat/docroot/vendor/va-gov/content-build/build/vagovdev/fonts/
+# Copy the built files.
 cp -rf build/vagovdev/generated/* /var/lib/tugboat/docroot/vendor/va-gov/content-build/build/vagovdev/generated/ &>> ${logfile}
 cp -rf build/vagovdev/generated/*.ttf /var/lib/tugboat/docroot/vendor/va-gov/content-build/build/vagovdev/fonts/ &>> ${logfile}
 cp -rf build/vagovdev/generated/*.woff2 /var/lib/tugboat/docroot/vendor/va-gov/content-build/build/vagovdev/fonts/ &>> ${logfile}
 shopt -u nullglob
+popd
 echo "Finished installing and building vets-website" >> ${logfile}
 
 
