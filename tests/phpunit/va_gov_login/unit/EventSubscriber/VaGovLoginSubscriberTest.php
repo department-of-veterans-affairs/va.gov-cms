b4352d7677bfd6e7956fb84f87294adfc32e42e8
Brian Seek <brian.seek@civicactions.com>
2025-11-17 15:04:10 -0800

    added new event subscriber and test to redirect anon to login on 403

---
+++ tests/phpunit/va_gov_login/unit/EventSubscriber/VaGovLoginSubscriberTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\Tests\va_gov_login\Unit\EventSubscriber;
+
+use Drupal\Core\Session\AccountInterface;
+use Drupal\va_gov_login\EventSubscriber\VaGovLoginSubscriber;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * Unit test for the va_gov_login event subscriber.
+ *
+ * @group unit
+ * @group all
+ *
+ * @coversDefaultClass \Drupal\va_gov_login\EventSubscriber\VaGovLoginSubscriber
+ */
+class VaGovLoginSubscriberTest extends TestCase {
+
+  /**
+   * Make sure anonymous user is redirected on 403.
+   */
+  public function testOnExceptionRedirectsAnonymousUser() {
+    // Mock AccountInterface to simulate anonymous user.
+    $account = $this->createMock(AccountInterface::class);
+    $account->method('isAnonymous')->willReturn(TRUE);
+
+    $subscriber = new VaGovLoginSubscriber($account);
+
+    // Mock ExceptionEvent.
+    $kernel = $this->createMock(HttpKernelInterface::class);
+    $request = new Request();
+    $exception = new \Exception('Forbidden');
+    $event = new ExceptionEvent($kernel, $request, HttpKernelInterface::MAIN_REQUEST, $exception);
+
+    // Call the method.
+    $subscriber->onException($event);
+
+    // Assert that the response is a redirect to <front>.
+    $response = $event->getResponse();
+    $this->assertInstanceOf(Response::class, $response);
+    $this->assertTrue($response->isRedirect());
+    $this->assertStringContainsString('/', $response->headers->get('Location'));
+  }
+
+  /**
+   * Make sure authenticated user is not redirected on 403.
+   */
+  public function testOnExceptionDoesNotRedirectAuthenticatedUser() {
+    // Mock AccountInterface to simulate authenticated user.
+    $account = $this->createMock(AccountInterface::class);
+    $account->method('isAnonymous')->willReturn(FALSE);
+
+    $subscriber = new VaGovLoginSubscriber($account);
+
+    // Mock ExceptionEvent.
+    $kernel = $this->createMock(HttpKernelInterface::class);
+    $request = new Request();
+    $exception = new \Exception('Forbidden');
+    $event = new ExceptionEvent($kernel, $request, HttpKernelInterface::MAIN_REQUEST, $exception);
+
+    // Call the method.
+    $subscriber->onException($event);
+
+    // Assert that the response is still null (no redirect).
+    $this->assertNull($event->getResponse());
+  }
+
+}
