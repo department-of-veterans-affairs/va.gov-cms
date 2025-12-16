d4afb972521f3f1362e4bd0f87d24de4b16157b9
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-12-15 16:44:22 -0800

    Create a custom test flow per Brian's recommendation to avoid hard-coding ids

--- tests/cypress/integration/features/api/vamc_system_medical_records_office.feature
+++ tests/cypress/integration/features/api/vamc_system_medical_records_office.feature
@@ -1,17 +1,5 @@
 Feature: API
 
-  Scenario: JSON API consumer can access route translation for a given vamc_system_medical_records_offi
+  Scenario: JSON API consumer can fetch all related data for a given vamc_system_medical_records_office path
     Given I am an anonymous user
-    And I should receive status code 200 when I request "/router/translate-path?path=%2Fboston-health-care%2Fmedical-records-office%2F"
-  
-  Scenario: JSON API consumer can access vamc_system_medical_records_offi nodes
-    Given I am an anonymous user
-    And I should receive status code 200 when I request "/jsonapi/node/vamc_system_medical_records_offi?page[limit]=1&filter[status]=1&include=field_office"
-  
-  Scenario: JSON API consumer can access health_care_local_facility nodes to get facility ids for services
-    Given I am an anonymous user
-    And I should receive status code 200 when I request "/jsonapi/node/health_care_local_facility?page[limit]=1&filter[status]=1"
-  
-  Scenario: JSON API consumer can access vha_facility_nonclinical_service nodes by list of facility ids
-    Given I am an anonymous user
-    And I should receive status code 200 when I request "/jsonapi/node/vha_facility_nonclinical_service?page[limit]=1&filter[status]=1&filter[field_service_name_and_descripti.name]=Medical records&filter[field_facility_location.id][condition][path]=field_facility_location.id&filter[field_facility_location.id][condition][value][0]=e452dbe8-9c73-4bed-b9ea-06f9420db982&filter[field_facility_location.id][condition][value][1]=9759def1-4e36-4ec5-b741-6cc8b40884c5&filter[field_facility_location.id][condition][value][2]=61cc9edd-7155-4882-99ae-128ae0984df6&filter[field_facility_location.id][condition][value][3]=cb577a82-953f-4306-84c6-a65abf1b45ea&filter[field_facility_location.id][condition][value][4]=9bfd9427-3c57-4713-9b89-71b0d95ddada&filter[field_facility_location.id][condition][value][5]=f4f16d9e-9fe4-4be4-8492-48b3f6519f89&filter[field_facility_location.id][condition][value][6]=7bf97522-875d-4524-a208-6f4ba19a81aa&filter[field_facility_location.id][condition][value][7]=d811b929-f561-4128-9b30-1debfe3e6a4a&filter[field_facility_location.id][condition][operator]=IN&filter[field_facility_location.status]=1&include=field_facility_location,field_service_location,field_service_location.field_email_contacts,field_service_location.field_other_phone_numbers,field_service_location.field_service_location_address,field_service_location.field_phone"
+    And I can successfully follow the vamc_system_medical_records_office API flow for "/boston-health-care/medical-records-office/"
---
+++ tests/cypress/integration/step_definitions/api/vamc_system_medical_records_office_api_data_flow.js
@@ -0,0 +1,108 @@
+import { Given } from "@badeball/cypress-cucumber-preprocessor";
+
+/**
+ * Helper function to fetch facilities for a given region page.
+ * @param {string} regionPageId - The UUID of the region page (health_care_region_page).
+ * @return {Cypress.Chainable<string[]>} Array of facility IDs
+ */
+function fetchFacilities(regionPageId) {
+  return cy
+    .request(
+      `/jsonapi/node/health_care_local_facility?filter[status]=1&filter[field_region_page.id]=${regionPageId}&page[limit]=50&page[offset]=0`
+    )
+    .then((response) => {
+      expect(response.status).to.eq(200);
+      return response.body.data.map((facility) => facility.id);
+    });
+}
+
+/**
+ * Helper function to build filter parameters for vha_facility_nonclinical_service.
+ * @param {string[]} facilityIds - Array of facility IDs to filter by.
+ * @return {URLSearchParams} Query parameters
+ */
+function buildServiceParams(facilityIds) {
+  const params = new URLSearchParams({
+    "page[limit]": "1",
+    "filter[status]": "1",
+    "filter[field_service_name_and_descripti.name]": "Medical records",
+    "filter[field_facility_location.status]": "1",
+    include:
+      "field_facility_location,field_service_location,field_service_location.field_email_contacts,field_service_location.field_other_phone_numbers,field_service_location.field_service_location_address,field_service_location.field_phone",
+  });
+
+  // Add facility ID filters using the IN operator format (matching original test structure)
+  // First add all the value filters
+  facilityIds.forEach((facilityId, index) => {
+    params.append(
+      `filter[field_facility_location.id][condition][value][${index}]`,
+      facilityId
+    );
+  });
+  // Then add the condition path and operator
+  params.append(
+    "filter[field_facility_location.id][condition][path]",
+    "field_facility_location.id"
+  );
+  params.append(
+    "filter[field_facility_location.id][condition][operator]",
+    "IN"
+  );
+
+  return params;
+}
+
+/**
+ * Helper function to fetch and validate vha_facility_nonclinical_service nodes.
+ * @param {string[]} facilityIds - Array of facility IDs to filter by.
+ */
+function fetchServices(facilityIds) {
+  const params = buildServiceParams(facilityIds);
+  cy.request(
+    `/jsonapi/node/vha_facility_nonclinical_service?${params.toString()}`
+  ).then((response) => {
+    expect(response.status).to.eq(200);
+  });
+}
+
+/**
+ * Helper function to process the node response and fetch related services.
+ * @param {string} uuid - The UUID of the vamc_system_medical_records_office node.
+ */
+function processNodeAndFetchServices(uuid) {
+  cy.request(
+    `/jsonapi/node/vamc_system_medical_records_offi/${uuid}?include=field_office`
+  ).then((nodeResponse) => {
+    expect(nodeResponse.status).to.eq(200);
+    expect(nodeResponse.body.data.id).to.eq(uuid);
+    const regionPageId =
+      nodeResponse.body.data.relationships.field_office.data.id;
+    // eslint-disable-next-line no-unused-expressions
+    expect(
+      regionPageId,
+      `Expected to find a field_office.id for vamc_system_medical_records_offi: ${uuid}`
+    ).to.not.be.undefined;
+
+    // Fetch facilities for this region page, then fetch services
+    fetchFacilities(regionPageId).then((facilityIds) => {
+      fetchServices(facilityIds);
+    });
+  });
+}
+
+Given(
+  "I can successfully follow the vamc_system_medical_records_office API flow for {string}",
+  (path) => {
+    // Request the router to translate the path to a UUID.
+    cy.request(`/router/translate-path?path=${path}`).then((response) => {
+      expect(response.status).to.eq(200);
+      const { uuid } = response.body.entity;
+      // eslint-disable-next-line no-unused-expressions
+      expect(uuid, `Expected to find a UUID for path: ${path}`).to.not.be
+        .undefined;
+
+      // Process the node and fetch services
+      processNodeAndFetchServices(uuid);
+    });
+  }
+);
