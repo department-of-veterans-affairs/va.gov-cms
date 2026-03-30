f0849d95ff58a98ec5d88f3bbf91097e695d986d
Christian Burk <766573+omahane@users.noreply.github.com>
2026-03-30 11:38:24 -0700

    VACMS-23711: Updates the post_update hook

--- docroot/modules/custom/va_gov_notifications/va_gov_notifications.post_update.php
+++ docroot/modules/custom/va_gov_notifications/va_gov_notifications.post_update.php
@@ -15,3 +15,19 @@ function va_gov_notifications_post_update_install_product_owner_contact_entity_t
 
   return 'Installed the product_owner_contact entity type.';
 }
+
+/**
+ * Ensures the product owner contact config entity type is installed.
+ */
+function va_gov_notifications_post_update_ensure_product_owner_contact_entity_type_installed(): string {
+  $change_list = \Drupal::entityDefinitionUpdateManager()->getChangeList();
+  if (!isset($change_list['product_owner_contact'])) {
+    return 'The product_owner_contact entity type is already installed.';
+  }
+
+  $entity_type_manager = \Drupal::entityTypeManager();
+  $entity_type = $entity_type_manager->getDefinition('product_owner_contact');
+  \Drupal::entityDefinitionUpdateManager()->installEntityType($entity_type);
+
+  return 'Installed the product_owner_contact entity type from follow-up update.';
+}
