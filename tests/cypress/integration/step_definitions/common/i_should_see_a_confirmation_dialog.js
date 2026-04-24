d473f4de04da11dcefde89e80e32f7d2772c00dd
Michael Kinnunen <10411319+Becapa@users.noreply.github.com>
2026-04-24 11:53:37 -0400

    VACMS-21675: Update cypress test and add phpunit test.

--- tests/cypress/integration/step_definitions/common/i_should_see_a_confirmation_dialog.js
+++ tests/cypress/integration/step_definitions/common/i_should_see_a_confirmation_dialog.js
@@ -1,7 +1,12 @@
 import { Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Then("I confirm the dialog with the text {string}", (expectedText) => {
-  cy.on("window:confirm", (text) => {
-    expect(text).to.contain(expectedText);
-  });
-});
+Then(
+  "I should see a confirmation dialog with the text {string}",
+  (expectedText) => {
+    cy.get("#va-gov-vamc-archive-modal").should("be.visible");
+    cy.get("#va-gov-vamc-archive-modal-message").should(
+      "contain",
+      expectedText
+    );
+  }
+);
