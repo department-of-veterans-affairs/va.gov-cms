4d91f9543f456a3f2ef08ece608db1ec59efd565
Christian Burk <766573+omahane@users.noreply.github.com>
2026-04-29 13:37:12 -0500

    VACMS-24047: Updates the config

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,26 @@
 const js = require("@eslint/js");
+const globals = require("globals");
 const eslintConfigPrettier = require("eslint-config-prettier");
 const eslintPluginCypress = require("eslint-plugin-cypress");
+const eslintPluginImport = require("eslint-plugin-import");
 const eslintPluginPrettier = require("eslint-plugin-prettier");
 
 module.exports = [
   js.configs.recommended,
   {
+    linterOptions: {
+      reportUnusedDisableDirectives: "off",
+    },
     plugins: {
-      cypress: eslintPluginCypress,
+      import: eslintPluginImport,
       prettier: eslintPluginPrettier,
     },
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: "module",
       globals: {
+        ...globals.browser,
+        ...globals.node,
         Drupal: "readonly",
         drupalSettings: "readonly",
         drupalTranslations: "readonly",
@@ -27,14 +34,22 @@ module.exports = [
         Popper: "readonly",
         Sortable: "readonly",
         CKEDITOR: "readonly",
+        once: "readonly",
       },
     },
     rules: {
       "prettier/prettier": "error",
-      ...eslintPluginCypress.configs.recommended.rules,
       "consistent-return": "off",
-      "cypress/no-unnecessary-waiting": "off",
+      "import/no-mutable-exports": "warn",
       "no-underscore-dangle": "off",
+        "eqeqeq": ["error", "always", { null: "ignore" }],
+        "curly": ["warn", "all"],
+        "radix": "error",
+      "no-alert": "warn",
+      "no-eval": "warn",
+      "no-implied-eval": "warn",
+      "no-new-func": "warn",
+      "no-throw-literal": "warn",
       "max-nested-callbacks": ["warn", 3],
       "no-plusplus": ["warn", {
         allowForLoopAfterthoughts: true,
@@ -48,6 +63,14 @@ module.exports = [
           ":": "ignore",
         },
       }],
+    },
+  },
+  {
+    ...eslintPluginCypress.configs.recommended,
+    files: ["tests/cypress/**/*.js"],
+    rules: {
+      ...eslintPluginCypress.configs.recommended.rules,
+      "cypress/no-unnecessary-waiting": "off",
     },
   },
   eslintConfigPrettier,
